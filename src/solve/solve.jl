86ed859aa7a2d53c25049aff2b9ef48af78aa697
Matthew Cocci <Matthew.Cocci@ny.frb.org>
2015-07-02 13:28:09 -0400

    Coerce G0,G1 to complex in solve.jl to force complex QZ decomp

--- src/solve/solve.jl
+++ src/solve/solve.jl
@@ -6,5 +6,5 @@ function solve(model::Model)
     G0, G1, C, Ψ, Π = model.eqcond(model.Θ, model.I)
 
     # Currently throws a SingularException
-    G1, C, impact, fmat, fwt, ywt, gev, eu, loose = GenSys.gensys(G0, G1, C, Ψ, Π, 1 + 1e-6)
+    G1, C, impact, fmat, fwt, ywt, gev, eu, loose = GenSys.gensys(complex(G0), complex(G1), C, Ψ, Π, 1 + 1e-5)
 end
