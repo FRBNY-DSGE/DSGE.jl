92fe0ff97c937ebef09013cf24cdad6697c074ca
Pearl Li <pearlli@sas.upenn.edu>
2015-07-16 18:09:15 -0400

    Modify Kalman filter to not compute likelihood of presample
    - Add argument `Ny0` for number of presample observations
    - Rename `notis_nan` to `nonmissing`
    - Add parentheses for clarity in ranges

--- src/estimate/Kalman.jl
+++ src/estimate/Kalman.jl
@@ -26,6 +26,8 @@ export kalcvf2NaN, kalsmth_k93
 #           that is, [η(t)', ε(t)']'.
 #     z0 is an optional [Nz x 1] initial state vector.
 #    vz0 is an optional [Nz x Nz] covariance matrix of an initial state vector.
+#    Ny0 is an optional scalar indicating the number of periods of presample
+#       (i.e. the number of periods which we don't add to the likelihood)
 #
 # The KALCVF function returns the following output:
 #   logl is a value of the average log likelihood function of the SSM
@@ -48,39 +50,23 @@ export kalcvf2NaN, kalsmth_k93
 #  03/19/2003  -  algorithm and interface were adapted from SAS/IML KALCVF subroutine for use in MATLAB M file
 #
 #==========================================================================#
-function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}, F::Array{S, 2}, b::Array{S, 2}, H::Array{S, 2}, var::Array{S, 2}, z0::Array{S, 2}, vz0::Array{S, 2})
-    T = size(data,2)
-    Nz = size(a,1)
-    Ny = size(b,1)
+function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}, F::Array{S, 2}, b::Array{S, 2}, H::Array{S, 2}, var::Array{S, 2}, z0::Array{S, 2}, vz0::Array{S, 2}, Ny0::Int64 = 0)
+    T = size(data, 2)
+    Nz = size(a, 1)
+    Ny = size(b, 1)
 
     z = z0
     P = vz0
 
     # Check input matrix dimensions
-    if size(data, 1) != Ny
-        error("data and b must have the same number of rows")
-    end
-    if size(a, 2) != 1
-        error("a must be column vector")
-    end
-    if size(F) != (Nz, Nz)
-        error("F must be square")
-    end
-    if size(b, 2) != 1
-        error("b must be column vector")
-    end
-    if size(H) != (Ny, Nz)
-        error("H must be Ny by Nz matrix")
-    end
-    if size(var) != (Ny+Nz, Ny+Nz)
-        error("var must be (Ny+Nz) by (Ny+Nz) matrix")
-    end
-    if size(z) != (Nz, 1)
-        error("z0 must be column vector of length Nz")
-    end
-    if size(P) != (Nz, Nz)
-        error("vz0 must be Nz by Nz matrix")
-    end
+    @assert size(data, 1) == Ny
+    @assert size(a, 2) == 1
+    @assert size(F) == (Nz, Nz)
+    @assert size(b, 2) == 1
+    @assert size(H) == (Ny, Nz)
+    @assert size(var) == (Ny + Nz, Ny + Nz)
+    @assert size(z) == (Nz, 1)
+    @assert size(P) == (Nz, Nz)
 
     # V(t) and R(t) are variances of η(t) and ε(t), respectively, and G(t) is a covariance of η(t) and ε(t)
     # In dsgelh :
@@ -88,8 +74,8 @@ function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}
     # --- R is same as EE
     # --- G is same as VV = QQ*MM
     V = var[1:Nz, 1:Nz]
-    R = var[Nz+1:end, Nz+1:end]
-    G = var[1:Nz, Nz+1:end]
+    R = var[(Nz+1):end, (Nz+1):end]
+    G = var[1:Nz, (Nz+1):end]
     
     pred = zeros(Nz, T)
     vpred = zeros(Nz, Nz, T)
@@ -105,13 +91,13 @@ function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}
     for t = 1:T
         # If an element of the vector y(t) is missing (NaN) for the observation t, the
         #   corresponding row is ditched from the measurement equation.
-        notis_nan = !isnan(data[:, t])
-        data_t = data[notis_nan, t]        # data_t = Y_T = [y1, y2, ..., yT] is matrix of observable data time-series
-        H_t = H[notis_nan, :]              # H_t = DD is matrix mapping states to observables
-        G_t = G[:, notis_nan]              # G_t = Cov(η_t, ε_t)
-        R_t = R[notis_nan, notis_nan]      # R_t = Var(ε_t)
+        nonmissing = !isnan(data[:, t])
+        data_t = data[nonmissing, t]       # data_t = Y_T = [y1, y2, ..., yT] is matrix of observable data time-series
+        H_t = H[nonmissing, :]             # H_t = DD is matrix mapping states to observables
+        G_t = G[:, nonmissing]             # G_t = Cov(η_t, ε_t)
+        R_t = R[nonmissing, nonmissing]    # R_t = Var(ε_t)
         Ny_t = length(data_t)              # Ny_t = T is length of time
-        b_t = b[notis_nan, :]              # b_t = DD
+        b_t = b[nonmissing, :]             # b_t = DD
         
         
         ## forecasting
@@ -120,18 +106,19 @@ function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}
         dy = data_t - H_t*z - b_t          # dy = y_t - H(Θ)*z_{t|t-1} - DD is prediction error or innovation
         HG = H_t*G_t                       # HG is ZZ*Cov(η_t, ε_t)
         D = H_t*P*H_t' + HG + HG' + R_t    # D = ZZ*P_{t+t-1}*ZZ' + HG + HG' + R_t 
-        D = .5*(D+D')
+        D = (D+D')/2
 
         pred[:, t] = z
         vpred[:, :, t] = P
-        yprederror[notis_nan, t] = dy
-        ystdprederror[notis_nan, t] = dy./sqrt(diag(D))
+        yprederror[nonmissing, t] = dy
+        ystdprederror[nonmissing, t] = dy./sqrt(diag(D))
 
         ddy = D\dy 
         # We evaluate the log likelihood function by adding values of L at every iteration
         #   step (for each t = 1,2,...T)
-        L += -log(det(D))/2 - dy'*ddy/2 - Ny_t*log(2*pi)/2
-
+        if t > Ny0
+            L += -log(det(D))/2 - dy'*ddy/2 - Ny_t*log(2*pi)/2
+        end
         
         ## updating
         PHG = P*H_t' + G_t
@@ -147,7 +134,7 @@ function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::S, a::Array{S, 2}
     Pend = P
 
     if lead > 1
-        for t = T+2:T+lead
+        for t = (T+2):(T+lead)
             z = F*z + a
             P = F*P*F' + V
             pred[:, t] = z
@@ -172,7 +159,7 @@ end
 # Note that all eigenvalues of the matrix F are inside the unit circle when the SSM is stationary.
 # When the preceding formula cannot be applied, the initial state vector estimate is set to a
 # and its covariance matrix is given by 1E6I. Optionally, you can specify initial values.
-function kalcvf2NaN(data, lead, a, F, b, H, var)
+function kalcvf2NaN{S<:FloatingPoint}(data::Array{S, 2}, lead::Int64, a::Array{S, 2}, F::Array{S, 2}, b::Array{S, 2}, H::Array{S, 2}, var::Array{S, 2}, Ny0::Int64 = 0)
     Nz = size(a, 1)
     V = var[1:Nz, 1:Nz]
 
