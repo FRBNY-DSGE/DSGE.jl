609a596ae4b355620a0b0175917a6f2db37660b1
Pearl Li <pearlli@sas.upenn.edu>
2015-07-21 11:25:12 -0400

    Tidy spec function in m990

--- src/models/m990/m990.jl
+++ src/models/m990/m990.jl
@@ -2,7 +2,6 @@
 # We can then concisely pass around a Model object to the remaining steps of the model
 #   (solve, estimate, and forecast).
 type Model990 <: AbstractModel
-    spec::String
     spec_vars::Dict{String, Any}
     Θ::Parameters
     I::ModelInds
@@ -15,8 +14,21 @@ end
 
 
 function Model990()
-    spec = spec_vars()
+    ### Model-specific specifications
+    spec = Dict{String, Any}()
+    # Number of anticipated policy shocks
+    spec["nant"] = 6
+    # Padding for nant
+    spec["nantpad"] = 20
+    # Number of periods back we should start incorporating zero bound expectations
+    # ZLB expectations should begin in 2008 Q4
+    spec["antlags"] = 24
+
+
+    
     Θ = Parameters990(spec)
     I = ModelInds(spec)
-    return Model990("990", spec, Θ, I, eqcond, measurement)
+    return Model990(spec, Θ, I, eqcond, measurement)
 end
+
+description(model::Model990) = "M990"
