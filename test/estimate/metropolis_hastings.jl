7bc9620643be2c94db724552cb568beb588d13d7
Pearl Li <pearlli@sas.upenn.edu>
2015-08-07 10:43:57 -0400

    Update Metropolis-Hastings test for post-SVD

--- test/estimate/metropolis_hastings.jl
+++ test/estimate/metropolis_hastings.jl
@@ -12,11 +12,7 @@ mf = MatFile("$path/metropolis_hastings.mat")
 mode = get_variable(mf, "params")
 hessian = get_variable(mf, "hessian")
 YY = get_variable(mf, "YYall")
-
 σ = get_variable(mf, "sigscale")
-Σ = get_variable(mf, "s")
-rank = get_variable(mf, "sigpropdim")
-logdet = get_variable(mf, "sigproplndet")
 
 randvecs = get_variable(mf, "randvecs")
 randvals = get_variable(mf, "randvals")
@@ -26,10 +22,7 @@ model = Model990()
 cc0 = 0.01
 cc = 0.09
 
-if false
-    propdist = DegenerateMvNormal(mode, σ, Σ, hessian, rank, logdet)
-else
-    propdist = proposal_distribution(mode, hessian)
-end
-para_sim = metropolis_hastings(propdist, model, YY, cc0, cc, randvecs, randvals)
-mean_para_sim = mean(para_sim, 1)
+propdist = DegenerateMvNormal(mode, σ)
+
+metropolis_hastings(propdist, model, YY, cc0, cc, randvecs, randvals)
+
