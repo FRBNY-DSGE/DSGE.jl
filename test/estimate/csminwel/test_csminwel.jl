3647630487b4250cb7936f3ec55473382597bff6
Micah Smith <micahjsmith@gmail.com>
2015-08-19 17:04:41 -0400

    Add csminwel test between Hobo/DSGE

---
+++ test/estimate/csminwel/test_csminwel.jl
@@ -0,0 +1,26 @@
+import DSGE, Hobo
+
+# Example usage:
+function rosenbrock(x::Vector)
+    (1-x[1])^2.0 + 105*(x[2]-x[1]^2.0)^4.0
+end
+
+# this is the actual gradient of the Rosenbrock function
+function rosenbrock_grad(x::Array)
+    dr = similar(x)
+    dr[1] = -2*(1 - x[1]) - 8*105*x[1]*(x[2]-x[1]^2.0)^3.0
+    dr[2] = 4*105*(x[2]-x[1]^2.0)^3.0
+    badg = false
+    return dr, badg
+end
+
+# A really bad guess
+x_init = [10.0, -9.0]
+
+println("Testing using Hobo.csminwel...")
+res_real_grad  = Hobo.csminwel(rosenbrock, rosenbrock_grad, [10.0, -9.0])
+res_numeric_grad  = Hobo.csminwel(rosenbrock, [10.0, -9.0])
+
+println("Testing using DSGE.csminwel...")
+res_real_grad  = DSGE.csminwel(rosenbrock, rosenbrock_grad, [10.0, -9.0])
+res_numeric_grad  = DSGE.csminwel(rosenbrock, [10.0, -9.0])
