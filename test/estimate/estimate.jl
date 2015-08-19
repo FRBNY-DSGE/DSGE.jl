1df715283f72584584a82e9734701af9ac18ec4b
Micah Smith <micahjsmith@gmail.com>
2015-08-19 18:19:36 -0400

    Continue estimate test skeleton code.

--- test/estimate/estimate.jl
+++ test/estimate/estimate.jl
@@ -4,7 +4,14 @@
 =#
 using DSGE
 
+# This runs.
 m = Model990()
 m.reoptimize=true
 m.recalculate_hessian=true
 estimate(m, verbose=true)
+
+# # We want something more like this:
+# m = ModelTEST()
+# m.reoptimize=true
+# m.recalculate_hessian=true
+# estimate(m, testing=true)
