dc029cab3c56603fa6e4bb1db25447992a75a0ee
Micah Smith <micahjsmith@gmail.com>
2015-08-19 14:51:56 -0400

    Catch gensys error from within MH.
    - Catch errors thrown by gensys from within MH and set post_new
      to silly value.
    - This has the side effect of catching *all* errors that were thrown
      down the posterior! call stack.
    - We could consider instead going as far down as the mh keyword argument
      is passed. Or, we can go even further, pass mh to solve, and catch
      gensys directly.

--- src/estimate/estimate.jl
+++ src/estimate/estimate.jl
@@ -258,9 +258,15 @@ function metropolis_hastings{T<:FloatingPoint}(propdist::Distribution, m::Abstra
                 para_new = rand(propdist; cc=cc)
             end
 
-            # Solve the model, check that parameters are within bounds, and
-            # evaluate the posterior.
-            post_new, like_new, out = posterior!(m, para_new, YY; mh=true)
+            # Solve the model, check that parameters are within bounds, and evaluate the
+            # posterior. If gensys throws an error down the call stack, then catch it, and
+            # set outputs to silly values.
+            try
+                post_new, like_new, out = posterior!(m, para_new, YY; mh=true)
+            catch
+                println(STDERR, "....1 loose endogenous error.")
+                post_new, like_new, out = -Inf, -Inf, None
+            end
 
             if verbose 
                 println("Iteration $j: posterior = $post_new")
