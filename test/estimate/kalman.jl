3a80688905fc30baebbecb7d79ab34f8732b8c33
Erica Moszkowski <erica.moszkowski@gmail.com>
2015-09-23 12:02:56 -0400

    Fix merge error in metropolis-hastings; now passes test and reads in correct version of randvecs/randvals

--- test/estimate/kalman.jl
+++ test/estimate/kalman.jl
@@ -9,15 +9,6 @@ path = dirname(@__FILE__)
 #   -> dsgelh (136) -> kalcvf2NaN
 # See kalcvf2NaN/test_kalcvf2NaN.m
 
-## mf = MatFile("$path/kalcvf2NaN/kalcvf2NaN_args.mat")
-## for arg in ["data", "lead", "a", "F", "b", "H", "var", "z0", "vz0"]
-##     eval(parse("$arg = get_variable(mf, \"$arg\")"))
-## end
-## for arg in ["a", "b", "z0"]
-##     eval(parse("$arg = reshape(get_variable(mf, \"$arg\"), length($arg), 1)"))
-## end
-## close(mf)
-
 
 h5 = h5open("$path/kalcvf2NaN/kalcvf2NaN_args.h5")
 for arg in ["data", "lead", "a", "F", "b", "H", "var", "z0", "vz0"]
@@ -27,7 +18,6 @@ for arg in ["a", "b", "z0"]
     eval(parse("$arg = reshape(read(h5, \"$arg\"), length($arg), 1)"))
 end
 
-close(mf)
 lead = round(Int,lead)
 
 
@@ -37,22 +27,6 @@ L, zend, Pend, pred, vpred, yprederror, ystdprederror, rmse, rmsd, filt, vfilt =
 
 L, zend, Pend = kalcvf2NaN(data, lead, a, F, b, H, var, z0, vz0)
 
-## mf = MatFile("$path/kalcvf2NaN/kalcvf2NaN_out9.mat")
-## for out in ["L", "zend", "Pend", "pred", "vpred", "yprederror", "ystdprederror", "rmse",
-##             "rmsd", "filt", "vfilt" ]
-##     eval(parse("$(out)_expected = get_variable(mf, \"$out\")"))
-
-##     if out == "L"
-##         @test_approx_eq L_expected L
-##     elseif out == "zend"
-##         # Not sure why this has to be enclosed in eval(parse()) to run
-##         eval(parse("zend_expected = reshape(zend_expected, length(zend_expected), 1)"))
-##         @test test_matrix_eq(zend_expected, zend)
-##     else
-##         eval(parse("test_matrix_eq($(out)_expected, $out)"))
-##     end
-## end
-## close(mf)
 
 h5 = h5open("$path/kalcvf2NaN/kalcvf2NaN_out9.h5")
 for out in ["L", "zend", "Pend", "pred", "vpred", "yprederror", "ystdprederror", "rmse",
@@ -79,23 +53,6 @@ L, zend, Pend, pred, vpred, yprederror, ystdprederror, rmse, rmsd, filt, vfilt =
 
 L, zend, Pend = kalcvf2NaN(data, lead, a, F, b, H, var)
 
-## mf = MatFile("$path/kalcvf2NaN/kalcvf2NaN_out7.mat")
-## for out in ["L", "zend", "Pend", "pred", "vpred", "yprederror", "ystdprederror", "rmse",
-##             "rmsd", "filt", "vfilt" ]
-##     eval(parse("$(out)_expected = get_variable(mf, \"$out\")"))
-
-##     if out == "L"
-##         @test_approx_eq_eps L_expected L 1e-4
-##     elseif out == "zend"
-##         zend_expected = reshape(zend_expected, length(zend_expected), 1)
-##         @test test_matrix_eq(zend_expected, zend)
-##     elseif out ∈ ["Pend", "vpred", "vfilt"]
-##         # These matrix entries are especially large, averaging 1e5, so we allow greater ε
-##         eval(parse("@test test_matrix_eq($(out)_expected, $out; ε=0.1)"))
-##     else
-##         eval(parse("@test test_matrix_eq($(out)_expected, $out)"))
-##     end
-## end
 
 h5 = h5open("$path/kalcvf2NaN/kalcvf2NaN_out7.h5")
 for out in ["L", "zend", "Pend", "pred", "vpred", "yprederror", "ystdprederror", "rmse",
