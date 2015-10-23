1b82d53b91e894613ed4692ac3e9681193351e20
Erica Moszkowski <erica.moszkowski@gmail.com>
2015-10-23 15:41:46 -0400

    Start adding documentation for new parameter types

--- src/new_parameters.jl
+++ src/new_parameters.jl
@@ -1,10 +1,29 @@
 using Compat, Distributions, Debug
 import DSGE: PointMass
+import Base: <=
 
+#=
+"""
+Interval{T} is a typealias for Tuple{T,T}
+"""
+=#
 typealias Interval{T} @compat Tuple{T,T}
 
-# define all the kinds of transformations we make
+
+#=
+"""
+Transform
+
+Subtypes of the abstract Transform type indicate how a `Parameter`'s
+value is transformed from model space (which can be bounded to a
+limited section of the real line) to the entire real line (which is
+necessary for mode-finding using csminwel). The transformation is
+performed by the `toreal` function, and is reversed by the `tomodel`
+function.
+"""
+=#
 abstract Transform
+
 type Untransformed <: Transform end
 type SquareRoot    <: Transform end
 type Exponential   <: Transform end
@@ -13,7 +32,31 @@ Base.show(io::IO, t::Untransformed) = @printf io "x -> x\n"
 Base.show(io::IO, t::SquareRoot)    = @printf io "x -> (a+b)/2 + (b-a)/2*c*x/sqrt(1 + c^2 * x^2)\n"
 Base.show(io::IO, t::Exponential)   = @printf io "x -> b + (1/c) * log(x-a)\n"
 
-abstract AbstractParameter{T<:Number} # made this real for conversions
+
+
+"""
+AbstractParameter{T<:Number}
+
+The AbstractParameter type is the common supertype of all model
+parameters, including steady-state values that we treat as
+parameters. Its subtype structure is as follows:
+
+*`AbstractParameter{T<:Number}`
+    *`Parameter{T<:Number, U<:Transform}`
+        *`UnscaledParameter{T<:Number, U:<Transform}`
+        *`ScaledParameter{T<:Number, U:<Transform}`
+    *`SteadyStateParameter{T<:Number}`
+"""
+abstract AbstractParameter{T<:Number} 
+
+
+
+"""
+Parameter{T<:Number, U<:Transform} <: AbstractParameter{T}
+
+The Parameter type is the common supertype of time-invariant, non-steady-state model parameters. It has 2 subtypes, `UnscaledParameter` and `ScaledParameter`. `ScaledParameter`s are parameters whose values are scaled when used in the model's equilibrium conditions
+
+"""
 abstract Parameter{T,U<:Transform} <: AbstractParameter{T}
 
 typealias ParameterVector{T} Vector{AbstractParameter{T}}
@@ -31,14 +74,32 @@ type UnscaledParameter{T,U} <: Parameter{T,U}
     texLabel::AbstractString    # LaTeX label for printing
 end
 
+
+"""
+ScaledParameter{T,U} <: Parameter{T,U}
+
+
+#### Fields
+
+* `key::Symbol`: parameter name
+* `value::T`: The parameter's unscaled value. For compatibility with the existing code setup, it should be initialized "untransformed", i.e. in model space, within `valuebounds`
+* `scaledvalue::T`: Parameter value scaled for use in `eqcond.jl`
+* `valuebounds::Interval{T}`: Bounds for the parameter's value 
+* `transform_parameterization::Interval{T}`: Parameters used to transform `value` between model space and the real line.
+* `transform::U`: The transformation used to convert `value` between model space and the real line, for use in csminwel.
+* `prior::NullablePrior`: Prior distribution for parameter value.
+* `fixed::Bool`: Whether or not the parameter's value is fixed rather than estimated.
+* `scaling::Function`: Function used to scale parameter value for use in equilibrium conditions.
+* `description::AbstractString`: A short description of the parameter's economic significance.
+* `texLabel::AbstractString`: For printing tables of parameter values to LaTeX
+"""
 type ScaledParameter{T,U} <: Parameter{T,U}
     key::Symbol
-    value::T                    # unscaled parameter value in model space
-    scaledvalue::T		# scaled parameter value in model space
-    valuebounds::Interval{T}    # 
-    transform_parameterization::Interval{T}    # 
-    transform::U                # we only use transformed values for csminwel.
-                                # tomodel/toreal takes care of the conversion.
+    value::T 
+    scaledvalue::T		
+    valuebounds::Interval{T}    
+    transform_parameterization::Interval{T}    
+    transform::U               
     prior::NullablePrior
     fixed::Bool
     scaling::Function
@@ -47,19 +108,20 @@ type ScaledParameter{T,U} <: Parameter{T,U}
 end
 
 """
-type SteadyStateParameter{T} <: AbstractParameter{T}
+SteadyStateParameter{T} <: AbstractParameter{T}
 
-Defines a type for the model's steady-state parameters, stored in `m.steady_state`. Their values are calculated and set by `steadystate!(m)`, rather than being estimated directly.  `SteadyStateParameter`s have the following fields:
+Defines a type for the model's steady-state parameters, stored in
+`m.steady_state`. Their values are calculated and set by
+`steadystate!(m)`, rather than being estimated directly. They do not
+require transformations from the model space to the real line or
+scalings.
 
--`key::Symbol`: Key for referencing this parameter
+#### Fields
 
--`value::T`: The parameter's steady-state value
-
--`description::AbstractString`: Short description of the parameter's economic significance
-
--`texLabel::AbstractString`: for LaTeX printing
-
-Unlike true model parameters (for which we must find modal values using csminwel), these values need not be transformed from the model space to the real line. Thus, they do not have `transform` fields. 
+* `key::Symbol`: Key for referencing this parameter
+* `value::T`: The parameter's steady-state value
+* `description::AbstractString`: Short description of the parameter's economic significance
+* `texLabel::AbstractString`: for LaTeX printing
 """
 type SteadyStateParameter{T} <: AbstractParameter{T}
     key::Symbol
@@ -72,11 +134,17 @@ hasprior(p::Parameter) = !isnull(p.prior)
 
 typealias NullableOrPrior Union(NullablePrior, ContinuousUnivariateDistribution)
 
-# We want to use values from UnscaledParameters and
+# We want to use value field from UnscaledParameters and
 # SteadyStateParameters in computation, so we alias their union here.
 typealias UnscaledOrSteadyState Union(UnscaledParameter, SteadyStateParameter)
 
-# method to construct parameters
+
+"""
+parameter{T,U<:Transform}(key::Symbol, value::T, [valuebounds = (value,value)], [transform_parameterization = (value,value)], [transform = Untransformed()], [prior = NullablePrior()], [fixed = true], [scaling::Function = identity], [description = "This variable is missing a description."],[texLabel::AbstractString = ""])
+
+
+By default, returns a fixed `UnscaledParameter` object with key `key` and value `value`. If `scaling` is given, a `ScaledParameter` object is returned.
+"""
 function parameter{T,U<:Transform}(key::Symbol,
                                    value::T,
                                    valuebounds::Interval{T} = (value,value),
