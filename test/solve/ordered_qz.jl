2414fac1de25e2313fa7daf4da93607ec7bf3545
Erica Moszkowski <erica.moszkowski@gmail.com>
2015-10-23 11:43:13 -0400

    bool(x::Number) deprecated, use x !=0 instead

--- test/solve/ordered_qz.jl
+++ test/solve/ordered_qz.jl
@@ -41,7 +41,7 @@ debug && println("Reorder Schur")
         S = schurfact(ordschura)
         select = Compat.bitrand(n)
         O = ordschur(S, select)
-        bool(sum(select)) && @test_approx_eq S[:values][find(select)] O[:values][1:sum(select)]
+        sum(select) != 0 && @test_approx_eq S[:values][find(select)] O[:values][1:sum(select)]
         @test_approx_eq O[:vectors]*O[:Schur]*O[:vectors]' ordschura
     end
 
