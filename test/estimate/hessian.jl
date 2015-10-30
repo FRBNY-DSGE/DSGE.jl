a8d2eb94b42692868d3cb4fb443d3b2f405d7e7e
Micah Smith <micahjsmith@gmail.com>
2015-10-30 16:32:14 -0400

    Confirm that hessian test passes

--- test/estimate/hessian.jl
+++ test/estimate/hessian.jl
@@ -4,14 +4,18 @@ using DSGE, HDF5
 include("../util.jl")
 path = dirname(@__FILE__)
 
-h5 = h5open("$path/../reference/hessian.h5")
-params = read(h5, "params")
-YY = read(h5, "YY")
-hessian_expected = read(h5, "hessian")
-close(h5)
+mode = h5open("$path/../reference/mode_in.h5") do file
+    read(file, "params")
+end
+YY = h5open("$path/../reference/YY.h5") do file
+    read(file, "YY")
+end
+hessian_expected = h5open("$path/../reference/hessian.h5") do file
+    read(file, "hessian")
+end
 
 model = Model990()
 
-@time hessian, stoph = hessizero!(model, params, YY; verbose=true)
-@test test_matrix_eq(hessian_expected, hessian; ϵ=1.0, noisy=true)
+@time hessian, stoph = hessizero!(model, mode, YY; verbose=true)
+@test test_matrix_eq(hessian_expected, hessian; ϵ=1.0, verbose=true)
 
