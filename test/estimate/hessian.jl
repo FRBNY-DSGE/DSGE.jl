c5b2e8de8bcda5e4b7fadd627a86dd0972ec378b
Micah Smith <micahjsmith@gmail.com>
2015-11-02 14:02:17 -0500

    Hessian tests pass, so update with julia versions

--- test/estimate/hessian.jl
+++ test/estimate/hessian.jl
@@ -40,3 +40,14 @@ model = Model990()
 # The entries at (11,9) are -360.19713132228446 + 0.0im (expected) and -359.678834150669 + 0.0im (actual)
 # Max percent error of 56.59815003314423 at entry (68,3)
 # The entries at (68,3) are 2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
+
+# Output - commit c723*, a lot of refactoring.
+# 3913.768002 seconds (1.64 G allocations: 4.428 TB, 5.78% gc time)
+# 734 of 7569 entries have opposite signs
+# 300 of 7569 entries have different signs, but one of the entries is 0
+# 4084 of 7569 entries with abs diff > 0
+# 0 of 7569 entries with abs diff > 1.0
+# Max abs diff of 0.5182971716154725 at entry (11,9)
+# The entries at (11,9) are -360.19713132228446 + 0.0im (expected) and -359.678834150669 + 0.0im (actual)
+# Max percent error of 56.59815003314423 at entry (68,3)
+# The entries at (68,3) are 2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
