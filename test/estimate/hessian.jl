322bba5e634b09c4cc3fa2837bdc96a66c2e8726
Micah Smith <micahjsmith@gmail.com>
2015-11-02 17:59:07 -0500

    First stab at small, working hessian test

--- test/estimate/hessian.jl
+++ test/estimate/hessian.jl
@@ -16,49 +16,17 @@ end
 
 model = Model990()
 
-@time hessian, hessian_errors = hessian!(model, mode, YY; verbose=true)
-@test test_matrix_eq(hessian_expected, hessian; ϵ=1.0, verbose=true)
+# Ad-hoc testing...
+toggle_test_mode(model)
+para_free      = [!θ.fixed for θ in model.parameters]
+para_free_inds = find(para_free)
+num_free_hessian_test = 4
+para_free_inds = para_free_inds[1:num_free_hessian_test]
+max_free_ind = max(para_free_inds...)
 
-# Output 2015-10-30 - normal code
-# 3893.685200 seconds (1.64 G allocations: 4.428 TB, 5.56% gc time)
-# 734 of 7569 entries have opposite signs
-# 300 of 7569 entries have different signs, but one of the entries is 0
-# 4084 of 7569 entries with abs diff > 0
-# 0 of 7569 entries with abs diff > 1.0
-# Max abs diff of 0.5182971716154725 at entry (11,9)
-# The entries at (11,9) are -360.19713132228446 + 0.0im (expected) and -359.678834150669 + 0.0im (actual)
-# Max percent error of 56.59815003314423 at entry (68,3)
-# The entries at (68,3) are 2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
+hessian, hessian_errors = hessian!(model, mode, YY; verbose=false)
+@test test_matrix_eq(hessian_expected[1:max_free_ind, 1:max_free_ind], 
+                     hessian[1:max_free_ind, 1:max_free_ind]; 
+                     ϵ=1.0, verbose=false)
 
-# Output - hessiandiagelement function only used
-# 3925.981645 seconds (1.64 G allocations: 4.428 TB, 5.59% gc time)
-# 734 of 7569 entries have opposite signs
-# 300 of 7569 entries have different signs, but one of the entries is 0
-# 4084 of 7569 entries with abs diff > 0
-# 0 of 7569 entries with abs diff > 1.0
-# Max abs diff of 0.5182971716154725 at entry (11,9)
-# The entries at (11,9) are -360.19713132228446 + 0.0im (expected) and -359.678834150669 + 0.0im (actual)
-# Max percent error of 56.59815003314423 at entry (68,3)
-# The entries at (68,3) are 2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
-
-# Output - commit c723*, a lot of refactoring.
-# 3913.768002 seconds (1.64 G allocations: 4.428 TB, 5.78% gc time)
-# 734 of 7569 entries have opposite signs
-# 300 of 7569 entries have different signs, but one of the entries is 0
-# 4084 of 7569 entries with abs diff > 0
-# 0 of 7569 entries with abs diff > 1.0
-# Max abs diff of 0.5182971716154725 at entry (11,9)
-# The entries at (11,9) are -360.19713132228446 + 0.0im (expected) and -359.678834150669 + 0.0im (actual)
-# Max percent error of 56.59815003314423 at entry (68,3)
-# The entries at (68,3) are 2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
-
-# Output (15 workers) - commit f086, everything parallelized
-# 337.695092 seconds (3.93 M allocations: 197.360 MB, 0.01% gc time)
-# 46 of 7569 entries have opposite signs
-# 36 of 7569 entries have different signs, but one of the entries is 0
-# 3484 of 7569 entries with abs diff > 0
-# 0 of 7569 entries with abs diff > 1.0
-# Max abs diff of 0.6220439783200788 at entry (17,10)
-# The entries at (17,10) are 37.70649227325377 + 0.0im (expected) and 38.32853625157385 + 0.0im (actual)
-# Max percent error of 54.59815003314423 at entry (68,32)
-# The entries at (68,32) are -2.7578448419364482e-5 + 0.0im (expected) and -0.0015333107129011558 + 0.0im (actual)
+toggle_test_mode(model)
