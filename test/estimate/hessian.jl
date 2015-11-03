97e2ace352aa41475da6c5ce05dee2c291124d48
Micah Smith <micahjsmith@gmail.com>
2015-11-02 22:21:20 -0500

    Test hessian at mode rather than at para vector

--- test/estimate/hessian.jl
+++ test/estimate/hessian.jl
@@ -4,13 +4,13 @@ using DSGE, HDF5
 include("../util.jl")
 path = dirname(@__FILE__)
 
-mode = h5open("$path/../reference/mode_in.h5") do file
-    read(file, "params")
+mode = h5open("$path/../reference/mode_in_optimized.h5") do file
+    read(file, "mode")
 end
 YY = h5open("$path/../reference/YY.h5") do file
     read(file, "YY")
 end
-hessian_expected = h5open("$path/../reference/hessian.h5") do file
+hessian_expected = h5open("$path/../reference/hessian_optimized.h5") do file
     read(file, "hessian")
 end
 
@@ -24,9 +24,9 @@ num_free_hessian_test = 4
 para_free_inds = para_free_inds[1:num_free_hessian_test]
 max_free_ind = max(para_free_inds...)
 
-hessian, hessian_errors = hessian!(model, mode, YY; verbose=false)
+hessian, hessian_errors = hessian!(model, mode, YY; verbose=true)
 @test test_matrix_eq(hessian_expected[1:max_free_ind, 1:max_free_ind], 
                      hessian[1:max_free_ind, 1:max_free_ind]; 
-                     ϵ=1.0, verbose=false)
+                     ϵ=1.0, verbose=true)
 
 toggle_test_mode(model)
