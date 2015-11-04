8efce5da0e93499ff8cfcb7fafc661fc3ed76040
Micah Smith <micahjsmith@gmail.com>
2015-11-04 10:55:55 -0500

    Give travis a helping hand

--- test/estimate/hessian.jl
+++ test/estimate/hessian.jl
@@ -28,7 +28,7 @@ if max_free_ind < maximum(para_free_inds)
     para_free_inds = para_free_inds[1:max_free_ind]
 end
 
-@time hessian, _ = hessian!(model, mode, YY)
+@time hessian, _ = hessian!(model, mode, YY; verbose=true)
 @test test_matrix_eq(hessian_expected[1:max_free_ind, 1:max_free_ind], 
                      hessian[1:max_free_ind, 1:max_free_ind]; 
                      ϵ=1.0)
