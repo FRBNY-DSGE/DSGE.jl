c86e107f197e21aefc9641c884e065738765375d
Micah Smith <micahjsmith@gmail.com>
2015-11-09 14:11:36 -0500

    Refactoring and documentation

--- src/estimate/posterior.jl
+++ src/estimate/posterior.jl
@@ -1,8 +1,8 @@
-#=
-doc"""
-Calculates (log of) the joint density of the model parameters.
 """
-=#
+`prior(model)`
+
+Calculates log joint prior density of the model parameters.
+"""
 function prior{T<:AbstractFloat}(model::AbstractDSGEModel{T})
     x = zero(T)
     for θ in model.parameters
@@ -13,44 +13,28 @@ function prior{T<:AbstractFloat}(model::AbstractDSGEModel{T})
     return x
 end
 
-# Type returned by posterior
-immutable Posterior{T<:AbstractFloat}
-    post::T
-    like::T
-    mats::Dict{Symbol, Matrix{T}}
-end
-function Posterior{T<:AbstractFloat}(post::T = -Inf,
-                                     like::T = -Inf,
-                                     mats::Dict{Symbol,Matrix{T}}    = Dict{Symbol,Matrix{T}}())
-    return Posterior{T}(post, like, mats)
-end
-function Base.getindex(P::Posterior, d::Symbol)
-    if d in (:post, :like, :mats)
-        return getfield(P, d)
-    else
-        throw(KeyError(d))
-    end
-end
+"""
+```
+posterior{T<:AbstractFloat}(model::AbstractDSGEModel{T}, YY::Matrix{T}; 
+                             mh::Bool = false, catch_errors::Bool = false)
+```
 
-#=
-doc"""
-posterior{T<:AbstractFloat}(model::AbstractDSGEModel, YY::Matrix{T}; mh::Bool = false, catch_errors::Bool = false)
+Calculates and returns the log of the posterior distribution for the model parameters:
+```
+log posterior = log likelihood + log prior
+log Pr(Θ|YY)  = log Pr(YY|Θ)   + log Pr(Θ)
+```
 
-### Parameters
+### Arguments
 -`model`: the model object
 -`YY`: matrix of data for observables
 
 ### Optional Arguments
--`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the log likelihood and the transition matrices for the zero-lower-bound period are also returned.
+-`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the log likelihood and the
+  transition matrices for the zero-lower-bound period are also returned.
 -`catch_errors`: Whether or not to catch errors of type `GensysError` or `ParamBoundsError`
-
-### Description
-Calculates and returns the log of the posterior distribution for the model parameters:
-  log posterior = log likelihood + log prior
-  log Pr(Θ|YY)  = log Pr(YY|Θ)   + log Pr(Θ)    # where Θ is `m.parameters`
 """
-=#
-function posterior{T<:AbstractFloat}(model::AbstractDSGEModel,
+function posterior{T<:AbstractFloat}(model::AbstractDSGEModel{T},
                                      YY::Matrix{T};
                                      mh::Bool = false,
                                      catch_errors::Bool = false)
@@ -64,23 +48,25 @@ function posterior{T<:AbstractFloat}(model::AbstractDSGEModel,
     end
 end
 
-#=
-doc"""
-posterior!{T<:AbstractFloat}(model::AbstractDSGEModel, parameters::Vector{T}, YY::Matrix{T}; [mh::Bool = false], [catch_errors::Bool = false])
+"""
+```
+posterior!{T<:AbstractFloat}(model::AbstractDSGEModel{T}, parameters::Vector{T}, YY::Matrix{T};
+                              mh::Bool = false, catch_errors::Bool = false)
+```
 
-### Parameters
+Evaluates the log posterior density at `parameters`
+
+### Arguments
 -`model`: The model object
 -`parameters`: New values for the model parameters
 - `YY`: Matrix of input data for observables
 
 ### Optional Arguments
--`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the log likelihood and the transition matrices for the zero-lower-bound period are also returned.
--`catch_errors`: Whether or not to catch errors of type `GensysError` or `ParamBoundsError`. If `mh = true`, both should always be caught.
-
-### Description
-Evaluates the log posterior distribution at `parameters`
+-`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the log likelihood and the
+  transition matrices for the zero-lower-bound period are also returned.
+-`catch_errors`: Whether or not to catch errors of type `GensysError` or `ParamBoundsError`.
+  If `mh = true`, both should always be caught.
 """
-=#
 function posterior!{T<:AbstractFloat}(model::AbstractDSGEModel{T},
                                       parameters::Vector{T},
                                       YY::Matrix{T};
@@ -102,23 +88,31 @@ function posterior!{T<:AbstractFloat}(model::AbstractDSGEModel{T},
 
 end
 
-#=
-doc"""
-likelihood{T<:AbstractFloat}(model::AbstractDSGEModel, YY::Matrix{T}; mh::Bool = false, catch_errors::Bool = false)
+"""
+```
+likelihood{T<:AbstractFloat}(model::AbstractDSGEModel, YY::Matrix{T}; 
+                              mh::Bool = false, catch_errors::Bool = false)
+```
 
-### Parameters
+`likelihood` is a dsge likelihood function that can handle 2-part estimation where the
+observed sample contains both a normal stretch of time (in which interest rates are
+positive) and a stretch of time in which interest rates reach the zero lower bound. If
+there is a zero-lower-bound period, then we filter over the 2 periods separately.
+Otherwise, we filter over the main sample all at once.
+
+### Arguments
 -`model`: The model object
 -`YY`: matrix of data for observables
 
-## Optional Arguments
--`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the transition matrices for the zero-lower-bound period are returned in a dictionary.
+### Optional Arguments
+-`mh`: Whether metropolis_hastings is the caller. If `mh=true`, the transition matrices for
+  the zero-lower-bound period are returned in a dictionary.
 -`catch_errors`: If `mh = true`, `GensysErrors` should always be caught.
-
-### Description
-`likelihood` is a dsge likelihood function that can handle 2-part estimation where the observed sample contains both a normal stretch of time (in which interest rates are positive) and a stretch of time in which interest rates reach the zero lower bound. If there is a zero-lower-bound period, then we filter over the 2 periods separately. Otherwise, we filter over the main sample all at once.
 """
-=#
-function likelihood{T<:AbstractFloat}(model::AbstractDSGEModel{T}, YY::Matrix{T}; mh::Bool = false, catch_errors::Bool = false)
+function likelihood{T<:AbstractFloat}(model::AbstractDSGEModel{T},
+                                      YY::Matrix{T};
+                                      mh::Bool = false,
+                                      catch_errors::Bool = false)
     catch_errors = catch_errors | mh
     LIKE_NULL_DICT   = Dict{Symbol, Matrix{T}}()
     LIKE_NULL_OUTPUT = (-Inf, LIKE_NULL_DICT)
@@ -261,117 +255,22 @@ function likelihood{T<:AbstractFloat}(model::AbstractDSGEModel{T}, YY::Matrix{T}
     end
 end
 
-
-
-#=
-doc"""
-DLYAP   Discrete Lyapunov equation solver.
-
-   X = DLYAP(A,Q) solves the discrete Lyapunov equation:
-
-    A*X*A' - X + Q = 0
-
-   See also  LYAP.
-
- This is a port of the following Matlab implementation:
- J.N. Little 2-1-86, AFP 7-28-94
- Copyright 1986-2001 The MathWorks, Inc.
- $Revision: 1.11 $  $Date: 2001/01/18 19:50:01 $
-
-LYAP  Solve continuous-time Lyapunov equations.
-
-  x = LYAP(a,c) solves the special form of the Lyapunov matrix
-  equation:
-
-          a*x + x*a' = -c
-
-  See also  DLYAP.
-
- This is a port of the following Matlab implementation:
- S.N. Bangert 1-10-86
- Copyright 1986-2001 The MathWorks, Inc.
- $Revision: 1.10 $  $Date: 2001/01/18 19:50:23 $
- Last revised JNL 3-24-88, AFP 9-3-95
-
-How to prove the following conversion is true.  Re: show that if
-        (1) Ad X Ad' + Cd = X             Discrete lyaponuv eqn
-        (2) Ac = inv(Ad + I) (Ad - I)     From dlyap
-        (3) Cc = (I - Ac) Cd (I - Ac')/2  From dlyap
-Then
-        (4) Ac X + X Ac' + Cc = 0         Continuous lyapunov
-
-Step 1) Substitute (2) into (3)
-        Use identity 2*inv(M+I) = I - inv(M+I)*(M-I)
-                                = I - (M-I)*inv(M+I) to show
-        (5) Cc = 4*inv(Ad + I)*Cd*inv(Ad' + I)
-Step 2) Substitute (2) and (5) into (4)
-Step 3) Replace (Ad - I) with (Ad + I -2I)
-        Replace (Ad' - I) with (Ad' + I -2I)
-Step 4) Multiply through and simplify to get
-        X -inv(Ad+I)*X -X*inv(Ad'+I) +inv(Ad+I)*Cd*inv(Ad'+I) = 0
-Step 5) Left multiply by (Ad + I) and right multiply by (Ad' + I)
-Step 6) Simplify to (1)
-"""
-=#
-function dlyap(a, c)
-    return dlyap!(copy(a), copy(c))
+# Type returned by posterior
+immutable Posterior{T<:AbstractFloat}
+    post::T
+    like::T
+    mats::Dict{Symbol, Matrix{T}}
 end
-function dlyap!(a, c)
-    m, n = size(a)
-    a = (a + UniformScaling(1))\(a - UniformScaling(1))
-    c = (UniformScaling(1)-a)*c*(UniformScaling(1)-a')/2
-
-    mc, nc = size(c)
-
-    # a and c must be square and the same size
-    if (m != n) || (m != mc) || (n != nc)
-        error("Dimensions do not agree.")
-    elseif m == 0
-        x = zeros(m, m)
-        return x
-    end
-
-    # Perform schur decomposition on a (and convert to complex form)
-    ta, ua, _ = schur(complex(a)) # matlab code converts to complex - come back to
-    # ua, ta = rsf2csf(ua, ta)
-    # Schur decomposition of a' can be calculated from that of a.
-    j = m:-1:1
-    ub = ua[:, j]
-    tb = ta[j ,j]'
-
-    # Check all combinations of ta(i, i)+tb(j, j) for zero
-    p1 = diag(ta).' # Use .' instead of ' in case a and a' are not real
-    p2 = diag(tb)
-    p_sum = abs(p1) .+ abs(p2)
-    if any(p_sum .== 0) || any(abs(p1 .+ p2) .< 1000*eps()*p_sum)
-        error("Solution does not exist or is not unique.")
-    end
-
-    # Transform c
-    ucu = -ua'*c*ub
-
-    # Solve for first column of transformed solution
-    y = complex(zeros(n, n))
-    y[:, 1] = (ta + UniformScaling(tb[1, 1]))\ucu[:, 1]
-
-    # Solve for remaining columns of transformed solution
-    for k=1:n-1
-        km1 = 1:k
-        y[:, k+1] = (ta + UniformScaling(tb[k+1, k+1]))\(ucu[:, k+1] - y[:, km1]*tb[km1, k+1])
-    end
-
-    # Find untransformed solution
-    x = ua*y*ub'
-
-    # Ignore complex part if real inputs (better be small)
-    if isreal(a) && isreal(c)
-        x = real(x)
-    end
-
-    # Force x to be symmetric if c is symmetric
-    if issym(c)
-        x = (x+x')/2
-    end
-
-    return x
+function Posterior{T<:AbstractFloat}(post::T = -Inf,
+                                     like::T = -Inf,
+                                     mats::Dict{Symbol,Matrix{T}}    = Dict{Symbol,Matrix{T}}())
+    return Posterior{T}(post, like, mats)
 end
+function Base.getindex(P::Posterior, d::Symbol)
+    if d in (:post, :like, :mats)
+        return getfield(P, d)
+    else
+        throw(KeyError(d))
+    end
+end
+
