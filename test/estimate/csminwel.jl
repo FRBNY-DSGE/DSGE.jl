05dee2d36848ca9dc6568a16c68a249db8f6c7a6
Micah Smith <micahjsmith@gmail.com>
2015-11-12 11:16:48 -0500

    More robust csminwel testing

--- test/estimate/csminwel.jl
+++ test/estimate/csminwel.jl
@@ -5,31 +5,32 @@ include("../util.jl")
 path = dirname(@__FILE__)
 
 # Test in generic case
+# Global minimum at (a, a^2)
+a = 1
+b = 100
 function rosenbrock(x::Vector)
-    a = 100
-    b = 1
-    return a*(x[2]-x[1]^2.0)^2.0 + b*(1-x[1])^2.0
+    return (a-x[1])^2.0 + b*(x[2]-x[1]^2.0)^2.0
 end
 
 # this is the actual gradient of the Rosenbrock function
 function rosenbrock_grad(x::Array)
     dr = similar(x)
-    dr[1] = -2*(1 - x[1]) - 8*105*x[1]*(x[2]-x[1]^2.0)^3.0
-    dr[2] = 4*105*(x[2]-x[1]^2.0)^3.0
+    dr[1] = -2*(a-x[1]) - 2b*(2x[1])*(x[2]-x[1]^2.0)
+    dr[2] = 2b*(x[2]-x[1]^2.0)
     badg = false
     return dr, badg
 end
 
 # A really bad guess
 x_init = [10.0, -9.0]
-x_expected = [1.0, 1.0]
+x_expected = [a, a^2.0]
 
 model = Model990()
 
 res_real_grad, _ = csminwel(rosenbrock, rosenbrock_grad, [10.0, -9.0])
-@test x_expected == res_real_grad.minimum
+@test_approx_eq x_expected res_real_grad.minimum
 res_numeric_grad, _ = csminwel(rosenbrock, [10.0, -9.0], model=model)
-@test x_expected == res_numeric_grad.minimum
+@test_approx_eq_eps x_expected res_numeric_grad.minimum 1e-8
 
 # Test in model optimization
 model = Model990()
@@ -42,11 +43,9 @@ YY = h5open(inpath(model, "data", "data_REF.h5")) do file
     read(file, "YY")
 end
 file = h5open("$path/../reference/csminwel_out.h5","r")
-x5 = read(file, "x5")
-f5 = read(file, "f5")
-f_calls = read(file, "f_calls")
-g_calls = read(file, "g_calls")
-H_expected = read(file, "H")
+minimum_ = read(file, "minimum")
+f_minimum = read(file, "f_minimum")
+H_expected = read(file, "H_expected")
 close(file)
 
 # See src/estimate/estimate.jl
@@ -63,11 +62,14 @@ nit = 5
 crit = 1e-10
 
 out, H = csminwel(posterior_min!, x0, H; model=model, ftol=crit, iterations=nit, show_trace=false, verbose=:none)
+# h5 = h5open("/home/rcemjs04/.julia/v0.4/DSGE/test/reference/csminwel_out.h5","w")
+# h5["minimum"] = out.minimum
+# h5["f_minimum"] = out.f_minimum
+# h5["H_expected"] = H
+# close(h5)
 
-@test_matrix_approx_eq x5 out.minimum
-@test_approx_eq f5 out.f_minimum
-@test f_calls == out.f_calls
-@test g_calls == out.g_calls
+@test_matrix_approx_eq minimum_ out.minimum
+@test_approx_eq f_minimum out.f_minimum
 @test_matrix_approx_eq H_expected H
 
 nothing
