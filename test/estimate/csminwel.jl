42b3df1de1ef8100c4c744f756b77be9a6b531b6
Micah Smith <micahjsmith@gmail.com>
2015-11-12 14:23:03 -0500

    Reduce scope of csminwel test

--- test/estimate/csminwel.jl
+++ test/estimate/csminwel.jl
@@ -25,36 +25,9 @@ end
 x_init = [10.0, -9.0]
 x_expected = [a, a^2.0]
 
-res_real_grad, _ = csminwel(rosenbrock, rosenbrock_grad, [10.0, -9.0])
+res_real_grad, _ = csminwel(rosenbrock, rosenbrock_grad, x_init)
 @test_approx_eq x_expected res_real_grad.minimum
-res_numeric_grad, _ = csminwel(rosenbrock, [10.0, -9.0])
+res_numeric_grad, _ = csminwel(rosenbrock, x_init)
 @test_approx_eq_eps x_expected res_numeric_grad.minimum 1e-8
-
-# Test in model optimization
-model = Model990()
-model.testing=true
-
-x0 = h5open(inpath(model, "user", "mode_in.h5")) do file
-    read(file, "params")
-end
-YY = h5open(inpath(model, "data", "data_REF.h5")) do file
-    read(file, "YY")
-end
-file = h5open("$path/../reference/csminwel_out.h5","r")
-minimum_ = read(file, "minimum")
-f_minimum = read(file, "f_minimum")
-H_expected = read(file, "H_expected")
-close(file)
-
-# See src/estimate/estimate.jl
-update!(model, x0)
-nit = 5
-crit = 1e-10
-
-@time out, H = optimize!(model, YY; ftol=crit, iterations=nit)
-
-@test_matrix_approx_eq minimum_ out.minimum
-@test_approx_eq f_minimum out.f_minimum
-@test_matrix_approx_eq H_expected H
 
 nothing
