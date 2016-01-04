7546b1329a4ae96812e29307b73984ee760ce790
Erica Moszkowski <erica.moszkowski@gmail.com>
2016-01-04 17:47:33 -0500

    Fixes and checks added to `load_data` and `load_fred_data`
    - Match quarters on end dates
    - Only select appropriate rows/columns
    - Add documentation
    - FRED mnemonics beginning with PRS cannot be fetched for some reason...

--- src/data/fred_data.jl
+++ src/data/fred_data.jl
@@ -1,66 +1,88 @@
 """
-`load_fred_data(m::AbstractModel; start_date="1959-01-01", end_date=last_quarter_end())`
+`load_fred_data(m::AbstractModel; start_date="1959-03-31", end_date=last_quarter_end())`
 
 Checks in `inpath(m)` for a FRED dataset corresponding to
-`data_vintage(m)`. Necessary series (identified by series mnemonics in
-`m.data_series[:fred]`) that are not found on disk are fetched from the FRED
-API. The full dataset is returned and written to the appropriate data vintage file.
+`data_vintage(m)`. The appropriate vintage of necessary series
+(identified by series mnemonics in `m.data_series[:fred]`) that are
+not found on disk are fetched via the FRED API. The full dataset is
+written to the appropriate data vintage file and returned.
 
 # Arguments
 - `m::AbstractModel`: the model object
-- `start_date`: starting date. 
+- `start_date`: starting date.
+- `end_date`: ending date. 
+
+# Notes
+
+The FRED API reports observations according to the quarter-start
+date. `load_fred_data` returns data indexed by quarter-end date for
+compatibility with other datasets.
 """
-function load_fred_data(m::AbstractModel; start_date="1959-01-01", end_date=last_quarter_end())
-    mnemonics = m.data_series[:fred]
+function load_fred_data(m::AbstractModel;
+                        start_date::Date=Date("1959-01-01", "y-m-d"),
+                        end_date::Date=last_quarter_end())
     
-    # Date formatting
-    df = "y-m-d"
-    start_date = Date(start_date, df)
-    if isa(end_date, AbstractString) end_date = Date(end_date, df) end
+    mnemonics = m.data_series[:fred]
+    vint = data_vintage(m)
+    df2 = "yymmdd"
+
+    # Have to do this wacky parsing to prepend the century to the data vintage
+    vint_date = if parse(Int,vint[1:2]) < 59
+        Year(2000) + Date(vint, df2)
+    else
+        Year(1900) + Date(vint, df2)
+    end
 
     # Get data vintage and check that last_quarter_end is before that.
-    vint = data_vintage(m)
-    vint_date = parse(Int,vint[1:2]) < 59 ? Year(2000) + Date(vint, "yymmdd") : Year(1900) + Date(vint, "yymmdd")
-    vint_date < end_date && error("Data vintage is older than the end date specified")
+    @assert vint_date >= end_date "Data vintage must be more recent than end date specified."
     
     n_quarters = 4*(year(end_date) - year(start_date)) - quarterofyear(start_date) + quarterofyear(end_date) + 1
     
     # Set up dataset and labels
-    missing_series = []
-    dataset = []
-    
-    datafile = inpath(m, "data", "data_$vint.txt") 
+    missing_series = Vector{Symbol}()
+    data = []
+
+    datafile = inpath(m, "data", "fred_$vint.txt")
     if isfile(datafile)
-
+        
         # Read in dataset and check that the file contains data for the proper dates
-        dataset = readtable(datafile, separator = '\t')
+        data = readtable(datafile, separator = '\t')
 
-        if !in(string(firstdayofquarter(start_date)), dataset[:date]) || 
-            !in(string(firstdayofquarter(end_date)), dataset[:date])
+        # Convert dates from strings to dates for date arithmetic
+        data[:date] = stringstodates(data[:date])
+        println(names(data))
+        map!(x->lastdayofquarter(x), data[:date], data[:date])
+        
+        qstart = lastdayofquarter(start_date)
+        qend = lastdayofquarter(end_date)
+
+        if !in(qstart, data[:date]) || !in(qend, data[:date])
+            println("Start and end dates are not in data...fetching all FRED series...")
+            data = DataFrame(date = get_quarter_ends(start_date,end_date))
             missing_series = mnemonics
         else
             # If we have the right dates but the series isn't in the file, add it to missing_series
             for series in mnemonics
-                if !in(symbol(series), names(dataset))
-                    append!(missing_series,series)
+                if !in(series, names(data))
+                    push!(missing_series,series)
                 end
             end
         end
     else
         missing_series = mnemonics
-        dataset = DataFrame(date = get_quarter_starts(start_date,end_date))
+        data = DataFrame(date = get_quarter_ends(start_date,end_date))
     end
 
     # Get the missing data series
-    if missing_series != []
+    if !isempty(missing_series)
         
         fredseries = Array{FredSeries, 1}(length(missing_series))
         f = Fred()
         
         for (i,s) in enumerate(missing_series)
-            println(s)
+            println("Fetching $(s)...")
             try
-                fredseries[i] = get_data(f, s; frequency="q", observation_start = string(start_date),
+                fredseries[i] = get_data(f, string(s); frequency="q", observation_start=string(start_date),
                                          observation_end=string(end_date), vintage_dates=string(vint_date))
             catch err
                 warn("$s could not be fetched.")
@@ -68,22 +90,32 @@ function load_fred_data(m::AbstractModel; start_date="1959-01-01", end_date=last
             end
         end
         
-        # Extract dataframes from series and merge them on date
+        # Extract dataframe from each series and merge on date
         for i = 1:length(fredseries)
             if isdefined(fredseries, i)
                 series = fredseries[i]
                 series_id = symbol(series.id)
                 rename!(series.df, :value, series_id)
-                dataset = join(dataset, series.df[:date, series_id], on=:date)
+                map!(x->lastdayofquarter(x), series.df[:date], series.df[:date])
+                data = join(data, series.df[:,[:date, series_id]], on=:date)
             end
         end
+        
+        # Change the dates to be the last day of each quarter
+        n_rows, n_cols = size(data)
 
-        writetable(datafile, dataset, separator = '\t')
+        for i = 1:n_rows
+            data[i,:date] = Dates.lastdayofquarter(data[i,:date])
+        end
+        
+        writetable(datafile, data, separator = '\t')
+        println("Data written to $datafile.")
     end
 
     # Make sure to only return the series and dates that are specified for this
-    # model (there may be additional series in the # file)
+    # model (there may be additional series in the file)
 
-     cols = [:date; convert(Vector{Symbol}, [symbol(s) for s in mnemonics])]
-     return dataset[string(start_date):string(end_date), cols]
+    rows = start_date .<= data[:date] .<= end_date
+    cols = [:date; mnemonics]
+    return data[rows, cols]
 end
--- src/data/load_data.jl
+++ src/data/load_data.jl
@@ -1,43 +1,79 @@
-function load_data(m::AbstractModel; start_date="1959-01-01", end_date=last_quarter_end())
-    data = load_fred_data(m, start_date, end_date)
+"""
+`load_data(m::AbstractModel; start_date="1959-03-31", end_date=last_quarter_end())`
 
-    # For each dataset, search for the file with the proper name. Open
+Checks in `inpath(m)` for vintaged datasets corresponding to the ones
+in `keys(m.data_series)`. Loads the appriopriate data series
+(specified in `m.data_series[source]`) for each data source, and
+returns a consolidated DataFrame (merged on date).
+
+# Arguments
+- `m::AbstractModel`: the model object
+- `start_date`: starting date.
+- `end_date`: ending date. 
+
+# Notes:
+The name of the input data file must be the same as the source string
+in `m.data_series`, and those files must be located in .txt files in
+`inpath(m, "data")`.
+"""
+function load_data(m::AbstractModel; start_date="1959-03-31", end_date=last_quarter_end())
+
+    # Date formatting
+    start_date = Date(start_date, "y-m-d")
+    end_date = Date(end_date, "y-m-d")
+
+    # Load FRED data first
+    data = load_fred_data(m; start_date=firstdayofquarter(start_date), end_date=end_date)
+
+    # For each additional source, search for the file with the proper name. Open
     # it, read it in, and merge it with fred_series
-
+    
     vint = data_vintage(m)
     for source in keys(m.data_series)
-        infile = inpath(m, "data", "$(string(source))_$vint")
+        
+        if isequal(source, :fred)
+            continue
+        end
+        
+        infile = inpath(m, "data", "$(string(source))_$vint.txt")
         mnemonics = m.data_series[source]
-        dataset = []
         
         if isfile(infile)
 
             # Read in dataset and check that the file contains data for the proper dates
-            addl_data = readtable(datafile, separator = '\t')
-            
-            if !in(string(firstdayofquarter(start_date)), dataset[:date]) || 
-                !in(string(firstdayofquarter(end_date)), dataset[:date])
-                error("$infile does not contain the date range specified. You may need to update your data.")
-            end
+            addl_data = readtable(infile, separator = '\t')
 
+            # Convert dates from strings to quarter-end dates for date arithmetic
+            addl_data[:date] = stringstodates(addl_data[:date])
+            map!(x->lastdayofquarter(x), addl_data[:date], addl_data[:date])
+            
+            if !in(lastdayofquarter(start_date), addl_data[:date]) || 
+                !in(lastdayofquarter(end_date), addl_data[:date])
+                error("$infile does not contain the date range specified. Check that data are up to date.")
+            end
+            
             # make sure each mnemonic that was specified is present
             for series in mnemonics
-                if !in(symbol(series), names(dataset))
-                    error("$(series) is missing from $infile.")
+                if !in(series, names(addl_data))
+                    error("$(string(series)) is missing from $infile.")
                 end
             end
 
             # extract just the columns and rows of the dataset we want, and merge them with data
-            cols = [:date; convert(Vector{Symbol}, [symbol(s) for s in mnemonics])]]
-            addl_data = addl_data[start_date:string(end_date), cols]            
+            cols = [:date; mnemonics]
+            rows = start_date .<= addl_data[:date] .<= end_date
+            
+            addl_data = addl_data[rows, cols]            
             data = join(data, addl_data, on=:date)
             
-        else
+        elseif isempty(m.data_series[source])
+            warn("No series were specificed from $(string(source))")
+            continue
+        else 
             error("$infile was not found." )
         end
-
     end
-
+    
     return data
 end
 
@@ -50,19 +86,31 @@ Returns the first day of the previous quarter
 function last_quarter_end()
     
     cqs = Dates.firstdayofquarter(now())     # current quarter start
-    lqe = cqs - Day(1)                       # last quarter end
+    lqe = cqs - Dates.Day(1)                       # last quarter end
 
-    Dates.format(lqe, "yyyy-mm-dd")
+    Date(lqe)
 end
 
 """
 Returns a DataArray of quarter start dates between `start_date` and `end_date`.
 """
-function get_quarter_starts(start_date,end_date)
+function get_quarter_ends(start_date,end_date)
     dr = start_date:end_date
     
     dates = recur(dr) do x
-        firstdayofquarter(x) == x 
+        lastdayofquarter(x) == x 
     end
 end
 
+"""
+Converts a collection of strings in "y-m-d" format to Dates.
+"""
+function stringstodates(stringarray)
+    n = length(stringarray)
+    dates = Vector{Date}(n)
+    for i = 1:n
+        dates[i] = Date(stringarray[i], "y-m-d")
+    end
+
+    dates
+end
