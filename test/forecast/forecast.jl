2c21350a73cf11440a153ae6e56b0ee265e391f8
Micah Smith <micahjsmith@gmail.com>
2016-05-18 16:55:31 -0400

    Fix test forecast call

--- test/forecast/forecast.jl
+++ test/forecast/forecast.jl
@@ -3,18 +3,18 @@
 
 path = dirname(@__FILE__())
 
-
 m = Model990()
 m.testing = true
 
 ndraws = 2
 sys = compute_system(m)
 syses = repmat([sys],ndraws)
-
-zend = zeros(ndraws,DSGE.n_states_augmented(m))
+zend = Vector{Vector{Float64}}()
+for i in 1:ndraws
+    push!(zend, zeros(DSGE.n_states_augmented(m)))
+end
 
 # fcasts = DSGE.forecast(m, syses, zend)
 states, observables = DSGE.forecast(m, syses, zend)
-
 
 nothing
