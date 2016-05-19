955dff337723dc725773a5864eaf0a6c7de380f8
Micah Smith <micahjsmith@gmail.com>
2016-05-19 19:33:19 -0400

    Add input_type option for "init"

--- test/forecast/forecast_all.jl
+++ test/forecast/forecast_all.jl
@@ -5,14 +5,9 @@ path = dirname(@__FILE__)
 
 # 
 m = Model990()
-#m.testing = true
+m.testing = true
 
-#datapath = inpath(m, "data", "data_REF.h5")
-datapath = abspath("$path/../reference/data/data_REF.h5")
-data = h5open(datapath) do file
-    read(file, "data")
-end
-
-forecast_one(m, data; input_type=:mode, output_type=:forecast, cond_type=:none)
+df = load_data(m; try_disk=true, verbose=:none)
+forecast_one(m, df; input_type=:init, output_type=:forecast, cond_type=:none)
 
 nothing
