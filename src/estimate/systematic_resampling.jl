a984492ca6bc99c67fb5cc2b88af9925f55ad2dc
Chris Rytting <christophermichaelrytting@gmail.com>
2016-07-11 15:51:27 -0400

    Translations of supplemental smc files

---
+++ src/estimate/systematic_resampling.jl
@@ -0,0 +1,36 @@
+function systematic_resampling( w )
+
+np = length(w)
+w = w'
+cw = cumsum(w)
+uu = zeros(length(w),1)
+csi=rand(1)
+
+for j=1:length(w)
+
+    uu(j) = (j-1)+csi
+    
+end
+    
+    indx = zeros(np, 1)
+
+parfor i = 1:np
+    
+    u = uu(i)/length(w)
+
+    j=1
+    while j <= np
+       if (u < cw(j)), break, end
+       
+       j = j+1
+        
+    end
+
+    indx(i) = j
+
+end
+
+m = 0
+
+return indx, m
+end
