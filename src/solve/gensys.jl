9b8d377dc2543a893fbc846c39200172e1419a5c
matthieugomez <gomez.matthieu@gmail.com>
2016-07-19 10:00:56 -0400

    improve gensys
    no error + in place operations + Int type

--- src/solve/gensys.jl
+++ src/solve/gensys.jl
@@ -3,9 +3,10 @@
 
 """
 ```
-gensys(Γ0, Γ1, c, ψ, π)
-gensys(Γ0, Γ1, c, ψ, π, div)
-gensys(F::Base.LinAlg.GeneralizedSchur, c, ψ, π, div)
+gensys(Γ0, Γ1, c, Ψ, Π)
+gensys(Γ0, Γ1, c, Ψ, Π, div)
+gensys(F::Base.LinAlg.GeneralizedSchur, c, Ψ, Π)
+gensys(F::Base.LinAlg.GeneralizedSchur, c, Ψ, Π, div)
 ```
 
 Generate state-space solution to canonical-form DSGE model.
@@ -43,74 +44,64 @@ We constrain Julia to use the complex version of the `schurfact` routine regardl
 types of `Γ0` and `Γ1`, to match the behavior of Matlab.  Matlab always uses the complex version
 of the Schur decomposition, even if the inputs are real numbers.
 """
-function gensys(Γ0, Γ1, c, ψ, π)
-    F = schurfact(complex(Γ0), complex(Γ1))
-    div = new_div(F)
-    gensys(F, c, ψ, π, div)
+function gensys(Γ0, Γ1, c, Ψ, Π, args...)
+    F = schurfact!(complex(Γ0), complex(Γ1))
+    gensys(F, c, Ψ, Π, args...)
 end
-function gensys(Γ0, Γ1, c, ψ, π, div)
-    F = schurfact(complex(Γ0), complex(Γ1))
-    gensys(F, c, ψ, π, div)
+
+function gensys(F::Base.LinAlg.GeneralizedSchur, c, Ψ, Π)
+    gensys(F, c, Ψ, Π, new_div(F))
 end
 
 # Method that does the real work. Work directly on the decomposition F
-function gensys(F::Base.LinAlg.GeneralizedSchur, c, ψ, π, div)
+function gensys(F::Base.LinAlg.GeneralizedSchur, c, Ψ, Π, div)
     eu = [0, 0]
     ϵ = 1e-6  # small number to check convergence
-    nunstab = 0.0
+    nunstab = 0
     zxz = 0
-    a, b, q, z = F[:S], F[:T], F[:Q]', F[:Z]
+    a, b, = F[:S], F[:T]
     n = size(a, 1)
 
-    for i=1:n
+    for i in 1:n
         nunstab += (abs(b[i, i]) > div * abs(a[i,i]))
-        if abs(a[i, i]) < ϵ && abs(b[i, i]) < ϵ
+        if (abs(a[i, i]) < ϵ) && (abs(b[i, i]) < ϵ)
             zxz = 1
         end
     end
 
     if zxz == 1
-        throw(GensysError("Coincident zeros.  Indeterminacy and/or nonexistence."))
+        info("Coincident zeros.  Indeterminacy and/or nonexistence.")
+        eu=[-2, -2]
+        G1 = Array{Float64, 2}() ;  C = Array{Float64, 1}() ; impact = Array{Float64, 2}() ; fmat = Array{Complex{Float64}, 2}() ; fwt = Array{Complex{Float64}, 2}() ; ywt = Vector{Complex{Float64}}() ; gev = Vector{Complex{Float64}}() ; loose = Array{Float64, 2}()
+        return G1, C, impact, fmat, fwt, ywt, gev, eu, loose
     end
 
-    select = (abs(F[:alpha]) .> div*abs(F[:beta]))
-    FS = ordschur(F, select)
-    a, b, q, z = FS[:S], FS[:T], FS[:Q]', FS[:Z]
-    gev = [diag(a) diag(b)]
-
-    nunstab_int = round(Int,nunstab)
-
-    q1 = q[1:n-nunstab_int, :]
-    q2 = q[n-nunstab_int+1:n, :]
-    z1 = z[:, 1:n-nunstab_int]'
-    z2 = z[:, n-nunstab_int+1:n]'
-    a2 = a[n-nunstab_int+1:n, n-nunstab_int+1:n]
-    b2 = b[n-nunstab_int+1:n, n-nunstab_int+1:n]
-
-    etawt = q2 * π
-    neta = size(π, 2)
+    select = abs(F[:alpha]) .> div * abs(F[:beta])
+    FS = ordschur!(F, select)
+    a, b, qt, z = FS[:S], FS[:T], FS[:Q], FS[:Z]
+    gev = vcat(diag(a), diag(b))
+    qt1 = qt[:, 1:(n - nunstab)]
+    qt2 = qt[:, (n - nunstab + 1):n]
+    etawt = Ac_mul_B(qt2, Π)
+    neta = size(Π, 2)
 
     # branch below is to handle case of no stable roots, rather than quitting with an error
     # in that case.
-    if isapprox(nunstab, 0.0)
+    if nunstab == 0
         etawt == zeros(0, neta)
         ueta = zeros(0, 0)
         deta = zeros(0, 0)
         veta = zeros(neta, 0)
         bigev = 0
     else
-        ueta, deta, veta = svd(etawt)
-        deta = diagm(deta)  # TODO: do we need to do this
-        md = min(size(deta)...)
-        bigev = find(diag(deta[1:md,1:md]) .> ϵ)
-        ueta = ueta[:, bigev]
-        veta = veta[:, bigev]
-        deta = deta[bigev, bigev]
+        etawtsvd = svdfact!(etawt)
+        bigev = find(etawtsvd[:S] .> ϵ)
+        ueta = etawtsvd[:U][:, bigev]
+        veta = etawtsvd[:V][:, bigev]
+        deta = diagm(etawtsvd[:S][bigev])
     end
 
     eu[1] = length(bigev) >= nunstab
-
-    # eu[1] == 1 && info("gensys: Existence of a solution!")
 
     # Note that existence and uniqueness are not just matters of comparing
     # numbers of roots and numbers of endogenous errors.  These counts are
@@ -124,97 +115,68 @@ function gensys(F::Base.LinAlg.GeneralizedSchur, c, ψ, π, div)
         veta1 = zeros(neta, 0)
         deta1 = zeros(0, 0)
     else
-        etawt1 = q1 * π
-        ndeta1 = min(n-nunstab, neta)
-        ueta1, deta1, veta1 = svd(etawt1)
-        deta1 = diagm(deta1)  # TODO: do we need to do this
-        md = min(size(deta1)...)
-        bigev = find(diag(deta1[1:md, 1:md]) .> ϵ)
-        ueta1 = ueta1[:, bigev]
-        veta1 = veta1[:, bigev]
-        deta1 = deta1[bigev, bigev]
+        etawt1 = Ac_mul_B(qt1, Π)
+        ndeta1 = min(n - nunstab, neta)
+        etawt1svd = svdfact!(etawt1)
+        bigev = find(etawt1svd[:S] .> ϵ)
+        ueta1 = etawt1svd[:U][:, bigev]
+        veta1 = etawt1svd[:V][:, bigev]
+        deta1 = diagm(etawt1svd[:S][bigev])
     end
 
     if isempty(veta1)
-        unique = 1
+        unique = true
     else
-        loose = veta1-veta*veta'*veta1
-        ul, dl, vl = svd(loose)
-        dl = diagm(dl)  # TODO: do we need to do this
-        nloose = sum(abs(diag(dl)) .> ϵ*n)
+        loose = veta1 - A_mul_Bc(veta, veta) * veta1
+        loosesvd = svdfact!(loose)
+        nloose = sum(abs(loosesvd[:S]) .> ϵ * n)
         unique = (nloose == 0)
     end
 
     if unique
-        # info("gensys: Unique solution!")
         eu[2] = 1
-    # else
-    #     println("Indeterminacy. $nloose loose endog errors.")
+    else
+        info("Indeterminacy. $(nloose) loose endogeneous errors")
     end
 
-    # Cast as an int because we use it as an int!
-    nunstab = round(Int, nunstab)
-    tmat = [eye(convert(Int64,(n-nunstab))) -(ueta*(deta\veta')*veta1*deta1*ueta1')']
 
-    G0 = [tmat*a; zeros(nunstab,n-nunstab) eye(nunstab)]
-    G1 = [tmat*b; zeros(nunstab,n)]
+    tmat = hcat(eye(n - nunstab), -(ueta * (deta \ veta') * veta1 * A_mul_Bc(deta1, ueta1))')
+
+    G0 = vcat(tmat * a, hcat(zeros(nunstab, n - nunstab), eye(nunstab)))
+    G1 = vcat(tmat * b, zeros(nunstab, n))
 
     # G0 is always non-singular because by construction there are no zeros on
     # the diagonal of a(1:n-nunstab,1:n-nunstab), which forms G0's ul corner.
     G0I = inv(G0)
-    G1 = G0I*G1
-    usix = n-nunstab+1:n
-    C = G0I * [tmat*q*c; (a[usix, usix] - b[usix,usix])\q2*c]
-    impact = G0I * [tmat*q*ψ; zeros(nunstab, size(ψ, 2))]
-    fmat = b[usix, usix]\a[usix,usix]
-    fwt = -b[usix, usix]\q2*ψ
+    G1 = G0I * G1
+    usix = (n - nunstab + 1):n
+    Busix = b[usix,usix]
+    Ausix = a[usix,usix]
+    C = G0I * vcat(tmat * Ac_mul_B(qt, c), (Ausix - Busix) \ Ac_mul_B(qt2, c))
+    impact = G0I * vcat(tmat * Ac_mul_B(qt, Ψ), zeros(nunstab, size(Ψ, 2)))
+    fmat = Busix \ Ausix
+    fwt = -Busix \ Ac_mul_B(qt2, Ψ)
     ywt = G0I[:, usix]
 
-    loose = G0I * [etawt1 * (eye(neta) - veta * veta'); zeros(nunstab, neta)]
+    loose = G0I * vcat(etawt1 * (eye(neta) - A_mul_Bc(veta, veta)), zeros(nunstab, neta))
 
-    # above are output for system in terms of z'y
-    G1 = real(z*G1*z')
-    C = real(z*C)
+    G1 = real(z * A_mul_Bc(G1, z))
+    C = real(z * C)
     impact = real(z * impact)
     loose = real(z * loose)
 
-    ywt=z*ywt
-
-    if eu[1] != 1 || eu[2] != 1
-        throw(GensysError("Gensys does not give existence and uniqueness."))
-    end
+    ywt = z * ywt
 
     return G1, C, impact, fmat, fwt, ywt, gev, eu, loose
 end
 
-"""
-```
-GensysError <: Exception
-```
-
-A `GensysError` is thrown when Gensys does not give a unique solution, or no solution
-exists. If a `GensysError`is thrown during Metropolis-Hastings, it is caught by `posterior`.
-`posterior` then returns a value of `-Inf`, which Metropolis-Hastings always rejects.
-
-### Fields
-
-* `msg::ASCIIString`: Info message. Default = "Error in gensys." 
-"""
-type GensysError <: Exception
-    msg::ASCIIString
-end
-GensysError() = GensysError("Error in gensys.")
-Base.showerror(io::IO, ex::GensysError) = print(io, ex.msg)
 
 function new_div(F::Base.LinAlg.GeneralizedSchur)
     ϵ = 1e-6  # small number to check convergence
     n = size(F[:T], 1)
-
-    a, b, q, z = F[:S], F[:T], F[:Q]', F[:Z]
-
+    a, b = F[:S], F[:T]
     div = 1.01
-
-    for i=1:n
+    for i in 1:n
         if abs(a[i, i]) > 0
             divhat = abs(b[i, i]) / abs(a[i, i])
             if 1 + ϵ < divhat && divhat <= div
@@ -225,3 +187,6 @@ function new_div(F::Base.LinAlg.GeneralizedSchur)
 
     return div
 end
+
+
+
