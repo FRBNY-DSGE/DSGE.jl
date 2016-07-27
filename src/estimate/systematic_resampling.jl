cb7025ab3478b3085db34a8bc41aa1e19fcdee58
caimichael <michaeldcai@gmail.com>
2016-07-27 11:00:06 -0400

    fixed minor syntax errors. Documentation needs to be written.

--- src/estimate/systematic_resampling.jl
+++ src/estimate/systematic_resampling.jl
@@ -7,27 +7,21 @@ uu = zeros(length(w),1)
 csi=rand(1)
 
 for j=1:length(w)
-
     uu(j) = (j-1)+csi
-    
 end
     
-    indx = zeros(np, 1)
+indx = zeros(np, 1)
 
-parfor i = 1:np
-    
+@parallel for i = 1:np
     u = uu(i)/length(w)
-
     j=1
     while j <= np
-       if (u < cw(j)), break, end
-       
-       j = j+1
-        
+        if (u < cw(j)) 
+            break
+        end
+        j = j+1
     end
-
     indx(i) = j
-
 end
 
 m = 0
