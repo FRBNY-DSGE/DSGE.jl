185ed2da6826c8e287c31b2090d8f305653f0992
Erica Moszkowski <erica.moszkowski@gmail.com>
2016-08-24 12:09:35 -0400

    actually add simulated_annealing file

---
+++ src/estimate/simulated_annealing.jl
@@ -0,0 +1,33 @@
+function simulated_annealing(fcn::Function,
+                             x0::Vector,
+                             args...;
+                             xtol::Real           = 1e-32, # default from Optim.jl
+                             ftol::Float64        = 1e-14, # Default from csminwel
+                             iterations::Int      = 1000,
+                             store_trace::Bool    = false,
+                             show_trace::Bool     = false,
+                             extended_trace::Bool = false,
+                             neighbor!::Function  = Optim.default_neighbor!,
+                             kwargs...)
+
+#    println("$(neighbor!)")
+    optim_options = OptimizationOptions(x_tol       = xtol,
+                                        f_tol       = ftol,
+                                        iterations  = iterations,
+                                        store_trace = store_trace,
+                                        show_trace  = show_trace,
+                                        extended_trace = extended_trace)
+
+    optimize(fcn, x0, SimulatedAnnealing(neighbor! = neighbor!), optim_options)
+end
+
+# function neighbor_dsge!(x::Array, x_proposal::Array)
+#     @assert size(x) == size(x_proposal)
+
+#     for i in 1:length(x)
+#         r = randn() * 0.01
+#         @inbounds x_proposal[i] = x[i] + r
+#     end
+
+    
+# end
