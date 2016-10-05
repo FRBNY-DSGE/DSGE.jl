16db3fd53c6895cc1d4313d5367cf1c9df28d2c4
Pearl Li <pearlzli16@gmail.com>
2016-10-05 14:45:10 -0400

    Update filterandsmooth tests for DArrays

--- test/forecast/filterandsmooth.jl
+++ test/forecast/filterandsmooth.jl
@@ -1,4 +1,4 @@
-using DSGE, DataFrames, HDF5
+using DSGE, DataFrames, HDF5, DistributedArrays
 include("../util.jl")
 
 path = dirname(@__FILE__())
@@ -15,28 +15,42 @@ end
 
 df = load_data(m; try_disk = true, verbose = :none)
 
+# Add parallel workers
 ndraws = 2
-syses = Vector{System{Float64}}(ndraws)
-for i = 1:ndraws
-    params = squeeze(params_sim[i, :], 1)
-    update!(m, params)
-    syses[i] = compute_system(m)
+my_procs = addprocs(ndraws)
+@everywhere using DSGE
+
+# Set up systems
+function init_systems(m, params_sim, ndraws, my_procs)
+    DArray((ndraws,), my_procs, [length(my_procs)]) do I
+        draw_inds = first(I)
+        ndraws_local = length(draw_inds)
+        localpart = Vector{System{Float64}}(ndraws_local)
+
+        for i in draw_inds
+            i_local = mod(i-1, ndraws_local) + 1
+
+            params = squeeze(params_sim[i, :], 1)
+            update!(m, params)
+            localpart[i_local] = compute_system(m)
+        end
+        return localpart
+    end
 end
+syses = init_systems(m, params_sim, ndraws, my_procs)
 
 z0  = (eye(n_states_augmented(m)) - syses[1][:TTT]) \ syses[1][:CCC]
 vz0 = QuantEcon.solve_discrete_lyapunov(syses[1][:TTT], syses[1][:RRR]*syses[1][:QQ]*syses[1][:RRR]')
 
-# Add parallel workers
-my_procs = addprocs(ndraws)
-@everywhere using DSGE
-states, shocks, pseudo = filterandsmooth(m, df, syses; allout = true)
-states, shocks, pseudo = filterandsmooth(m, df, syses, z0, vz0; allout = true)
+# Run to compile before timing
+states, shocks, pseudo = filterandsmooth(m, df, syses; my_procs = my_procs)
+states, shocks, pseudo = filterandsmooth(m, df, syses, z0, vz0; my_procs = my_procs)
 
 for smoother in [:durbin_koopman, :kalman]
     m <= Setting(:forecast_smoother, smoother)
 
     # Without providing z0 and vz0
-    @time states, shocks, pseudo = filterandsmooth(m, df, syses; allout = true)
+    @time states, shocks, pseudo = filterandsmooth(m, df, syses; my_procs = my_procs)
 
     exp_states = Vector{Matrix{Float64}}(ndraws)
     exp_shocks = Vector{Matrix{Float64}}(ndraws)
@@ -50,13 +64,13 @@ for smoother in [:durbin_koopman, :kalman]
             kalman_smoother(m, df, syses[i], kal[:z0], kal[:vz0], kal[:pred], kal[:vpred])
         end
 
-        @test_matrix_approx_eq exp_states[i] states[i]
-        @test_matrix_approx_eq exp_shocks[i] shocks[i]
-        @test all(x -> x == 0, pseudo[i])
+        @test_matrix_approx_eq exp_states[i] convert(Array, slice(states, i, :, :))
+        @test_matrix_approx_eq exp_shocks[i] convert(Array, slice(shocks, i, :, :))
+        @test all(x -> x == 0, convert(Array, slice(pseudo, i, :, :)))
     end
 
     # Providing z0 and vz0
-    @time states, shocks, pseudo = filterandsmooth(m, df, syses, z0, vz0; allout = true)
+    @time states, shocks, pseudo = filterandsmooth(m, df, syses, z0, vz0; my_procs = my_procs)
 
     exp_states = Vector{Matrix{Float64}}(ndraws)
     exp_shocks = Vector{Matrix{Float64}}(ndraws)
@@ -70,9 +84,9 @@ for smoother in [:durbin_koopman, :kalman]
             kalman_smoother(m, df, syses[i], kal[:z0], kal[:vz0], kal[:pred], kal[:vpred])
         end
 
-        @test_matrix_approx_eq exp_states[i] states[i]
-        @test_matrix_approx_eq exp_shocks[i] shocks[i]
-        @test all(x -> x == 0, pseudo[i])
+        @test_matrix_approx_eq exp_states[i] convert(Array, slice(states, i, :, :))
+        @test_matrix_approx_eq exp_shocks[i] convert(Array, slice(shocks, i, :, :))
+        @test all(x -> x == 0, convert(Array, slice(pseudo, i, :, :)))
     end
 
 end
