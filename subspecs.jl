b4bde6c7f8f639c0b36a1682fbf9bfbc13e18b10
Erica Moszkowski <erica.moszkowski@gmail.com>
2017-02-02 14:55:23 -0500

    add subspec 2 (estimate rho_AAA and sigma_AAA freely) to m1010

--- subspecs.jl
+++ subspecs.jl
@@ -8,6 +8,21 @@ called from within the model constructor.
 function init_subspec!(m::Model1010)
     if subspec(m) == "ss1"
         return
+
+    elseif subspec(m) == "ss2"
+        # estimate the ρ_AAA and put measurement error on the AAA spread
+        m <= parameter(:ρ_AAA, 0.5, (0.0, 1.0), (0.0, 1.0), DSGE.SquareRoot(), BetaAlt(0.5, 0.2),
+                       fixed=false,
+                       description="ρ_AAA: AR(1) coefficient in the AAA spread process.",
+                       tex_label="\\rho_{AAA}")
+
+        m <= parameter(:σ_AAA, 0.1, (1e-8, 5.),(1e-8, 5.),DSGE.Exponential(),DSGE.RootInverseGamma(2., 0.75),
+                       fixed=false,
+                       description="σ_AAA: Standard deviation on the AR(1) process for measurement error on the AAA spread.",
+                   tex_label="\\sigma_{AAA}")
+
+    elseif subspec(m) == "ss3"
+        return
     else
         error("This subspec is not defined.")
     end
