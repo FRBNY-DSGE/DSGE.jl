b4565797557a86b459aa63a0d909c4f87b10e3b4
Pearl Li <pearlzli16@gmail.com>
2017-02-17 15:55:45 -0500

    Write real filter and smooth tests

--- test/estimate/filter.jl
+++ test/estimate/filter.jl
@@ -12,14 +12,14 @@ df, system, z0, P0 = jldopen("$path/../reference/forecast_args.jld", "r") do fil
 end
 
 # Read expected output
-exp_kal_no_z0, exp_kal_z0 = jldopen("$path/../reference/filter_out.jld", "r") do file
-    read(file, "exp_kal_no_z0"), read(file, "exp_kal_z0")
+exp_kal = jldopen("$path/../reference/filter_out.jld", "r") do file
+    read(file, "exp_kal")
 end
 
 # Without providing z0 and P0
 kal = DSGE.filter(m, df, system)
-for out in setdiff(fieldnames(kal), [:ystdprederror, :rmsd]) # TODO FIXME
-    expect = exp_kal_no_z0[out]
+for out in fieldnames(kal)
+    expect = exp_kal[out]
     actual = kal[out]
 
     ndims(expect) == 0 ? @test_approx_eq(expect, actual) : @test_matrix_approx_eq(expect, actual)
@@ -27,11 +27,12 @@ end
 
 # Providing z0 and P0
 kal = DSGE.filter(m, df, system, z0, P0)
-for out in setdiff(fieldnames(kal), [:ystdprederror, :rmsd])
-    expect = exp_kal_z0[out]
+for out in fieldnames(kal)
+    expect = exp_kal[out]
     actual = kal[out]
 
     ndims(expect) == 0 ? @test_approx_eq(expect, actual) : @test_matrix_approx_eq(expect, actual)
 end
+
 
 nothing
--- test/forecast/smooth.jl
+++ test/forecast/smooth.jl
@@ -18,20 +18,17 @@ exp_states, exp_shocks, exp_pseudo = jldopen("$path/../reference/smooth_out.jld"
     read(file, "exp_pseudo")
 end
 
+
 # Call smoother and test
 for smoother in [:durbin_koopman, :koopman]
     m <= Setting(:forecast_smoother, smoother)
 
-    # Temporary (during renaming)
-    if smoother == :koopman
-        smoother = :kalman
-    end
-
     states, shocks, pseudo = smooth(m, df, system, kal)
 
-    @test_matrix_approx_eq exp_states[smoother, :z0] states
-    @test_matrix_approx_eq exp_shocks[smoother, :z0] shocks
-    @test_matrix_approx_eq exp_pseudo[smoother, :z0] pseudo
+    @test_matrix_approx_eq exp_states states
+    @test_matrix_approx_eq exp_shocks shocks
+    @test_matrix_approx_eq exp_pseudo pseudo
 end
+
 
 nothing
