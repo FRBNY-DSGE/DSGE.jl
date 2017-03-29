b5c693bc43497c07b80b895192116675e8b450f3
Pearl Li <pearlzli16@gmail.com>
2017-03-28 23:13:39 -0400

    Add model 1010, subspec 18

--- subspecs.jl
+++ subspecs.jl
@@ -400,6 +400,37 @@ function init_subspec!(m::Model1010)
                    description="σ_b_safep: Standard deviation of stationary component of safe asset preference shifter process.",
                    tex_label="\\sigma_{b^p, safe}")
 
+    elseif subspec(m) == "ss18"
+       # ss13 with ρ_z_p fixed at 0.99
+
+        m <= parameter(:ρ_AAA, 0.5, (0.0, 1.0), (0.0, 1.0), DSGE.SquareRoot(), BetaAlt(0.5, 0.1),
+                       fixed=false,
+                       description="ρ_AAA: AR(1) coefficient in the AAA spread process.",
+                       tex_label="\\rho_{AAA}")
+
+        m <= parameter(:ρ_BBB, 0.5, (0.0, 1.0), (0.0, 1.0), DSGE.SquareRoot(), BetaAlt(0.5, 0.1),
+                       fixed=false,
+                       description="ρ_BBB: AR(1) coefficient in the BBB spread process.",
+                       tex_label="\\rho_{BBB}")
+
+        m <= parameter(:σ_BBB, 0.1, (1e-8, 5.),(1e-8, 5.),DSGE.Exponential(),DSGE.RootInverseGamma(2., 0.10),
+                       fixed=false,
+                       description="σ_BBB: Standard deviation on the AR(1) process for measurement error on the BBB spread.",
+                       tex_label="\\sigma_{BBB}")
+
+        m <= parameter(:σ_b_liqp, sqrt(1/400)/4, (1e-8, 5.), (1e-8, 5.), DSGE.Exponential(), DSGE.RootInverseGamma(100., sqrt(1/400)/4),
+                   fixed=false,
+                   description="σ_b_liqp: Standard deviation of stationary component of liquid asset preference shifter process.",
+                   tex_label="\\sigma_{b^p, liq}")
+
+        m <= parameter(:σ_b_safep, sqrt(1/400)/4, (1e-8, 5.), (1e-8, 5.), DSGE.Exponential(), DSGE.RootInverseGamma(100., sqrt(1/400)/4),
+                   fixed=false,
+                   description="σ_b_safep: Standard deviation of stationary component of safe asset preference shifter process.",
+                   tex_label="\\sigma_{b^p, safe}")
+
+        m <= parameter(:ρ_z_p,     0.99, (0.0, 1.0),      (0.0, 1.0), DSGE.SquareRoot(),    BetaAlt(0.5, 0.2),
+                   fixed=true, description="ρ_z_p: No description available.", tex_label="\\rho_{z^p}")
+
     else
         error("This subspec is not defined.")
     end
