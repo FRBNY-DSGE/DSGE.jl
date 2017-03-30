c0b1ce78797f9f31dc0091c52e1c5be59865cdb7
Pearl Li <pearlzli16@gmail.com>
2017-03-30 11:51:49 -0400

    Add plot_irfs

---
+++ src/plot/plot_irfs.jl
@@ -0,0 +1,59 @@
+using DSGE, Plots, DataStructures
+
+"""
+```
+plot_irfs(shock, vars, mb; bands = [\"90.0%\"], output_file = "")
+```
+
+Plots the responses of `vars` to `shock`, given a `MeansBands` object `mb`. If
+`output_file` is provided in the form \"base.ext\", the individual plots will
+also each be saved as \"base__var.ext\". By default, only 90% bands are plotted.
+
+Returns an `OrderedDict{Symbol, Plot}` indexed by variable name.
+"""
+function plot_irfs(shock::Symbol, vars::Vector{Symbol}, mb::MeansBands;
+                   bands::Vector{ASCIIString} = ["90.0%"],
+                   output_file::AbstractString = "")
+    # Split `output_file` into base and extension
+    if !isempty(output_file)
+        strs = split(output_file, ".")
+        base = join(strs[1:end-1], ".")
+        ext  = "." * strs[end]
+        save_plots = true
+    end
+
+    nperiods = size(mb.means, 1)
+    periods = 0:nperiods
+
+    nplots = length(vars)
+    allplots = OrderedDict{Symbol, Plot}()
+
+    # Iterate through variables
+    for var in vars
+        p = Plots.plot(title = string(var), margin = 10px)
+        varshock = Symbol("$(var)__$(shock)")
+
+        # Plot 90% bands
+        for pct in bands
+            ub = vcat([0.0], mb.bands[varshock][Symbol("$pct UB")])
+            lb = vcat([0.0], mb.bands[varshock][Symbol("$pct LB")])
+            plot!(p, periods, ub, fillto = lb,
+                  label = "", color = :blue, α = 0.10)
+        end
+
+        # Plot mean
+        mean = vcat([0.0], mb.means[varshock])
+        plot!(p, periods, mean, label = "", linewidth = 2, linecolor = :black)
+
+        # Save if `output_file` provided
+        if save_plots
+            this_file = base * "__$var" * ext
+            Plots.savefig(this_file)
+            println("Saved $this_file")
+        end
+
+        allplots[var] = p
+    end
+
+    return allplots
+end
