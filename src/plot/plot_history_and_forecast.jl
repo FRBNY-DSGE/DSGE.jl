33969dd0ed67192c703ce45c985e57732f4aa833
Pearl Li <pearlzli16@gmail.com>
2017-05-03 16:22:33 -0400

    Add start_date for plot_history_and_forecast

--- src/plot/plot_history_and_forecast.jl
+++ src/plot/plot_history_and_forecast.jl
@@ -1,8 +1,7 @@
-using DSGE, GR, Plots
+using DSGE, Plots
 
 function plot_history_and_forecast(var::Symbol, history::MeansBands, forecast::MeansBands;
-                                   # start_date::Nullable{Date} = Nullable{Date}(),
-                                   # end_date::Nullable{Date} = Nullable{Date}(),
+                                   start_date::Nullable{Date} = Nullable{Date}(),
                                    output_file::AbstractString = "",
                                    hist_label::AbstractString = "History",
                                    forecast_label::AbstractString = "Forecast",
@@ -11,32 +10,40 @@ function plot_history_and_forecast(var::Symbol, history::MeansBands, forecast::M
                                    forecast_band_color::Colorant = RGBA(0., 0., 1., 0.1))
     # Concatenate MeansBands
     combined = cat(history, forecast)
-    hist_inds  = 1:size(history.means, 1)
-    fcast_inds = size(history.means, 1):size(combined.means, 1)
 
     # Dates
-    dates      = map(quarter_date_to_number, combined.means[:date])
-    start_date = ceil(dates[1] / 5) * 5
-    end_date   = dates[end]
-    date_ticks = start_date:5:end_date
+    datenums = map(quarter_date_to_number, combined.means[:date])
+    start_ind = if !isnull(start_date)
+        @assert history.means[1, :date] <= get(start_date) <= history.means[end, :date]
+        findfirst(datenums, quarter_date_to_number(get(start_date)))
+    else
+        1
+    end
+    t0 = ceil(datenums[start_ind] / 5) * 5
+    t1 = datenums[end]
+    date_ticks = t0:5:t1
 
-    # Initialize GR backend
+    hist_inds  = start_ind:size(history.means, 1)
+    fcast_inds = size(history.means, 1):size(combined.means, 1)
+    all_inds   = start_ind:size(combined.means, 1)
+
+   # Initialize GR backend
     gr()
     p = Plots.plot(xtick = date_ticks)
 
     # Plot bands
     band_percents = DSGE.which_density_bands(combined; uniquify = true)
     for pct in band_percents
-        plot!(p, dates, combined.bands[var][Symbol("$pct UB")],
-              fillto = combined.bands[var][Symbol("$pct LB")],
+        plot!(p, datenums[all_inds], combined.bands[var][all_inds, Symbol("$pct UB")],
+              fillto = combined.bands[var][all_inds, Symbol("$pct LB")],
               label = "", color = forecast_band_color)
               # label = "", color = :blue, α = 0.10)
     end
 
     # Plot mean
-    plot!(p, dates[hist_inds],  combined.means[hist_inds,  var], label = hist_label,
+    plot!(p, datenums[hist_inds],  combined.means[hist_inds,  var], label = hist_label,
           linewidth = 2, linecolor = hist_color)
-    plot!(p, dates[fcast_inds], combined.means[fcast_inds, var], label = forecast_label,
+    plot!(p, datenums[fcast_inds], combined.means[fcast_inds, var], label = forecast_label,
           linewidth = 2, linecolor = forecast_mean_color)
 
     # Save if `output_file` provided
