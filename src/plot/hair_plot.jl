530ef8c18d19fa517fca98845e6adaa0545af5bc
Pearl Li <pearlzli16@gmail.com>
2017-05-03 16:25:29 -0400

    Add hair plot method taking in vector of initial values

--- src/plot/hair_plot.jl
+++ src/plot/hair_plot.jl
@@ -5,6 +5,21 @@ function hair_plot(var::Symbol, df::DataFrame,
                    output_file::AbstractString = "",
                    hist_label::AbstractString = "Realized",
                    forecast_label::AbstractString = "Forecasts",
+                   forecast_palette::Symbol = Symbol(),
+                   forecast_color::Colorant = RGBA(1., 0., 0., 1.))
+
+    initial_values = map(history -> history.means[end, var], histories)
+    hair_plot(var, df, initial_values, forecasts,
+              output_file = output_file, hist_label = hist_label, forecast_label = forecast_label,
+              forecast_palette = forecast_palette, forecast_color =forecast_color)
+end
+
+function hair_plot(var::Symbol, df::DataFrame,
+                   initial_values::Vector{Float64}, forecasts::Vector{MeansBands};
+                   output_file::AbstractString = "",
+                   hist_label::AbstractString = "Realized",
+                   forecast_label::AbstractString = "Forecasts",
+                   forecast_palette::Symbol = Symbol(),
                    forecast_color::Colorant = RGBA(1., 0., 0., 1.))
     # Dates
     dates      = map(quarter_date_to_number, df[:date])
@@ -20,12 +35,19 @@ function hair_plot(var::Symbol, df::DataFrame,
     plot!(p, dates, df[var], label = hist_label, linewidth = 2, linecolor = :black)
 
     # Plot each forecast
-    for (history, forecast) in zip(histories, forecasts)
-        dates = vcat([history.means[end, :date]], forecast.means[:date])
+    for (initial_value, forecast) in zip(initial_values, forecasts)
+        date_0 = DSGE.iterate_quarters(forecast.means[1, :date], -1)
+        dates = vcat([date_0], forecast.means[:date])
         dates = map(quarter_date_to_number, dates)
-        series = vcat([history.means[end, var]], forecast.means[var])
-        label = history == histories[1] ? forecast_label : ""
-        plot!(p, dates, series, label = label, linewidth = 1, linecolor = forecast_color)
+
+        series = vcat([initial_value], forecast.means[var])
+
+        label = forecast == forecasts[1] ? forecast_label : ""
+        if forecast_palette == Symbol()
+            plot!(p, dates, series, label = label, linewidth = 1, linecolor = forecast_color)
+        else
+            plot!(p, dates, series, label = label, linewidth = 1, palette = forecast_palette)
+        end
     end
 
     # Save if `output_file` provided
