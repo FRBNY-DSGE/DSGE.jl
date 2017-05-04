1ae1e49f6c5f98982a9e1989fd54320d91f97838
Pearl Li <pearlzli16@gmail.com>
2017-05-04 13:51:43 -0400

    Add end_date and tick_size to plot_history_and_forecast

--- src/plot/plot_history_and_forecast.jl
+++ src/plot/plot_history_and_forecast.jl
@@ -2,40 +2,41 @@ using DSGE, Plots
 
 function plot_history_and_forecast(var::Symbol, history::MeansBands, forecast::MeansBands;
                                    start_date::Nullable{Date} = Nullable{Date}(),
+                                   end_date::Nullable{Date} = Nullable{Date}(),
                                    output_file::AbstractString = "",
                                    hist_label::AbstractString = "History",
                                    forecast_label::AbstractString = "Forecast",
                                    hist_color::Colorant = RGBA(0., 0., 0., 1.),
                                    forecast_mean_color::Colorant = RGBA(1., 0., 0., 1.),
-                                   forecast_band_color::Colorant = RGBA(0., 0., 1., 0.1))
+                                   forecast_band_color::Colorant = RGBA(0., 0., 1., 0.1),
+                                   tick_size::Int = 5)
     # Concatenate MeansBands
     combined = cat(history, forecast)
 
     # Dates
-    datenums   = map(quarter_date_to_number, df[:date])
-    start_ind  = if !isnull(start_date)
-        @assert history.means[1, :date] <= get(start_date) <= history.means[end, :date]
-        findfirst(combined.means[:date], get(start_date))
-    else
-        1
-    end
-    date_ticks = get_date_ticks(combined.means[start_ind:end, :date])
+    start_date, end_date = get_date_limits(start_date, end_date, combined.means[:date])
+    start_ind,  end_ind  = get_date_limit_indices(start_date, end_date, combined.means[:date])
+    datenums             = map(quarter_date_to_number, combined.means[:date])
 
-    hist_inds  = start_ind:size(history.means, 1)
-    fcast_inds = size(history.means, 1):size(combined.means, 1)
-    all_inds   = start_ind:size(combined.means, 1)
+    # Indices
+    n_hist_periods  = size(history.means,  1)
+    n_fcast_periods = size(forecast.means, 1)
+    n_all_periods   = n_hist_periods + n_fcast_periods
 
-   # Initialize GR backend
+    hist_inds  = start_ind:min(end_ind, n_hist_periods)
+    fcast_inds = max(start_ind, n_hist_periods):end_ind
+    all_inds   = start_ind:end_ind
+
+    # Initialize GR backend
     gr()
-    p = Plots.plot(xtick = date_ticks)
+    p = Plots.plot()
 
     # Plot bands
     band_percents = DSGE.which_density_bands(combined; uniquify = true)
     for pct in band_percents
         plot!(p, datenums[all_inds], combined.bands[var][all_inds, Symbol("$pct UB")],
               fillto = combined.bands[var][all_inds, Symbol("$pct LB")],
-              label = "", color = forecast_band_color)
-              # label = "", color = :blue, α = 0.10)
+              label = "", color = forecast_band_color, α = 0.10)
     end
 
     # Plot mean
@@ -44,7 +45,14 @@ function plot_history_and_forecast(var::Symbol, history::MeansBands, forecast::M
     plot!(p, datenums[fcast_inds], combined.means[fcast_inds, var], label = forecast_label,
           linewidth = 2, linecolor = forecast_mean_color)
 
-    # Save if `output_file` provided
+    # Set x-axis limits and ticks
+    # xlims attribute only sets finite values, e.g. (-Inf, 2) sets only the right limit
+    t0 = isnull(start_date) ? -Inf : quarter_date_to_number(get(start_date))
+    t1 = isnull(end_date)   ?  Inf : quarter_date_to_number(get(end_date))
+    date_ticks = get_date_ticks(get(start_date), get(end_date), tick_size = tick_size)
+    xaxis!(p, xlims = (t0, t1), xtick = date_ticks)
+
+    # Save if output_file provided
     if !isempty(output_file)
         output_dir = dirname(output_file)
         !isdir(output_dir) && mkdir(output_dir)
--- src/plot/util.jl
+++ src/plot/util.jl
@@ -12,10 +12,56 @@ function quarter_date_to_number(date::Date)
     end
 end
 
-function get_date_ticks(dates::AbstractArray{Date, 1})
+function get_date_ticks(start_date::Date, end_date::Date;
+                        tick_size::Int = 5)
+    dates = DSGE.quarter_range(start_date, end_date)
+    get_date_ticks(dates, tick_size = tick_size)
+end
+
+function get_date_ticks(dates::AbstractArray{Date, 1};
+                        tick_size::Int = 5)
     datenums = map(quarter_date_to_number, dates)
-    t0 = ceil(datenums[start_ind] / 5) * 5
+    t0 = ceil(datenums[1] / tick_size) * tick_size
     t1 = datenums[end]
-    ticks = t0:5:t1
+    ticks = t0:tick_size:t1
     return ticks
+end
+
+function get_date_limits(start_date::Nullable{Date}, end_date::Nullable{Date},
+                         dates::AbstractArray{Date, 1})
+    if isnull(start_date)
+        start_date = Nullable(dates[1])
+    end
+    if isnull(end_date)
+        end_date = Nullable(dates[end])
+    end
+
+    return start_date, end_date
+end
+
+function get_date_limit_indices(start_date::Nullable{Date}, end_date::Nullable{Date},
+                                dates::AbstractArray{Date, 1})
+    start_ind = if isnull(start_date)
+        1
+    else
+        if dates[1] <= get(start_date) <= dates[end]
+            findfirst(dates, get(start_date))
+        elseif get(start_date) < dates[1]
+            1
+        else
+            error("start_date $(get(start_date)) cannot be after last forecast period $(dates[end])")
+        end
+    end
+    end_ind = if isnull(end_date)
+        length(dates)
+    else
+        if dates[1] <= get(end_date) <= dates[end]
+            findfirst(dates, get(end_date))
+        elseif get(end_date) > dates[end]
+            length(dates)
+        else
+            error("end_date $(get(end_date)) cannot be before first historical period $(dates[1])")
+        end
+    end
+    return start_ind, end_ind
 end
