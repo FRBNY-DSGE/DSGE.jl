667a44c211eb08a766123f0c41751abd98579247
Abhi Gupta <abhig94@gmail.com>
2017-06-16 15:46:02 -0400

    cleanup to smc settings, mutation

--- src/estimate/systematic_resampling.jl
+++ src/estimate/systematic_resampling.jl
@@ -23,42 +23,42 @@ function systematic_resampling(m, weight)
     cweight = cumsum(weight')
     uu = zeros(npart,1)
     csi=rand()
-    
+
     for j=1:npart
         uu[j] = (j-1)+csi
     end
-    
+
     indx = zeros(npart, 1)
 
     function subsys(i)
         u = uu[i]/npart
         j=1
         while j <= npart
-            if (u < cweight[j]) 
+            if (u < cweight[j])
                 break
             end
             j = j+1
         end
         indx[i] = j
     end
-    
+
     parallel = get_setting(m,:use_parallel_workers)
     if parallel
-        parindx = @sync @parallel (hcat) for j = 1:npart 
+        parindx = @sync @parallel (hcat) for j = 1:npart
             subsys(j)
         end
     else
         parindx = [subsys(j) for j = 1:npart]'
     end
     indx = parindx'
-    
+
     indx = round(Int, indx)
-    
+
     if m.testing
         open("resamples.csv","a") do x
             writecsv(x,indx')
-        end 
+        end
     end
-    
+
     return vec(indx)
 end
