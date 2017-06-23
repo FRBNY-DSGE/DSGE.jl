30d22bbe3fafada8615db9f4c0d22423819e598c
Ethan Matlin <ethanmatlin@gmail.com>
2017-06-23 17:39:24 -0400

    deleted two unncessary files

--- src/estimate/multinomial_resampling1.jl
+++
@@ -1,9 +0,0 @@
-using StatsBase
-
-function multinomial_resampling1(m,weight)
-    numParticles=length(weight)
-    idx=sample(collect(1:numParticles), weights(weight), numParticles, replace=true)
-    return idx
-end
-
-println(multinomial_resampling1(1, [.2,.2,.3,.3, .1]))
