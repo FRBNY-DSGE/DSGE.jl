dcb28bc35ab8d5e994cc17d0f3f10589c99f7b37
Ethan Matlin <ethanmatlin@gmail.com>
2017-06-23 12:31:58 -0400

    a simple test that compares matlab output to julia output

--- src/estimate/mutation.jl
+++ src/estimate/mutation.jl
@@ -8,35 +8,40 @@ function mutation(m::AbstractModel, yt::Array{Float64,1},s_init::Array{Float64,1
 
     #number of metropolis-hastings steps. this will eventually be a setting, but hard-coded for now. Should be low.
     N_MH=1
-   
+   #Set path
+    path = dirname(@__FILE__)
     #solve for the matrices in the state equation (using TPF paper notation, not standard)
     sys=compute_system(m)
     R=sys.transition.RRR
     φ=sys.transition.TTT
     C=sys.transition.CCC
     H=sys.measurement.EE
-    
+    ind_s=0
+    ind_ε=0
     #varStateEq is the covariance matrix of the state equation(should change name. TPF paper calls it S2 but probably want to call it something different.
     varStateEq=sys.measurement.MM*sys.measurement.QQ*sys.measurement.MM'
     #Get the meta-paramater c from model settings (I think we want a separate c for this part rather than what is used for regular MH but can change later)
     c=get_setting(m,:c)
     #Convert the varStateEq matrix into a covariance matrix of the state error
-    println(R)
-    println(H)
-    println(chol(H))
-    cov_mat=R*chol(H)'
-    println(cov_mat)
-
+    U, E, V = svd(varStateEq)
+    cov_mat = U * diagm(sqrt(E))
+    
+    sqrtS2 = R*cov_mat'
     #initialize acceptance counter to zero
     acpt=0
     
     for i=1:N_MH
+        #Isolate random matrix for testing purposes
+        randMat = randn(size(cov_mat,1),1)
+        h5open("$path/../../test/reference/mutationRandomMatrix.h5","w") do file
+            write(file, "randMat", randMat)
+        end
         #Generate new draw of ε from a N(ε_init, c²cov_mat) distribution
-        ε_new=ε_init + c*cov_mat'*randn(size(cov_mat,1),1)
+        ε_new=ε_init + c*cov_mat'*randMat
         #Use the state equation to calculate the corresponding state from that ε 
-        s_new_fore = φ*s_init+R*varStateEq*ε_new
+        s_new_fore = φ*s_init+sqrtS2*ε_new
         #Use the state equation to calculate the state corresponding to ε_init
-        s_init_fore = φ*s_init+R*varStateEq*ε_init
+        s_init_fore = φ*s_init+sqrtS2*ε_init
 
         #Calculate difference between data and expected y from measurement equation and calculated states from above for both the new draw of ε_new and the old ε_init (we do this to calculate probabilities below. Since the error is still drawn from a Normal and everything is still linear, we know that y will also be normal. See equation of multivariate normal for how error_new and error_init enter into the pdf).
         error_new = yt-sys.measurement.ZZ*s_new_fore-sys.measurement.DD
@@ -49,14 +54,19 @@ function mutation(m::AbstractModel, yt::Array{Float64,1},s_init::Array{Float64,1
         α = exp(post_new - post_init)
         
         #Accept the particle with probability α (rand() generates Unif(0,1) r.v. If accept set s_init to the particle and ε_init to the error for starting the loop over again
+       
         if rand()<α 
             #accept
-            s_init = s_new_fore
-            ε_init = ε_new
+            ind_s = s_new_fore
+            ind_ε = ε_new
             acpt = acpt+1
-        #else reject and keep the old particle unchanged
+        else 
+            #reject and keep the old particle unchanged
+            ind_s = s_init_fore
+            ind_ε = ε_init
+            #acpt = 0
         end
     end
-    return s_init, ε_init, acpt 
+    return ind_s, ind_ε, acpt 
 end
 
--- test/estimate/mutation.jl
+++ test/estimate/mutation.jl
@@ -2,11 +2,25 @@ using DSGE,DataFrames,HDF5
 
 m=AnSchorfheide(testing=true)
 m<=Setting(:date_forecast_start,quartertodate("2015-Q4"))
+srand(1234)
+path=dirname(@__FILE__)
+sys=compute_system(m)
+h5open("$path/../reference/matricesForMutation.h5","w") do file
+    write(file, "phi", sys.transition.TTT)
+    write(file, "D", sys.measurement.DD)
+    write(file, "R", sys.transition.RRR)
+    write(file, "H", sys.measurement.EE)
+    write(file, "C", sys.transition.CCC)
+    write(file, "M", sys.measurement.MM)
+    write(file, "Q", sys.measurement.QQ)
+    write(file, "Z", sys.measurement.ZZ)
+end
+    
 
 #test it. it works!
 s_part1, eps_part1, acpt = mutation(m,[50.2,8.3,7.6],[.8,.9,.6,.9,.11,5,7,10],[.2,.5,.7])
 
-path=dirname(@__FILE__)
+
 h5open("$path/../reference/mutation_RWMH1.h5","w") do file
     write(file, "s_part1", s_part1)
     write(file, "eps_part1",eps_part1)
@@ -16,19 +30,16 @@ data=h5open("$path/../reference/mutation_RWMH.h5","r") do file
     read(file,"data")
 end
 
-println(data)
-println(size(data)) #3x230
 
 
 #test it with one column of data. it works!
-s,eps,acpt=mutation(m,data[:,1],[.8,.9,.6,.9,.11,5,7,10],[.2,.5,.7])
-   
-
+#s,eps,acpt=mutation(m,data[:,1],[.8,.9,.6,.9,.11,5,7,10],[.2,.5,.7])
+ s, eps, acpt = mutation(m, data[:,1],ones(8), zeros(3))  
+print(s)
 c = h5open("$path/../reference/mutation_RWMH1.h5","r") do file
     read(file,"s_part1")
     read(file,"eps_part1")
 end
-println(c)
 
 #path=dirname(@__FILE__)
 #acc_log1h= h5read("$path/../reference/mutation_RWMH.h5","acc_log1h")
