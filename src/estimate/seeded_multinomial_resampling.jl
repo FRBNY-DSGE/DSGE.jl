4923462db200251efa03749d41a1f8cbfc699d2d
Ethan Matlin <ethanmatlin@gmail.com>
2017-07-03 16:59:26 -0400

    lot of debugging. fixed some issues, still not totally precise. as of now we attribute remaining errors to issues with the numerical solver being inconsistent between matlab and julia and due to rounding errors particularly with cov_s (and the binary choice between SPD and diagonalizing)

--- src/estimate/seeded_multinomial_resampling.jl
+++ src/estimate/seeded_multinomial_resampling.jl
@@ -6,13 +6,20 @@ function seeded_multinomial_resampling(weight::Array{Float64,1})
     sorted_idx = sortperm(weight,rev=true)
     out_weight::Array{Int64, 1}=zeros(num_particles)
     i=k=is_first=1
-
+    still_on_first=1
     while i<=num_particles
         if (weight[sorted_idx[k]]>0.0) | (is_first==1)
             weight[sorted_idx[k]]-=1
             out_weight[i]=Integer(sorted_idx[k])
             i+=1
             is_first=0
+            if k>1
+                still_on_first = 0
+            end
+            if (still_on_first == 1) & (i == round(num_particles*0.75))
+                k += 1
+                is_first=1
+            end
         else
             k+=1
             is_first=1
