1e582512a7f1fea9e961fba5b25303ddce1d9da5
Ethan Matlin <ethanmatlin@gmail.com>
2017-07-07 17:33:14 -0400

    it works! with full randomness and the way it should be run (fixed by using optimize to get better paramaters to start with)--broke before because after a while every particle was super unlikely because they started with bad params

--- test/estimate/tpf.jl
+++ test/estimate/tpf.jl
@@ -1,14 +1,9 @@
 using DSGE, HDF5, DataFrames, ClusterManagers
 using QuantEcon: solve_discrete_lyapunov
 
-m = AnSchorfheide(testing=true)
-m<=Setting(:data_forecast_start,quartertodate("2015-Q4"))
-m<=Setting(:tpf_rstar,2.0)
-m<=Setting(:tpf_c,0.1)
-m<=Setting(:tpf_acpt_rate,0.5)
-m<=Setting(:tpf_trgt,0.25)
-m<=Setting(:tpf_N_MH,2)
-m<=Setting(:tpf_n_particles,4000)
+custom_settings = Dict{Symbol, Setting}(
+    :date_forecast_start => Setting(:date_forecast_start, quartertodate("2015-Q4")))
+m = AnSchorfheide(custom_settings = custom_settings, testing=true)
 
 srand(1234)
 
@@ -63,20 +58,47 @@ m<=Setting(:TTT,Φ)
 m<=Setting(:EE,H)
 m<=Setting(:tpf_S2,S2)
 
+#get better starting parameters to that the whole code runs
+file = "$path/../reference/optimize.h5"
+x0 = h5read(file,"params")
+data = h5read(file, "data")'
+minimizer = h5read(file,"minimizer")
+minimum = h5read(file,"minimum")
+H_expected = h5read(file,"H")
+
+update!(m,x0)
+n_iterations = 3
+
+x0=Float64[p.value for p in m.parameters]
+out, H = optimize!(m,data; iterations=500)
+
+params = out.minimizer
+
+update!(m,params)
+
+m<=Setting(:tpf_rstar,2.0)
+m<=Setting(:tpf_c,0.1)
+m<=Setting(:tpf_acpt_rate,0.5)
+m<=Setting(:tpf_trgt,0.25)
+m<=Setting(:tpf_N_MH,2)
+m<=Setting(:tpf_n_particles,500)
+
+
 s0 = zeros(8)
 P0=nearestSPD(solve_discrete_lyapunov(Φ, R*S2*R'))
-df = readtable("$path/../../../../../us.txt",header=false, separator=' ')
-data = convert(Matrix{Float64},df)
-data=data'
+#df = readtable("$path/../../../../../us.txt",header=false, separator=' ')
+#data = convert(Matrix{Float64},df)
+#data=data'
 
 tic()
-neff, lik = tpf(m, data, s0,P0, testing=0, parallel=0)
+neff, lik = tpf(m, data, s0,P0, testing=0, parallel=1)
 toc()
 
 #neff, lik = tpf(m, data, s0,P0, testing=0, parallel=0)
 
 #neff, lik = tpf(m, data, s0, P0, testing=1, parallel=0)
 
+#This test only passes if n_particles=4000 and testing=1
 @test good_likelihoods == lik
 
 h5open("$path/../reference/output_likelihoods.h5","w") do file
