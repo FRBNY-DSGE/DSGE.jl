b8c2d2a77917e9094f18b32af26c11b2320aab19
Pearl Li <pearlzli16@gmail.com>
2017-07-10 18:30:25 -0400

    Refactor individual IRF plot to separate function

--- src/plot/plot_irfs.jl
+++ src/plot/plot_irfs.jl
@@ -1,63 +1,93 @@
 """
 ```
-plot_irfs(shock, vars, mb; bands = [\"90.0%\"], output_file = "")
+plot_irfs(shock, var, mb; bands = [\"90.0\"], output_file = "", title = "")
+
+plot_irfs(shock, vars, mb; bands = [\"90.0%\"], output_file = "", titles = [],
+    layout = Plots.EmptyLayout())
 ```
 
-Plots the responses of `vars` to `shock`, given a `MeansBands` object `mb`. By
-default, only 90% bands are plotted.
+Plots the responses of either a single `var` (method 1) or all provided `vars`
+(method 2) to `shock`, given a `MeansBands` object `mb`. By default, only 90%
+bands are plotted. In method 2, a single plot with subplots for each variable is
+returned and/or saved.
 
 ### Inputs
 
 - `shock::Symbol`: e.g. `:g_sh`
-- `vars::Vector{Symbol}`: response variables, e.g. `[:obs_gdp, :obs_corepce]`
+- `var::Symbol` or `vars::Vector{Symbol}`: response variable(s), e.g. `:obs_gdp`
 - `mb::MeansBands`
 
 ### Keyword Arguments
 
 - `bands::Vector{String}`
-- `output_file::String`: if `output_file` is provided in the form \"base.ext\",
-  the individual plots will also each be saved as \"base__var.ext\"
+- `output_file::String`: if specified, plot will be saved there as a PDF
+- `title::String` or `titles::Vector{String}`
+- `layout::Plots.AbstractLayout` (method 2 only)
 
 ### Output
 
-- `allplots::OrderedDict{Symbol, Plot}`: indexed by variable name
+- `p::Plot`
 """
+function plot_irfs(shock::Symbol, var::Symbol, mb::MeansBands;
+                   bands::Vector{String} = ["90.0%"],
+                   output_file::String = "",
+                   title::String = "")
+
+    # Plot title
+    if isempty(title)
+        title = string(var)
+    end
+    p = plot(title = title, margin = 10px)
+
+    varshock = Symbol("$(var)__$(shock)")
+
+    # Plot 90% bands
+    for pct in bands
+        plot!(p, mb.bands[varshock][Symbol("$pct UB")],
+              fillto = mb.bands[varshock][Symbol("$pct LB")],
+              label = "", color = :blue, α = 0.10)
+    end
+
+    # Plot mean
+    plot!(p, mb.means[varshock], label = "", linewidth = 2, linecolor = :black)
+
+    # Save if `output_file` provided
+    if !isempty(output_file)
+        save_plot(p, output_file)
+    end
+
+    return p
+end
+
 function plot_irfs(shock::Symbol, vars::Vector{Symbol}, mb::MeansBands;
                    bands::Vector{String} = ["90.0%"],
-                   output_file::String = "")
-    # Split `output_file` into base and extension
-    if !isempty(output_file)
-        strs = split(output_file, ".")
-        base = join(strs[1:end-1], ".")
-        ext  = "." * strs[end]
-        save_plots = true
+                   output_file::String = "",
+                   titles::Vector{String} = Vector{String}(),
+                   layout::Plots.AbstractLayout = Plots.EmptyLayout())
+
+    # Determine title
+    if isempty(titles)
+        titles = map(string, vars)
     end
 
-    allplots = OrderedDict{Symbol, Plots.Plot}()
+    plots = OrderedDict{Symbol, Plots.Plot}()
 
     # Iterate through variables
-    for var in vars
-        p = plot(title = string(var), margin = 10px)
-        varshock = Symbol("$(var)__$(shock)")
-
-        # Plot 90% bands
-        for pct in bands
-            plot!(p, mb.bands[varshock][Symbol("$pct UB")],
-                  fillto = mb.bands[varshock][Symbol("$pct LB")],
-                  label = "", color = :blue, α = 0.10)
-        end
-
-        # Plot mean
-        plot!(p, mb.means[varshock], label = "", linewidth = 2, linecolor = :black)
-
-        # Save if `output_file` provided
-        if save_plots
-            this_file = base * "__$var" * ext
-            save_plot(p, this_file)
-        end
-
-        allplots[var] = p
+    for (var, title) in zip(vars, titles)
+        plots[var] = plot_irfs(shock, var, mb, bands = bands, title = title)
     end
 
-    return allplots
+    # Plot all variables together
+    p = if isa(layout, Plots.EmptyLayout)
+        plot(collect(values(plots))...)
+    else
+        plot(collect(values(plots))..., layout = layout)
+    end
+
+    # Save if `output_file` provided
+    if !isempty(output_file)
+        save_plot(p, output_file)
+    end
+
+    return p
 end
