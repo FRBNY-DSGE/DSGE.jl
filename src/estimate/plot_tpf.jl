e527e88dd15ccc0409f16874387ff34f985d451f
Ethan Matlin <ethanmatlin@gmail.com>
2017-07-12 17:20:58 -0400

    rearranged tpf test to make more organized, moved plotting into a separate file/method in estimate folder (plot_tpf.jl)

---
+++ src/estimate/plot_tpf.jl
@@ -0,0 +1,123 @@
+function plot_tpf()
+ #   using DSGE, HDF5, DataFrames, Plots
+ #   using QuantEcon: solve_discrete_lyapunov
+
+    m = AnSchorfheide(testing=true)
+    path = dirname(@__FILE__)
+
+    m<=Setting(:tpf_rstar,2.0)
+    m<=Setting(:tpf_N_MH, 2)
+    m<=Setting(:tpf_c,0.1)
+    m<=Setting(:tpf_acpt_rate,0.5)
+    m<=Setting(:tpf_trgt,0.25)
+    n_particles=500
+    m<=Setting(:tpf_n_particles,n_particles)
+    m<=Setting(:use_parallel_workers, true)
+    m<=Setting(:x_tolerance, 1e-3)
+    
+    srand(1234)
+    df = readtable("$path/../../test/reference/us.txt",header=false,separator=' ')
+    data=convert(Matrix{Float64},df)
+    data=data'
+    file = "$path/../../test/reference/matlab_variable_for_testing.h5"
+    A = h5read(file,"A")
+    B = h5read(file, "B")
+    H = h5read(file, "H")
+    R = h5read(file, "R")
+    S2 = h5read(file, "S2")
+    Φ = h5read(file, "Phi")
+    m<=Setting(:DD,A[:,1])
+    m<=Setting(:ZZ,B)
+    m<=Setting(:RRR,R)
+    m<=Setting(:TTT,Φ)
+    m<=Setting(:EE,H)
+    m<=Setting(:tpf_S2,S2)
+    C= zeros(8,1)
+    m<=Setting(:CCC,C)
+    params=[2.09,0.98,2.25,0.65,0.34,3.16,0.51,0.81,0.98,0.93,0.19,0.65,0.24,0.12,0.29,0.45]
+    update!(m,params)
+    s0=zeros(8)
+    P0 = nearestSPD(solve_discrete_lyapunov(Φ, R*S2*R'))
+     
+
+    neff, lik = tpf(m,data,s0,P0,testing=true)
+    h5open("$path/../../test/reference/output_likelihoods_500_testing.h5","w") do file
+        write(file, "julia_likelihoods",lik)
+    end
+    
+    #4000 particles. testing
+    n_particles = 4000
+    m<=Setting(:tpf_n_particles,n_particles)
+    neff,lik = tpf(m,data,s0,P0,testing=true)
+    h5open("$path/../../test/reference/output_likelihoods_4000_testing.h5","w") do file
+        write(file,"julia_likelihoods",lik)
+    end
+    
+    #4000 particles. no testing]
+    df = readtable("$path/../../test/reference/us.txt", header=false, separator=' ')
+    data = convert(Matrix{Float64},df)
+    data=data'
+    
+    file = "$path/../../test/reference/matlab_variable_for_testing.h5"
+    A = h5open(file, "r") do file
+        read(file,"A")
+    end
+    B = h5open(file,"r") do file
+        read(file,"B")
+    end
+    H = h5open(file,"r") do file
+        read(file,"H")
+    end
+    R = h5open(file,"r") do file
+        read(file,"R")
+    end
+    S2 = h5open(file,"r") do file
+        read(file,"S2")
+    end
+    Φ = h5open(file,"r") do file
+        read(file, "Phi")
+    end
+    m<=Setting(:DD,A[:,1])
+    m<=Setting(:ZZ,B)
+    m<=Setting(:RRR,R)
+    m<=Setting(:TTT,Φ)
+    m<=Setting(:EE,H)
+    m<=Setting(:tpf_S2,S2)
+    C = zeros(8,1)
+    m<=Setting(:CCC,C)
+    params=[2.09,0.98,2.25,0.65,0.34,3.16,0.51,0.81,0.98,0.93,0.19,0.65,0.24,0.12,0.29,0.45]
+    update!(m,params)
+    
+    neff,lik = tpf(m,data,s0,P0,testing=false)
+    h5open("$path/../../test/reference/output_likelihoods_4000_notesting.h5","w") do file
+        write(file,"julia_likelihoods",lik)
+    end
+    
+    #Plot run-time vs. number of particles
+    sys = compute_system(m)
+    R = sys.transition.RRR
+    S2 = sys.measurement.QQ
+    Φ = sys.transition.TTT
+    file = "$path/../../test/reference/optimize.h5"
+    x0 = h5read(file,"params")
+    data = h5read(file,"data")
+    miniimzer = h5read(file,"data")
+    minimizer = h5read(file, "minimizer")
+    update!(m,x0)
+    x0 = Float64[p.value for p in m.parameters]
+    out,H = optimize!(m,data;iterations=500)
+    params = out.minimizer
+    @show params
+    update!(m,params)
+    s0 = zeros(8)
+    P0 = nearestSPD(solve_discrete_lyapunov(Φ, R*S2*R'))
+                    
+    times = []
+    ns = [100,300,500,700,900,1100,1300,1500,1700,1900,2100,4000]
+    
+    times = pmap(n->time(m,data,s0,P0,false,n),ns)
+    @show times
+    plotly()
+    plot(ns,times)
+    gui()
+end
