56f85cbfa07cf7eefda5d2cd8383c250ca6749c4
Pearl Li <pearlzli16@gmail.com>
2017-07-18 16:35:29 -0400

    Update run_default with forecast_one and means_bands_all

--- docs/examples/run_default.jl
+++ docs/examples/run_default.jl
@@ -1,12 +1,11 @@
 using DSGE
 
-# estimate as of 2015-Q3 using the default data vintage from 2015 Nov 27
-custom_settings = Dict{Symbol, Setting}(
-    :data_vintage        => Setting(:data_vintage, "151127"),
-    :date_forecast_start => Setting(:date_forecast_start, quartertodate("2015-Q4")))
-
 # construct a model object
 m = Model990()
+
+# estimate as of 2015-Q3 using the default data vintage from 2015 Nov 27
+m <= Setting(:data_vintage, "151127")
+m <= Setting(:date_forecast_start, quartertodate("2015-Q4"))
 
 # reoptimize parameter vector, compute Hessian at mode, and full posterior
 # parameter sampling
@@ -15,11 +14,16 @@ estimate(m)
 # produce LaTeX tables of parameter moments
 compute_moments(m)
 
-# forecast using 10 processes
+# forecast full distribution using 10 processes
 my_procs = addprocs(10)
 @everywhere using DSGE
-
 m <= Setting(:use_parallel_workers, true)
-output_vars = get_output_vars(m, :simple)
-forecast_all(m, [:none, :semi, :full], [:mode, :full], output_vars; procs = my_procs)
+
+# compute smoothed historical states, forecasted states and observables
+output_vars = [:histstates, :forecaststates, :forecastobs]
+forecast_one(m, :full, :none, output_vars)
+
+# compute means and bands
+means_bands_all(m, :full, :none, output_vars)
+
 rmprocs(my_procs)
