333e96d1f97fa60a46853e168e285aac0fa40039
Michael Cai <michaeldcai@gmail.com>
2017-07-19 10:32:24 -0400

    completed mutation blocking

--- src/estimate/mutation_block_RWMH.jl
+++ src/estimate/mutation_block_RWMH.jl
@@ -32,7 +32,7 @@ Execute one proposed move of the Metropolis-Hastings algorithm for a given param
 - `accept`: Indicator for acceptance or rejection (0 reject, 1 accept)
 
 """
-function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init::Array{Float64,1}, like_init::Float64, post_init::Float64, i::Int64, R::Array{Float64,2}, tempering_schedule::Array{Float64,1}; rvec = [], rval = [])
+function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init::Array{Float64,1}, like_init::Float64, post_init::Float64, i::Int64, R::Array{Float64,2}, tempering_schedule::Array{Float64,1}; rvec = [], rval = [], rstep = [])
 
     n_Φ = get_setting(m, :n_Φ)
     n_part = get_setting(m, :n_particles)
@@ -41,13 +41,13 @@ function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init:
     n_steps = get_setting(m, :n_MH_steps_smc)
 
     fixed_para_inds = find([ θ.fixed for θ in m.parameters])
-    free_para_inds  = find([!θ.fixed for θ in m.parameters])
     c = get_setting(m, :step_size_smc)
     accept = get_setting(m, :init_accept)
     target = get_setting(m, :target_accept)
 
     # draw initial step and step probability
-    step = isempty(rvec) ? randn(n_para,1): rvec
+    # conditions for testing purposes
+    step = isempty(rstep) ? (isempty(rvec) ? randn(n_para,1): rvec) : rstep
     step_prob = isempty(rval) ? rand() : rval
 
     # Solve for covariance and zero-out variables that are fixed
@@ -64,77 +64,69 @@ function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init:
 ### BLOCKING ###
 
     # Generate random index for each parameter, as means of sorting
-    para_inds = zeros(n_para)
-    for i=1:n_para
-        para_inds[i] = rand()
+    if m.testing
+        para_inds = collect(1:n_para) 
+    else
+        para_inds = zeros(n_para)
+        for j=1:n_para
+            para_inds[j] = rand()
+        end
+        # Returns indices sorted by their random value
+        para_inds = sortperm(para_inds)
     end
-    # Returns indices sorted by their random value
-    para_inds = sortperm(para_inds)
-    
-### BLOCKING ###
-    n_blocks = 4
+
+    # Find the indices for the lower and upper bounds on each block
     b = 1
-    while b <= n_blocks # corresponds to jth block
-        
-        # If b <= 1, then blocking procedure does not occur. 
-        if b >= 1
-            cov_mat_temp = isolate_block(cov_mat, b, n_blocks, para_inds)            
-            para_new = para + c * cov_mat_temp * step
-            para_new2 = para + c*cov_mat*step
-        else
-            para_new = para + c * cov_mat * step
-        end
-        
-        @show (para_new)
-        @show size(cov_mat)
-        @show para_new2
-        @show size(augment_draw(para_new,m))
-        post_new = posterior!(m, para_new, data; 
-                              φ_smc = tempering_schedule[i],
-                              sampler = true)
-        like_new = post_new - prior(m)
-
-        # if step is invalid, retry
-        if !isfinite(post_new)
-            #j -= 1
-            b -= 1
-        end
-
-        # Accept/Reject
-        α = exp(post_new - post) # this is RW, so q is canceled out
-
-        if step_prob < α # accept
-            para   = para_new
-            like   = like_new
-            post   = post_new
-            accept = 1
-        end
-
-        # draw again for the next step
-        step = randn(n_para,1)
-        step_prob = rand()
-        b += 1  
+    blo = Array{Int64}(n_blocks)
+    bhi = Array{Int64}(n_blocks)
+    for b in 1:n_blocks
+        blo[b] = convert(Int64,max(0,ceil((b-1)*n_para/n_blocks)))
+        bhi[b] = convert(Int64,min(n_para+1,ceil(b*n_para/n_blocks)+1))
     end
-    return augment_draw(para, m), like, post, accept
-end
 
-# Zeros-out the lines of the matrices not in block
-function isolate_block(cov_mat::AbstractArray, b, n_blocks, para_inds::AbstractArray)
-    cov_mat_block = cov_mat
-    n_para = length(para_inds)
-    sup = min(b*n_blocks,n_para)
-    inf = max(1,(b-1)*n_blocks)
-    for i = 1:inf
-        inds = para_inds[i]
-        cov_mat_block[:,inds] = zeros(n_para)
-        cov_mat_block[inds,:] = zeros(n_para)
+    #assuming blocks > 1
+    for (j,k) in zip(blo,bhi)
+        #Zeroing out relevant rows in cov matrix
+        cov_mat_block = copy(cov_mat)
+        blo_inds = para_inds[1:j]
+        bhi_inds = para_inds[k:end]
+        cov_mat_block[:,blo_inds] = zeros(size(cov_mat_block[:,blo_inds]))
+        cov_mat_block[blo_inds,:] = zeros(size(cov_mat_block[blo_inds,:]))
+        cov_mat_block[:,bhi_inds] = zeros(size(cov_mat_block[:,bhi_inds]))
+        cov_mat_block[bhi_inds,:] = zeros(size(cov_mat_block[bhi_inds,:]))
+
+        l = 0
+        while l < n_steps
+            para_new = para+c*cov_mat_block*step
+            post_new = posterior!(m, vec(para_new), data; 
+                                  φ_smc = tempering_schedule[i],
+                                  sampler = true)
+            like_new = post_new - prior(m)
+
+            # if step is invalid, retry
+            if !isfinite(post_new) && isempty(rstep)
+                step = randn(n_para,1)
+                step_prob = rand()
+                continue
+            end
+
+            # Accept/Reject
+            α = isempty(rstep) ? exp(post_new - post): 1 # this is RW, so q is canceled out
+
+            if step_prob < α # accept
+                para   = para_new
+                like   = like_new
+                post   = post_new
+                accept = 1
+            end
+
+            # draw again for the next step
+            step = isempty(rstep) ? randn(n_para,1): step.+1
+            step_prob = rand()
+            l += 1
+        end
     end
-    for i = sup:n_para
-        inds = para_inds[i]
-        cov_mat_block[:,inds] = zeros(n_para)
-        cov_mat_block[inds,:] = zeros(n_para)
-    end
-    return cov_mat_block
+    return para, like, post, accept
 end
 
 # Function used at the start of program to zero-out fixed rows and columns
@@ -145,7 +137,6 @@ function augment_cov_mat(cov_mat,list_inds)
     end
     return cov_mat
 end
-
 
 # Not currently being called
 function augment_draw(draw, m)
