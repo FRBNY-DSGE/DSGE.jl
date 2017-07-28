4d64bae0192c2a381142576e5e9e84a2a14ad468
Reca Sarfati <rebecca_sarfati@brown.edu>
2017-07-28 15:12:58 -0400

    Cleaning code, fixing measurement error, plotting.

--- src/estimate/density.jl
+++ src/estimate/density.jl
@@ -6,10 +6,10 @@ Returns the appropriate probability (right now from a Normal distribution) evalu
 """
 
 function density(φ_new::Float64, φ_old::Float64, yt::Array{Float64,1}, perror::Array{Float64,1}, EE::Array{Float64,2};initialize::Int64=0)
-    #Non-initialization step (tempering and final iteration)
+    # Non-initialization step (tempering and final iteration)
     if initialize==0
         return (φ_new/φ_old)^(length(yt)/2)*exp(-1/2*perror'*(φ_new-φ_old)*inv(EE)*perror)[1]
-    #Initialiation step (using 2π instead of φ_old)
+    # Initialization step (using 2π instead of φ_old)
     else
         return ((φ_new/(2*pi))^(length(yt)/2)*(det(EE)^(-1/2))*exp((-1/2)*perror'*φ_new*inv(EE)*perror))[1]
     end
