85c8b23cd1a1329b23af1b59378c3f4854332b02
Pearl Li <pearlzli16@gmail.com>
2017-07-31 15:30:12 -0400

    Add model-specific plot_history_and_forecast method

--- src/plot/plot_history_and_forecast.jl
+++ src/plot/plot_history_and_forecast.jl
@@ -1,5 +1,13 @@
 """
 ```
+plot_history_and_forecast(m, var, class, input_type, cond_type;
+    forecast_string = "", bdd_and_unbdd = false, output_file = "",
+    start_date = Nullable{Date}(), end_date = Nullable{Date}(),
+    hist_label = \"History\", forecast_label = \"Forecast\",
+    hist_color = :black, forecast_mean_color = :red,
+    forecast_band_color = RGBA(0, 0, 1, 0.1), tick_size = 5, legend = :best,
+    plot_handle = plot())
+
 plot_history_and_forecast(var, history, forecast; output_file = "",
     start_date = Nullable{Date}(), end_date = Nullable{Date}(),
     hist_label = \"History\", forecast_label = \"Forecast\",
@@ -13,13 +21,22 @@ full-distribution forecast, the forecast will be a fan chart.
 
 ### Inputs
 
-- `var::Symbol`: e.g. `:obs_gdp`
+- `var::Symbol`: variable to be plotted, e.g. `:obs_gdp`
+
+**Method 1 only:**
+
+- `m::AbstractModel`
+- `class::Symbol`
+
+**Method 2 only:**
+
 - `history::MeansBands` or `Vector{MeansBands}`
 - `forecast::MeansBands` or `Vector{MeansBands}`
 
 ### Keyword Arguments
 
-- `output_file::String`: if specified, plot will be saved there as a PDF
+- `output_file::String`: if specified, plot will be saved there. In method 1, if
+  `output_file` is not specified, one will be computed using `get_forecast_filename`
 - `start_date::Nullable{Date}`
 - `end_date::Nullable{Date}`
 - `hist_label::String`
@@ -31,10 +48,52 @@ full-distribution forecast, the forecast will be a fan chart.
 - `legend`
 - `plot_handle::Plots.Plot`: a plot handle to add `history` and `forecast` to
 
+**Method 1 only:**
+
+- `forecast_string::String`
+- `bdd_and_unbdd::Bool`: if true, then unbounded means and bounded bands are plotted
+
 ### Output
 
 - `p::Plot`
 """
+function plot_history_and_forecast(m::AbstractModel, var::Symbol, class::Symbol,
+                                   input_type::Symbol, cond_type::Symbol;
+                                   forecast_string::String = "",
+                                   bdd_and_unbdd::Bool = false,
+                                   output_file::String = "",
+                                   start_date::Nullable{Date} = Nullable{Date}(),
+                                   end_date::Nullable{Date} = Nullable{Date}(),
+                                   hist_label::String = "History",
+                                   forecast_label::String = "Forecast",
+                                   hist_color::Colorant = RGBA(0., 0., 0., 1.),
+                                   forecast_mean_color::Colorant = RGBA(1., 0., 0., 1.),
+                                   forecast_band_color::Colorant = RGBA(0., 0., 1., 0.1),
+                                   tick_size::Int = 5,
+                                   legend = :best,
+                                   plot_handle::Plots.Plot = plot())
+    # Read in MeansBands
+    hist  = read_mb(m, input_type, cond_type, Symbol(:hist, class),
+                    forecast_string = forecast_string)
+    fcast = read_mb(m, input_type, cond_type, Symbol(:forecast, class),
+                    forecast_string = forecast_string, bdd_and_unbdd = bdd_and_unbdd)
+
+    # Get output file name if not provided
+    if isempty(output_file)
+        output_file = get_forecast_filename(m, input_type, cond_type, Symbol("forecast_", var),
+                                            pathfcn = figurespath, fileformat = plot_extension())
+    end
+
+    # Appeal to second method
+    plot_history_and_forecast(var, hist, fcast, output_file = output_file,
+                              start_date = start_date, end_date = end_date,
+                              hist_label = hist_label, forecast_label = forecast_label,
+                              hist_color = hist_color, forecast_mean_color = forecast_mean_color,
+                              forecast_band_color = forecast_band_color,
+                              tick_size = tick_size, legend = legend,
+                              plot_handle = plot_handle)
+end
+
 function plot_history_and_forecast(var::Symbol, history::MeansBands, forecast::MeansBands;
                                    output_file::String = "",
                                    start_date::Nullable{Date} = Nullable{Date}(),
