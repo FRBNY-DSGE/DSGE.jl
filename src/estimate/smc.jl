9df93591d30ef082c58bc96aa266a400f79e458e
Michael Cai <michaeldcai@gmail.com>
2017-08-03 10:08:33 -0400

    created initial_draw function

--- src/estimate/smc.jl
+++ src/estimate/smc.jl
@@ -15,6 +15,8 @@ smc(m::AbstractModel)
 	- `:none`: No status updates will be reported.
 	- `:low`: Status updates for SMC initialization and recursion will be included.
 	    - `:high`: Status updates for every iteration of SMC is output, which includes the mu and sigma of each individual parameter draw after each iteration, as well as the calculated acceptance rate, the ESS, and the number of times resampled.
+- `start_approx_post`: Have the initial parameters drawn from the approximated posterior (a degenerate multivariate normal
+centered at the mode).
 
 ### Outputs
 
@@ -49,24 +51,17 @@ function smc(m::AbstractModel, data::Matrix; verbose::Symbol=:low)
     c = get_setting(m, :step_size_smc)
     accept = get_setting(m, :init_accept)
     target = get_setting(m, :target_accept)
-    
-    draw_type = get_setting(m,:draw_type)
-    resampler(x) = if get_setting(m, :resampler_smc) == :systematic
-        systematic_resampling(m, x)
-    elseif get_setting(m, :resampler_smc) == :multinomial
-        sample(1:n_part, weights(x), n_part, replace = true)
-    else
-        throw("Invalid resampler in SMC. Set :resampler_smc setting to either :systematic or :multinomial")
-    end
 
     parallel = get_setting(m, :use_parallel_workers)
 
-    # Creating the tempering schedule
+    #Distribution that initial draws are generated from
+    dist_type = :prior
+    
+    #Creating the tempering schedule
     λ = get_setting(m, :λ)
     tempering_schedule = ((collect(1:1:n_Φ)-1)/(n_Φ-1)).^λ
 
     #Matrices for storing
-
     para_sim = zeros(n_Φ, n_part, n_params) # parameter draws
     weight_sim = zeros(n_part, n_Φ) # weights
     zhat = zeros(n_Φ, 1) # normalization constant
@@ -98,46 +93,12 @@ function smc(m::AbstractModel, data::Matrix; verbose::Symbol=:low)
         srand(42)
     end
 
-    ##Get parameters at mode and hessian
-    params = h5open(rawpath(m,"estimate", "paramsmode.h5"),"r") do file
-        read(file, "params")
-    end
-
-    # fn = hessian_path(m)
-    fn = rawpath(m,"estimate","hessian.h5")
-    hessian = h5open(fn,"r") do file
-        read(file, "hessian")
-    end
-
-    S_diag, U = eig(hessian)
-    big_eig_vals = find(x -> x>1e-6, S_diag)
-    rank = length(big_eig_vals)
-    n = length(params)
-    S_inv = zeros(n,n)
-    for i = (n-rank+1):n
-        S_inv[i,i] = 1/S_diag[i]
-    end
-    hessian_inv = U*sqrt(S_inv) #inverse of Hessian
-    dist = DSGE.DegenerateMvNormal(params, hessian_inv)
- 
-    if :draw_type==mode 
-        if parallel
-            draws = pmap(x -> draw_from_approximated_post(m,data,dist), 1:n_part)
-        else
-            draws = [draw_from_approximated_post(m,data,dist) for j = 1:n_part]
-        end
-    else
-        if parallel
-            draws = pmap(x -> DSGE.draw_from_prior(m,data), 1:n_part)
-        else
-            draws = [DSGE.draw_from_prior(m,data)  for j = 1:n_part]
-        end
-end
+    (init_draws,init_logpost,init_loglh) = initial_draw(m,data,n_part;dist_type=dist_type)
 
     for i = 1:n_part
-        prior_sim[i,:] = draws[i][1]
-        logpost[i]     = draws[i][2]
-        loglh[i]       = draws[i][3]
+        prior_sim[i,:] = init_draws[i,:]
+        logpost[i]     = init_logpost[i]
+        loglh[i]       = init_loglh[i]
     end
 
     para_sim[1,:,:] = prior_sim # Draws from prior
@@ -157,7 +118,6 @@ end
     μ = sum(para.*weight,1)
     σ = sqrt(sum((para - repmat(μ, n_part, 1)).^2 .*weight,1))
 
-
     if VERBOSITY[verbose] >= VERBOSITY[:low]
 	println("--------------------------")
         println("Iteration = $(i) / $(n_Φ)")
@@ -173,7 +133,6 @@ end
         end
     end
 
-
     #RECURSION
 
     if VERBOSITY[verbose] >= VERBOSITY[:low]
@@ -184,18 +143,21 @@ end
 
     for i=2:n_Φ
         tic()
+
 	#------------------------------------
 	# (a) Correction
 	#------------------------------------
+
 	# Incremental weights
 	incweight = exp((tempering_schedule[i] - tempering_schedule[i-1]) * loglh)
 
-       	# Update weights
+    # Update weights
 	weight_sim[:,i] = weight_sim[:,i-1].*incweight
-        zhat[i] = sum(weight_sim[:,i])
+    zhat[i] = sum(weight_sim[:,i])
 
 	# Normalize weights
 	weight_sim[:, i] = weight_sim[:, i]/zhat[i]
+
 	#------------------------------------
 	# (b) Selection
 	#------------------------------------
@@ -204,9 +166,8 @@ end
         @assert !isnan(ESS) "no particles have non-zero weight"
 
 	if (ESS < n_part/2)
-	    id = resampler(weight_sim[:,i])
-            # sample(1:n_part, weights(weight_sim[:,i]), n_part, replace = true)
-            # systematic_resampling(m, weight_sim[:,i])
+        id = resample(weight_sim[:,i]; method=get_setting(m,:resampler_smc),
+                     parallel=parallel, testing=m.testing)
             para_sim[i-1, :, :] = para_sim[i-1, id, :]
 	    loglh = loglh[id]
 	    logpost = logpost[id]
@@ -222,26 +183,26 @@ end
         c = c*(0.95 + 0.10*exp(16*(accept - target))/(1 + exp(16*(accept - target))))
         m <= Setting(:step_size_smc, c)
 
-        para = para_sim[i-1, :, free_para_inds]
-        weight = repmat(weight_sim[:,i], 1, n_params)[:,free_para_inds]
+        para = para_sim[i-1,:,:]
+        weight = repmat(weight_sim[:,i], 1, n_params)
         μ = sum(para.*weight,1)
-	z =  (para - repmat(μ, n_part, 1))
-        R_temp = (z.*weight)'*z
-        R = nearest_spd(R_temp)
+    	z = (para - repmat(μ, n_part, 1))
+        R = cov(para,1)
 
-	temp_accept = zeros(n_part, 1) # Initialize acceptance indicator
+	    temp_accept = zeros(n_part, 1) # Initialize acceptance indicator
 
         if parallel
-            out = pmap(j -> mutation_RWMH(m, data, vec(para[j,:]'), loglh[j], logpost[j], i, R, tempering_schedule), 1:n_part)
-            out = [mutation_RWMH(m, data, vec(para[j,:]'), loglh[j], logpost[j], i, R, tempering_schedule)  for j = 1:n_part]
+            out = pmap(j -> mutation_block_RWMH(m, data, para[j,:], loglh[j], logpost[j],
+                                                i, R, tempering_schedule), 1:n_part)
+        else
+            out = [mutation_block_RWMH(m, data, para[j,:], loglh[j], logpost[j],
+                                       i, R, tempering_schedule)  for j = 1:n_part]
         end
 
-        para_sim_temp = Array{Float64}[out[i][1] for i=1:length(out)]
-        fillmat = zeros(length(para_sim_temp), length(para_sim_temp[1]))
-        for j = 1:length(para_sim_temp)
-            fillmat[j,:] = para_sim_temp[j]
+        # Populate para_sim with the newly mutated particles
+        for j = 1:n_part
+            para_sim[i,:,:][j,:] = out[j][1]
         end
-        para_sim[i,:,:] = fillmat
         loglh = Float64[out[i][2] for i=1:length(out)]
         logpost = Float64[out[i][3] for i=1:length(out)]
         temp_accept = Int[out[i][4] for i=1:length(out)]
@@ -251,8 +212,8 @@ end
         ESS_sim[i,:]  = ESS # ESS
         accept_sim[i,:] = accept # average acceptance rate
 
-        para = para_sim[i, :, :]
-        weight = repmat(weight_sim[:, i], 1, n_params)
+        para = para_sim[i,:,:]
+        weight = repmat(weight_sim[:,i], 1, n_params)
 
         μ  = sum(para.*weight,1)
         σ = sqrt(sum((para - repmat(μ, n_part, 1)).^2 .*weight,1))
@@ -285,7 +246,7 @@ end
 
     end
     if m.testing
-        open(rawpath(m,"estimate,"wtsim.csv"),"a") do x
+        open(rawpath(m,"estimate","wtsim.csv"),"a") do x
             writecsv(x,weight_sim)
         end
     end
@@ -324,42 +285,72 @@ end
 
 """
 ```
-draw_from_prior(m::AbstractModel, data::Matrix)
+initial_draw(m::AbstractModel, data::Array{Float64}, n_part::Int64;dist_type=:normal)
 ```
 
-Draw from prior distribution and resample if certain conditions are not met.
-Returns a tuple (prior_draw, logpost, loglh).
+Draw from a general starting distribution (set by default to be a normal approximation centered
+at the mode) to initialize the SMC algorithm.
+Returns a tuple (draw, logpost, loglh).
 
 """
-function draw_from_prior(m::AbstractModel, data::Matrix)
-    success = false
-    n_params = n_parameters(m)
-    T = typeof(m.parameters[1].value)
-    prior_draw = Array{T}(n_params)
-    logpost = 0
-    loglh = 0
+function initial_draw(m::AbstractModel, data::Array{Float64}, n_part::Int64;dist_type::Symbol = :normal)
+    if dist_type == :normal
+        #add exception handling for no mode or hessian
+        params = h5open(rawpath(m,"estimate","paramsmode.h5"),"r") do file
+            read(file,"params")
+        end
+                    
+        fn = rawpath(m,"estimate","hessian.h5")
+        hessian = h5open(fn,"r") do file
+            read(file,"hessian")
+        end
 
-    while !success
-        for j in 1:n_params
-            prior_draw[j] = if !m.parameters[j].fixed
-                prior = rand(m.parameters[j].prior.value)
-
-                # Resample until all prior draws are within the value bounds
-                while !(m.parameters[j].valuebounds[1] < prior < m.parameters[j].valuebounds[2])
-                    prior = rand(m.parameters[j].prior.value)
+        S_diag, U = eig(hessian)
+        big_eig_vals = find(x -> x>1e-6, S_diag)
+        rank = length(big_eig_vals)
+        n = length(params)
+        S_inv = zeros(n,n)
+        for i = (n-rank+1):n
+            S_inv[i,i] = 1/S_diag[i]
+        end
+        hessian_inv = U*sqrt(S_inv)
+        dist = DSGE.DegenerateMvNormal(params, hessian_inv)
+        
+        draws = rand(dist,n_part)' 
+        logpost = zeros(n_part)
+        loglh = zeros(n_part)
+        for i in 1:size(draws)[1]
+            success = false
+            while !success 
+                try
+                    logpost[i] = posterior!(m,draws[i,:],data)
+                    loglh[i] = logpost[i] - prior(m)
+                catch
+                    draws[i,:] = rand(dist,1)
+                    continue
                 end
-                prior
-            else
-                m.parameters[j].value
-            end
-        end
-        try
-            logpost = posterior!(m, convert(Array{Float64,1},prior_draw), data)
-            loglh = logpost - prior(m)
-            success = true
-        end
+                success = true
+            end    
+        end    
+        return (draws,logpost,loglh)
+    else
+        draws = rand_prior(m;ndraws=n_part)
+        logpost = zeros(n_part)
+        loglh = zeros(n_part)
+        for i in 1:size(draws)[1]
+            success = false
+            while !success 
+                try
+                    logpost[i] = posterior!(m,draws[i,:],data)
+                    loglh[i] = logpost[i] - prior(m)
+                catch
+                    draws[i,:] = rand_prior(m,1)
+                    continue
+                end
+                success = true
+            end    
+        end    
+        return (draws,logpost,loglh)
     end
-
-    return (prior_draw, logpost, loglh)
 end
 
