3fcfaea516bc51570c05964887814d9dc4cb8673
Reca Sarfati <rebecca_sarfati@brown.edu>
2017-08-04 12:04:56 -0400

    Clean code, refactor for consistency with package

--- src/estimate/tpf/mutation.jl
+++ src/estimate/tpf/mutation.jl
@@ -1,28 +1,38 @@
 """
 ```
-function mutation(c::Float64, N_MH::Int64, deterministic::Bool, system::System{Float64}, y_t::Array{Float64,1}, s_init::Array{Float64,1}, ε_init::Array{Float64,1}, nonmissing::Array{Bool,1})
+function mutation{S<:AbstractFloat}(system::System{S}, y_t::Array{S,1}, s_init::Array{S,1}, ε_init::Array{S,1}, c::S, N_MH::Int, deterministic::Bool, nonmissing::Array{Bool,1})
 ```
-Runs random-walk Metropolis Hastings for ONE particle. The caller should loop through all particles, calling this method on each. 
-m: The model. Used to get the setting tpf_c (which is VERY important it turns out...) 
-y_t: A 1 x num_measurement vector at time t for all observed y (GDP, PCE, FFR, etc.)
-s_init: The starting state before mutation.
-ε_init: The starting epsilon (state error) before mutation.
+Runs random-walk Metropolis Hastings for single particle. The caller should loop through all particles, calling this method on each. 
+
+### Inputs
+
+- `system::System{S}`: state-space system matrices
+- `y_t`: 1 x num_measurement vector at time t for all observed y (GDP, PCE, FFR, etc.)
+- `s_init`: starting state before mutation
+- `ε_init`: starting state error before mutation
+
+### Outputs
+
+- `s_hat`:
+- `ε_hat`: 
+- `accept_rate`: 
+
 """
-function mutation(system::System{Float64}, y_t::Array{Float64,1}, s_init::Array{Float64,1}, ε_init::Array{Float64,1}, c::Float64, N_MH::Int64, deterministic::Bool, nonmissing::Array{Bool,1}, rand_mat::Array{Float64})
+function mutation{S<:AbstractFloat}(system::System{S}, y_t::Array{S,1}, s_init::Array{S,1}, ε_init::Array{S,1}, c::S, N_MH::Int, deterministic::Bool, nonmissing::Array{Bool,1}, rand_mat::Array{S})
     #------------------------------------------------------------------------
     # Setup
     #------------------------------------------------------------------------
-    DD     = system.measurement.DD[nonmissing]
-    ZZ     = system.measurement.ZZ[nonmissing,:]
-    EE     = system.measurement.EE[nonmissing,nonmissing]
-    RRR    = system.transition.RRR[:,nonmissing]
-    TTT    = system.transition.TTT
-    QQ     = system.measurement.QQ[nonmissing,nonmissing]
+    DD     = system[:DD][nonmissing]
+    ZZ     = system[:ZZ][nonmissing,:]
+    EE     = system[:EE][nonmissing,nonmissing]
+    RRR    = system[:RRR][:,nonmissing]
+    TTT    = system[:TTT]
+    QQ     = system[:QQ][nonmissing,nonmissing]
     sqrtS2 = RRR*Matrix(chol(nearestSPD(QQ)))'
 
-    # Initialize ind_s and ind_ε
-    ind_s = s_init
-    ind_ε = ε_init
+    # Initialize s_hat and ε_hat
+    s_hat = s_init
+    ε_hat = ε_init
 
     # Store length of y_t
     n_obs = length(y_t)
@@ -42,14 +52,15 @@ function mutation(system::System{Float64}, y_t::Array{Float64,1}, s_init::Array{
         ε_new = ε_init + c*rand_mat
         
         # Use the state equation to calculate the corresponding state from that ε 
-        s_new_fore = TTT*s_init + sqrtS2*ε_new
+        s_new_e = TTT*s_init + sqrtS2*ε_new
 
         # Use the state equation to calculate the state corresponding to ε_init
-        s_init_fore = TTT*s_init + sqrtS2*ε_init
+        s_init_e = TTT*s_init + sqrtS2*ε_init
 
         # Calculate difference between data and expected y from measurement equation and calculated states from above for both the new draw of ε_new and the old ε_init (we do this to calculate probabilities below. Since the error is still drawn from a Normal and every_thing is still linear, we know that y will also be normal. See equation of multivariate normal for how error_new and error_init enter into the pdf).
-        error_new  = y_t - ZZ*s_new_fore - DD
-        error_init = y_t - ZZ*s_init_fore - DD
+        error_new  = y_t - ZZ*s_new_e - DD
+        error_init = y_t - ZZ*s_init_e - DD
+
         # Calculate the top and bottom probabilities for the α ratio.
         post_new = log(pdf(MvNormal(zeros(n_obs),EE),error_new)[1]*pdf(MvNormal(zeros(length(ε_new)),eye(length(ε_new),length(ε_new))),ε_new)[1])
         post_init = log(pdf(MvNormal(zeros(n_obs),EE),error_init)[1]*pdf(MvNormal(zeros(length(ε_init)),eye(length(ε_init),length(ε_init))),ε_init)[1])
@@ -62,20 +73,20 @@ function mutation(system::System{Float64}, y_t::Array{Float64,1}, s_init::Array{
         
         if num < α 
             # Accept
-            ind_s = s_new_fore
-            ind_ε = ε_new
+            s_hat = s_new_e
+            ε_hat = ε_new
             accept += 1
         else 
-            # Reject and keep the old particle unchanged
-            ind_s = s_init_fore
-            ind_ε = ε_init
+            # Reject and keep particle unchanged
+            s_hat = s_init_e
+            ε_hat = ε_init
             
             # THIS IS DIFFERENT THAN ORIGINAL MATLAB BUT WE THINK MATLAB IS WRONG 
             # accept = 0
         end
-        ε_init = ind_ε
+        ε_init = ε_hat
     end
     accept_rate = accept/N_MH
-    return ind_s, ind_ε, accept_rate 
+    return s_hat, ε_hat, accept_rate 
 end
 
