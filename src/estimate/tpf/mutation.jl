71240c77e994da32f29f0e135490bc4273d3a13a
Reca Sarfati <rebecca_sarfati@brown.edu>
2017-08-09 11:42:38 -0400

    Annotate functions, rename variables for consistency

--- src/estimate/tpf/mutation.jl
+++ src/estimate/tpf/mutation.jl
@@ -8,16 +8,25 @@ all particles, calling this method on each.
 
 ### Inputs
 
-- `system::System{S}`: state-space system matrices
-- `y_t`: 1 x num_measurement vector at time t for all observed y (GDP, PCE, FFR, etc.)
-- `s_init`: starting state before mutation
-- `ε_init`: starting state error before mutation
+- `system`: state-space system matrices
+- `y_t`: vector of observables at time t
+- `s_init`: vector of starting state before mutation (ŝ in paper) 
+- `ε_init`: vector of starting state error before mutation
+- `c`: scaling factor used to achieve a desired acceptance rate, adjusted via:
+ 
+        cₙ = cₙ₋₁*f(1-R̂ₙ₋₁(cₙ₋₁))
+
+        Where c₁ = c_star and R̂ₙ₋₁(cₙ₋₁) is the emprical rejection rate based on mutation
+        phase in iteration n-1. Average is computed across previous N_MH RWMH steps.
+
+- `N_MH`: number of Metropolis Hastings steps
+- `nonmissing`: vector of booleans used to remove NaN values from matrices in system object
 
 ### Outputs
 
-- `s_hat`:
-- `ε_hat`: 
-- `accept_rate`: 
+- `s_out`: mutated state vector
+- `ε_out`: output ε shock corresponding to state vector 
+- `accept_rate`: acceptance rate across N_MH steps, used for updating c later
 
 """
 function mutation{S<:AbstractFloat}(system::System{S}, y_t::Array{S,1}, s_init::Array{S,1}, 
@@ -34,12 +43,13 @@ function mutation{S<:AbstractFloat}(system::System{S}, y_t::Array{S,1}, s_init::
     QQ     = system[:QQ][nonmissing,nonmissing]
     sqrtS2 = RRR*Matrix(chol(nearestSPD(QQ)))'
 
-    # Initialize s_hat and ε_hat
-    s_hat = s_init
-    ε_hat = ε_init
+    # Initialize s_out and ε_out
+    s_out = s_init
+    ε_out = ε_init
 
-    # Store length of y_t
+    # Store length of y_t, ε
     n_obs = length(y_t)
+    n_states = length(ε_init)
 
     # Initialize acceptance counter to zero
     accept = 0
@@ -62,29 +72,31 @@ function mutation{S<:AbstractFloat}(system::System{S}, y_t::Array{S,1}, s_init::
         error_new  = y_t - ZZ*s_new_e - DD
         error_init = y_t - ZZ*s_init_e - DD
 
-        # Calculate the top and bottom probabilities for the α ratio.
-        post_new = log(pdf(MvNormal(zeros(n_obs),EE),error_new)[1]*pdf(MvNormal(zeros(length(ε_new)),eye(length(ε_new),length(ε_new))),ε_new)[1])
-        post_init = log(pdf(MvNormal(zeros(n_obs),EE),error_init)[1]*pdf(MvNormal(zeros(length(ε_init)),eye(length(ε_init),length(ε_init))),ε_init)[1])
+        # Calculate posteriors
+        post_new = log(pdf(MvNormal(zeros(n_obs), EE), error_new)[1] * 
+                       pdf(MvNormal(zeros(n_states), eye(n_states, n_states)), ε_new)[1])
+        post_init = log(pdf(MvNormal(zeros(n_obs), EE), error_init)[1] * 
+                        pdf(MvNormal(zeros(n_states), eye(n_states, n_states)), ε_init)[1])
 
-        # α is the probability of accepting the new particle 
+        # Calculate α, probability of accepting the new particle 
         α = exp(post_new - post_init)
 
         # Accept the particle with probability α
         if rand() < α 
             # Accept
-            s_hat = s_new_e
-            ε_hat = ε_new
+            s_out = s_new_e
+            ε_out = ε_new
             accept += 1
         else 
             # Reject and keep particle unchanged
-            s_hat = s_init_e
-            ε_hat = ε_init
-            
-            # THIS IS DIFFERENT THAN ORIGINAL MATLAB BUT WE THINK MATLAB IS WRONG 
-            #accept = 0
+            s_out = s_init_e
+            ε_out = ε_init
         end
-        ε_init = ε_hat
+        ε_init = ε_out
     end
+
+    # Calculate acceptance rate
     accept_rate = accept/N_MH
-    return s_hat, ε_hat, accept_rate 
+
+    return s_out, ε_out, accept_rate 
 end
