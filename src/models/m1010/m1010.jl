b1989b7bec5ff0b530337187b2ec90e369fc472d
Pearl Li <pearlzli16@gmail.com>
2017-08-09 16:43:16 -0400

    Reimplement σ_ω_star -> σ_c in 1010 as ss20

--- src/models/m1010/m1010.jl
+++ src/models/m1010/m1010.jl
@@ -659,7 +659,11 @@ function steadystate!(m::Model1010)
     Rhostar     = 1/nkstar - 1
 
     # define betabar
-    betabar     = exp( (m[:σ_c] -1) * m[:z_star]) / m[:β]
+    if subspec(m) == "ss20"
+        betabar = exp( (m[:σ_c] -1) * m[:z_star]) / m[:β]
+    else
+        betabar = exp( (σ_ω_star -1) * m[:z_star]) / m[:β]
+    end
 
     # evaluate wekstar and vkstar
     wekstar     = (1-(m[:γ_star]*betabar))*nkstar - m[:γ_star]*betabar*(m[:spr]*(1-μ_estar*Gstar) - 1)
