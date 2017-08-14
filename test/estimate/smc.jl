91c22044966f5ccdc9a357dfc10791606cc5a34a
Michael Cai <michaeldcai@gmail.com>
2017-08-14 10:39:42 -0400

    wrote tests for newly refactored smc code

--- test/estimate/smc.jl
+++ test/estimate/smc.jl
@@ -10,45 +10,50 @@ custom_settings = Dict{Symbol, Setting}(
     :date_forecast_start  => Setting(:date_forecast_start, quartertodate("2015-Q4")))
 s = AnSchorfheide(custom_settings = custom_settings, testing = true)
 
-s <= Setting(:saveroot, "$path/../../save")
-s <= Setting(:dataroot, "$path/../../save/input_data")
+saveroot = normpath(joinpath(dirname(@__FILE__),"..","..","save"))
+dataroot = normpath(joinpath(dirname(@__FILE__),"..","..","save","input_data"))
 
+s <= Setting(:saveroot, saveroot)
+s <= Setting(:dataroot, dataroot)
 s <= Setting(:n_particles, 400)
 s <= Setting(:n_Φ, 200)
 s <= Setting(:λ, 2.0)
 s <= Setting(:n_smc_blocks, 1)
-s <= Setting(:parallel, true)
+s <= Setting(:use_parallel_workers, false)
 s <= Setting(:step_size_smc,0.5)
 s <= Setting(:n_MH_steps_smc, 5)
-s <= Setting(:resampler_smc, :multinomial)
+s <= Setting(:resampler_smc, :systematic)
 s <= Setting(:target_accept, 0.25)
-s <= Setting(:draw_type, "mode")
-try
-    rm("resamples.csv")
-    rm("wtsim.csv")
-    rm("draws.csv")
-end
+
+data = h5read("$path/../reference/smc.h5","data")
+cloud = ParticleCloud(s,get_setting(s,:n_particles))
+
+# Testing initial_draw()
+s <= Setting(:initial_draw_source, :normal)
+initial_draw(s, data, cloud)
+@assert sum(!isfinite(DSGE.get_logpost(cloud))) == 0
+@assert sum(!isfinite(DSGE.get_loglh(cloud))) == 0
+
+s <= Setting(:initial_draw_source, :prior)
+initial_draw(s, data, cloud)
+@assert sum(!isfinite(DSGE.get_logpost(cloud))) == 0
+@assert sum(!isfinite(DSGE.get_loglh(cloud))) == 0
 
 # Reading in reference
 data = h5read("$path/../reference/smc.h5","data")
-resamples = h5read("$path/../reference/smc.h5","resamples")
-wtsim = h5read("$path/../reference/smc.h5","wtsim")
+weights = h5read("$path/../reference/smc.h5","weights")
 draws = h5read("$path/../reference/smc.h5","draws")
+mutated_values = h5read("$path/../reference/smc.h5","mutated_values")
 
-##to precompute the hessian and mode parameters
-#estimate(s, data, method=:MH)
-#estimate(s, data[:,100:200], method =:MH, proposal_covariance=ones(3,3))
-##
-smc(s,data[:,100:120],verbose=:low)
+# to precompute the hessian and mode parameters
+smc(s,data,verbose=:none)
 
-#=
-@test_approx_eq resamples readcsv("resamples.csv")
-@test_approx_eq wtsim readcsv("wtsim.csv")
 @test_approx_eq draws readcsv("draws.csv")
-=#
+@test_approx_eq weights readcsv("weights.csv")
+@test_approx_eq mutated_values readcsv("mutated_values.csv")
 
-rm("resamples.csv")
-rm("wtsim.csv")
 rm("draws.csv")
+rm("weights.csv")
+rm("mutated_values.csv")
 
 nothing
