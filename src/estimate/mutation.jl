d588e362d8286bdc201f63ebbd9379a172eed5e0
Michael Cai <michaeldcai@gmail.com>
2017-08-14 10:40:32 -0400

    refactored mutation/condensed blocked and non-blocked mutation into a single method

--- src/estimate/mutation.jl
+++ src/estimate/mutation.jl
@@ -1,6 +1,7 @@
 """
 ```
-mutation_block_RWMH(m, data, p0, l0, post0, i, R, tempering_schedule; rvec=[], rval=[])
+mutation(m::AbstractModel, data::Matrix{Float64}, p::Particle, R::Array{Float64,2},
+         current_φ::Float64, previous_φ::Float64; rvec = [], rval = [], rstep = [])
 ```
 
 Execute one proposed move of the Metropolis-Hastings algorithm for a given parameter
@@ -32,7 +33,8 @@ Execute one proposed move of the Metropolis-Hastings algorithm for a given param
 - `accept`: Indicator for acceptance or rejection (0 reject, 1 accept)
 
 """
-function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init::Array{Float64,1}, like_init::Float64, post_init::Float64, i::Int64, R::Array{Float64,2}, tempering_schedule::Array{Float64,1}; rvec = [], rval = [], rstep = [])
+function mutation(m::AbstractModel, data::Matrix{Float64}, p::Particle, R::Array{Float64,2},
+                  current_φ::Float64, previous_φ::Float64; rvec = [], rval = [], rstep = [])
 
     n_Φ = get_setting(m, :n_Φ)
     n_part = get_setting(m, :n_particles)
@@ -45,60 +47,76 @@ function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init:
     accept = get_setting(m, :init_accept)
     target = get_setting(m, :target_accept)
 
-    # draw initial step and step probability
+    # draw initial step probability
     # conditions for testing purposes
-    # step = isempty(rstep) ? (isempty(rvec) ? randn(n_para,1): rvec) : rstep
     step_prob = isempty(rval) ? rand() : rval
 
-    cov_mat = R
-    para = para_init
-    like = like_init
+    U, E, V = svd(R)
+    cov_mat = U * diagm(sqrt(E))
+
+    para = p.value
+    like = p.loglh
+    post_init = p.logpost
 
     # Previous posterior needs to be updated (due to tempering)
-    post = post_init + (tempering_schedule[i] - tempering_schedule[i-1]) * like
-    accept = 0
-    
-### BLOCKING ###
+    post = post_init + (current_φ - previous_φ) * like
+    accept = false
 
-    # Generate random index for each parameter, as means of sorting
+    ### BLOCKING ###
+
+    # Generate random ordering
     if m.testing
-        para_inds = collect(1:n_para) 
+        para_inds = collect(1:n_para)
     else
-        para_inds = zeros(n_para)
-        for j=1:n_para
-            para_inds[j] = rand()
-        end
         # Returns indices sorted by their random value
+        para_inds = [rand() for j in 1:n_para]
         para_inds = sortperm(para_inds)
     end
 
     # Find the indices for the lower and upper bounds on each block
     b = 1
-    blo = Array{Int64}(n_blocks)
-    bhi = Array{Int64}(n_blocks)
+    blo = Array{Int}(n_blocks)
+    bhi = Array{Int}(n_blocks)
     for b in 1:n_blocks
-        blo[b] = convert(Int64,max(0,ceil((b-1)*n_para/n_blocks)))
-        bhi[b] = convert(Int64,min(n_para+1,ceil(b*n_para/n_blocks)+1))
+        blo[b] = convert(Int,max(0,ceil((b-1)*n_para/n_blocks)))
+        bhi[b] = convert(Int,min(n_para+1,ceil(b*n_para/n_blocks)+1))
     end
 
-    for (j,k) in zip(blo,bhi)
-        #Zeroing out relevant rows in cov matrix
-        cov_mat_block = copy(cov_mat)
-        blo_inds = para_inds[1:j]
-        bhi_inds = para_inds[k:end]
-        cov_mat_block[:,blo_inds] = zeros(size(cov_mat_block[:,blo_inds]))
-        cov_mat_block[blo_inds,:] = zeros(size(cov_mat_block[blo_inds,:]))
-        cov_mat_block[:,bhi_inds] = zeros(size(cov_mat_block[:,bhi_inds]))
-        cov_mat_block[bhi_inds,:] = zeros(size(cov_mat_block[bhi_inds,:]))
+    l = 0
+    while l < n_steps
+        for (j, k) in zip(blo, bhi)
+            #Zeroing out relevant rows in cov matrix
+            cov_mat_block = copy(cov_mat)
+            blo_inds = para_inds[1:j]
+            bhi_inds = para_inds[k:end]
 
-        l = 0
-        while l < n_steps
-            #para_new = para+c*cov_mat_block*step
-            para_new = rand(DegenerateMvNormal(para,cov_mat_block);cc=c)
-            post_new = posterior!(m, vec(para_new), data; 
-                                  φ_smc = tempering_schedule[i],
-                                  sampler = true)
-            like_new = post_new - prior(m)
+            cov_mat_block[:,blo_inds] = 0
+            cov_mat_block[blo_inds,:] = 0
+            cov_mat_block[:,bhi_inds] = 0
+            cov_mat_block[bhi_inds,:] = 0
+
+            para_new =
+            if isempty(rstep) && !isempty(rvec)
+                para + c*cov_mat_block*rvec
+            elseif !isempty(rstep) && isempty(rvec)
+                para + c*cov_mat_block*rstep
+            else
+                rand(DegenerateMvNormal(para, cov_mat_block); cc = c)
+            end
+
+            like_new = -Inf
+            post_new = -Inf
+            try
+                update!(m, para_new)
+                like_new = likelihood(m, data;sampler = true)
+                post_new = current_φ*like_new + prior(m)
+            catch err
+                if isa(err, GensysError) || isa(err, ParamBoundsError)
+                    post_new = like_new = -Inf
+                else
+                    throw(err)
+                end
+            end
 
             # if step is invalid, retry
             if !isfinite(post_new) && isempty(rstep)
@@ -114,36 +132,18 @@ function mutation_block_RWMH(m::AbstractModel, data::Matrix{Float64}, para_init:
                 para   = para_new
                 like   = like_new
                 post   = post_new
-                accept = 1
+                accept = true
             end
 
             # draw again for the next step
-            #step = isempty(rstep) ? randn(n_para,1): step.+1
             step_prob = rand()
-            l += 1
+            if m.testing && !isempty(rstep)
+                rstep += 1
+            end
         end
+        l += 1
     end
-    return para, like, post, accept
-end
-
-# Function used at the start of program to zero-out fixed rows and columns
-function augment_cov_mat(cov_mat,list_inds)
-    for id in list_inds
-        cov_mat[:,id] = zeros(size(cov_mat)[1])
-        cov_mat[id,:] = zeros(size(cov_mat)[1])
-    end
-    return cov_mat
-end
-
-# Not currently being called
-function augment_draw(draw, m)
-    new_draw = map(x -> x.value, m.parameters)
-    #j = 1
-    #for (i,θ) in enumerate(m.parameters)
-    #    if !θ.fixed
-    #        new_draw[i] = draw[j]
-    #        j += 1
-    #    end
-    #end
-    return new_draw
+    update_mutation!(p, para, like, post, accept)
+    return p
+    # return p,para,like,post,accept
 end
