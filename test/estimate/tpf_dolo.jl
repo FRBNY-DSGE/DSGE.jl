ffce0841bdbab16e5f013a5501774abda6ed684a
Michael Cai <michaeldcai@gmail.com>
2017-08-22 14:03:41 -0400

    added measurement error to data generation.

--- test/estimate/tpf_dolo.jl
+++ test/estimate/tpf_dolo.jl
@@ -1,11 +1,13 @@
+using ClusterManagers
 using AxisArrays
 using DSGE, Distributions
-using Plots
 import Dolo
-gr()
+
+# addprocs_sge(15, queue = "background.q")
+# @everywhere using DSGE
 
 model = Dolo.yaml_import("sudden_stop.yaml")
-variables = vcat(model.symbols[:states], model.symbols[:controls]) # obsrved states
+variables = vcat(model.symbols[:states], model.symbols[:controls]) # observed states
 
 n_s = length(model.symbols[:states])
 n_x = length(model.symbols[:controls])
@@ -29,20 +31,23 @@ function Φ(s::Vector, ϵ::Vector)
 end
 
 function Ψ(s::Vector, u::Vector)
-    s[2:3] + u
+    s[[2,4]] + u
 end
-# Because we've chosen the data to only be on the middle two states
-data = data[2:3,:]
-
-# Φ(s_t1::Matrix, ε_t1::Matrix) = hcat([Φ(s_t1[:,i], ε_t1[:,i]) for i in 1:size(s_t1)[2]]...)
-# Ψ(s_t1::Matrix, u_t1::Matrix) = hcat([Ψ(s_t1[:,i], u_t1[:,i]) for i in 1:size(s_t1)[2]]...)
+# Because we've chosen the observed data to only be on the middle two states
+data = data[[2,4],:].data
 
 s0 = vcat(model.calibration[:states, :controls]...)
 e0 = model.calibration[:exogenous]
 
 # Convert dolo distrib to Distriutions object
 F_ϵ = Distributions.MvNormal(model.exogenous.mu, model.exogenous.Sigma)
-F_u = Distributions.MvNormal(zeros(2), 1e-8*eye(2))
+
+# Setting measurement error = std of states / 5
+F_u = Distributions.MvNormal(zeros(2), sqrt.(diagm(diag(cov(data, 2))/5)))
+# F_u = Distributions.MvNormal(zeros(2), 1e-8*eye(2))
+
+# Adding measurement error
+data = data + rand(F_u, size(data)[2])
 
 # Instantiating a model purely for obtaining settings for tpf algorithm
 m = AnSchorfheide()
@@ -52,6 +57,7 @@ n_parts = 4000
 parallel = false
 
 m <= Setting(:tpf_n_mh_simulations, N_MH)
+m <= Setting(:tpf_r_star, 1.1)
 m <= Setting(:tpf_c_star, 0.1)
 m <= Setting(:tpf_n_particles, n_parts)
 m <= Setting(:use_parallel_workers, parallel)
@@ -59,4 +65,7 @@ m <= Setting(:x_tolerance, 0.)
 m <= Setting(:tpf_deterministic, false)
 
 s_init = initialize_state_draws(s0, F_ϵ, Φ, n_parts)
-Neff, lik_tpf = tpf(m, data.data, Φ, Ψ, F_ϵ, F_u, s_init)
+
+Neff, lik_tpf, times, φ_probs, p_errors = tpf(m, data, Φ, Ψ, F_ϵ, F_u, s_init)
+
+# rmprocs(procs())
