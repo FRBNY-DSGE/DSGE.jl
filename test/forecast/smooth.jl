1fd6711fd462a944118fb66830a6f7fa259f36a2
Michael Cai <michaeldcai@gmail.com>
2017-08-29 16:15:11 -0400

    Update tests to incorporate new Kalman object (now with marginal likelihood)

--- test/forecast/smooth.jl
+++ test/forecast/smooth.jl
@@ -7,7 +7,7 @@ m = AnSchorfheide(testing = true)
 m <= Setting(:date_forecast_start, quartertodate("2015-Q4"))
 
 df, system, kal = jldopen("$path/../reference/forecast_args.jld","r") do file
-    read(file, "df"), read(file, "system"), read(file, "kal")
+    read(file, "df"), read(file, "system"), read(file, "kalman")
 end
 
 # Read expected output
