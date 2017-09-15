09338b1448766af8930ec0856d74e3d8b2850b2b
Pearl Li <pearlzli16@gmail.com>
2017-09-15 16:38:03 -0400

    Change plot_scenario to take in AbstractScenario

--- src/plot/plot_scenario.jl
+++ src/plot/plot_scenario.jl
@@ -1,8 +1,8 @@
 """
 ```
-plot_scenario(m, var, class, key, vint; title = "", kwargs...)
+plot_scenario(m, var, class, scen; title = "", kwargs...)
 
-plot_scenario(m, vars, class, key, vint; untrans = false, fourquarter = false,
+plot_scenario(m, vars, class, scen; untrans = false, fourquarter = false,
     plotroot = figurespath(m, \"scenarios\"), titles = [], tick_size = 1,
     legend = :none, kwargs...)
 ```
@@ -15,8 +15,7 @@ specified by `key` and `vint`.
 - `var::Symbol` or `vars::Vector{Symbol}`: variable(s) to be plotted,
   e.g. `:obs_gdp` or `[:obs_gdp, :obs_nominalrate]`
 - `class::Symbol`
-- `key::Symbol`: scenario key
-- `vint::String`: scenario vintage
+- `scen::AbstractScenario`: scenario
 
 ### Keyword Arguments
 
@@ -32,19 +31,17 @@ specified by `key` and `vint`.
 
 - `p::Plot` or `plots::OrderedDict{Symbol, Plot}`
 """
-function plot_scenario(m::AbstractModel, var::Symbol, class::Symbol,
-                       key::Symbol, vint::String;
-                       title::String = "",
-                       kwargs...)
+function plot_scenario(m::AbstractModel, var::Symbol, class::Symbol, scen::AbstractScenario;
+                       title::String = "", kwargs...)
 
-    plots = plot_scenario(m, [var], class, key, vint;
+    plots = plot_scenario(m, [var], class, scen,
                           titles = isempty(title) ? String[] : [title],
                           kwargs...)
     return plots[var]
 end
 
 function plot_scenario(m::AbstractModel, vars::Vector{Symbol}, class::Symbol,
-                       key::Symbol, vint::String; untrans::Bool = false, fourquarter::Bool = false,
+                       scen::AbstractScenario; untrans::Bool = false, fourquarter::Bool = false,
                        plotroot::String = figurespath(m, "scenarios"),
                        titles::Vector{String} = String[],
                        tick_size::Int = 1,
@@ -63,7 +60,7 @@ function plot_scenario(m::AbstractModel, vars::Vector{Symbol}, class::Symbol,
 
     # Read in MeansBands
     hist  = MeansBands()
-    fcast = read_scenario_mb(m, key, vint, Symbol(fcast_prod, class))
+    fcast = read_scenario_mb(m, scen, Symbol(fcast_prod, class))
 
     # Get titles if not provided
     if isempty(titles)
@@ -77,7 +74,7 @@ function plot_scenario(m::AbstractModel, vars::Vector{Symbol}, class::Symbol,
         output_file = if isempty(plotroot)
             ""
         else
-            get_scenario_filename(m, key, vint, Symbol(fcast_prod, "_", DSGE.detexify(var)),
+            get_scenario_filename(m, scen, Symbol(fcast_prod, "_", DSGE.detexify(var)),
                                   pathfcn = figurespath,
                                   fileformat = DSGE.plot_extension())
         end
