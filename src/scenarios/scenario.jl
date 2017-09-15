305d465e3b4b41e6c2c3c23cc341024630a2a893
Abhi Gupta <abhig94@gmail.com>
2017-09-15 13:24:29 -0400

    first pass at switching scenarios

--- src/scenarios/scenario.jl
+++ src/scenarios/scenario.jl
@@ -53,3 +53,43 @@ function targets_to_data(m::AbstractModel, scen::Scenario)
     end
     return df
 end
+
+abstract SwitchingScenario <: Scenario
+
+function Base.show(io::IO, scen::SwitchingScenario)
+    @printf io "%-12s %s\n" "Original:" scen.key
+    @printf io "%-12s %s\n" "Description:" scen.description
+    @printf io "%-12s %s\n" "Description:" scen.default_scenario
+end
+
+
+function define_switching_scenario(original::Scenario)
+    new_name = Symbol(string(original.key) * "Switching")
+
+    return @eval begin
+        type $(new_name) <: SwitchingScenario
+            key::Symbol
+            description::String
+            default_scenario::Symbol
+            probs_enter::Vector{Float64}
+            probs_leave::Vector{Float64}
+        end
+    end
+ end
+
+function construct_switching_scenario(original::Scenario, default::Scenario,
+                                      prob_enter::Vector{Float64},
+                                      prob_leave::Vector{Float64})
+
+    new_name = Symbol(string(original.key) * "Switching")
+    if !isdefined(new_name)
+        define_switching_scenario(original)
+    end
+
+    @assert length(prob_enter) == length(prob_leave) == n_target_horizons(original)
+
+    @eval constructor = $(new_name)
+    new_scenario = constructor(original.key, original.description,
+                               default.key, prob_enter, prob_leave)
+    return new_scenario
+end
