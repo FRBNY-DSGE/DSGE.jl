34b0a67c943c703cdc7bb0d87857c92500c45b54
Abhi Gupta <abhig94@gmail.com>
2017-09-16 14:21:38 -0400

    Implement switching (aka stochastic entry and exit)

---
+++ src/scenarios/switching.jl
@@ -0,0 +1,90 @@
+"""
+```
+simulate_switching(m, scenario; verbose = :low)
+```
+"""
+function simulate_switching(m::AbstractModel, scen::SwitchingScenario;
+                           verbose::Symbol = :low)
+
+    if DSGE.VERBOSITY[verbose] >= DSGE.VERBOSITY[:low]
+        info("Simulating switching for $(scen.key)...")
+        println("Start time: " * string(now()))
+        println("Outputs will be saved in " * rawpath(m, "scenarios"))
+        tic()
+    end
+
+    original_output_files = get_scenario_output_files(m, scen.original,
+                                                      [:forecastobs, :forecastpseudo])
+
+    default_output_files = get_scenario_output_files(m, scen.default,
+                                                     [:forecastobs, :forecastpseudo])
+
+    results = Dict{Symbol, Array{Float64}}()
+    for output_var in [:forecastobs, :forecastpseudo]
+        # these forecasts are n_draws x n_vars x n_periods
+        original_draws = load(original_output_files[output_var])["arr"]
+        default_draws  = load(default_output_files[output_var])["arr"]
+
+        n_draws, n_vars, n_periods = size(original_draws)
+        @assert n_vars == size(default_draws, 2)
+        @assert n_periods <= size(default_draws, 3)
+
+        results[output_var] = zeros(n_draws, n_vars, n_periods)
+
+        for i = 1:n_draws
+            j = rand(1:size(default_draws,1))
+            results[output_var][i, :, :] = switch(original_draws[i,:,:], default_draws[j,:,:],
+                                                  scen.probs_enter, scen.probs_exit)
+        end
+
+    end
+
+    #results = DSGE.assemble_block_outputs(results)
+    output_files = get_scenario_output_files(m, scen, [:forecastobs, :forecastpseudo])
+    write_scenario_forecasts(m, output_files, results, verbose = verbose)
+
+    # Print
+    if DSGE.VERBOSITY[verbose] >= DSGE.VERBOSITY[:low]
+        switching_time = toq()
+        switching_time_min = switching_time/60
+        println("\nTime elapsed: $switching_time_min minutes")
+        println("Switching complete: $(now())")
+    end
+
+    return results
+
+end
+
+
+function switch(original::Array{Float64,2}, default::Array{Float64,2},
+                probs_enter::Vector{Float64}, probs_exit::Vector{Float64})
+
+    n_vars, n_periods = size(original)
+    output = zeros(n_vars, n_periods)
+
+    # find period we switch into the original from the default
+    period_in  = choose_last_period(probs_enter, 1, n_periods)
+    # spend 1 period in the original scenario at least
+    period_out = choose_last_period(probs_exit, period_in + 2, n_periods)
+
+    output[:,1:period_in]            = default[:,1:period_in]
+    output[:,period_in+1:period_out] = original[:,1:(period_out-period_in)]
+    if period_out < n_periods
+        output[:, period_out+1:n_periods] = default[:,period_out+1:n_periods]
+    end
+
+    return output
+end
+
+function choose_last_period(probs::Array{Float64,1}, first_period::Int, n_periods::Int)
+    last_period = n_periods
+
+    for t = first_period:n_periods
+        if rand() < probs[t]
+            last_period = t-1
+            return last_period
+        end
+    end
+
+    return last_period
+end
