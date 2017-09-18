10dfa75d959415acc1efbced6c1672f186806444
Abhi Gupta <abhig94@gmail.com>
2017-09-18 15:55:39 -0400

    Update test formatting for switching

--- test/scenarios/switching.jl
+++ test/scenarios/switching.jl
@@ -4,11 +4,11 @@ using DSGE, JLD
 original = zeros(1,4)
 default  = ones(1,4)
 
-@assert switch(original,default,[0. for x in 1:4],[0. for x in 1:4]) == default
-@assert switch(original,default,[1. for x in 1:4],[0. for x in 1:4]) == original
-@assert switch(original,default,[1. for x in 1:4],[0. for x in 1:4]) == [0. 1. 1. 1. 1.]
+@tests switch(original, default, zeros(4), zeros(4)) == default
+@tests switch(original, default, ones(4), zeros(4)) == original
+@tests switch(original, default, ones(4), zeros(4)) == [0. 1. 1. 1. 1.]
 
 # Test choose_last_period
-@assert choose_last_period([1. 1. 1.], 1, 3) == 0
-@assert choose_last_period([1. 1. 1.], 2, 3) == 1
-@assert choose_last_period([1. 1. 1.], 4, 3) == 3
+@tests choose_last_period(ones(3), 1, 3) == 0
+@tests choose_last_period(ones(3), 2, 3) == 1
+@tests choose_last_period(ones(3), 4, 3) == 3
