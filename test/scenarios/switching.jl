4142a21d33b45d1722299bdde257d314981c6224
Pearl Li <pearlzli16@gmail.com>
2017-09-20 12:10:56 -0400

    Use DSGE. in scenario switching tests

--- test/scenarios/switching.jl
+++ test/scenarios/switching.jl
@@ -4,11 +4,11 @@ using DSGE, Base.Test
 original = zeros(1,4)
 default  = ones(1,4)
 
-@test switch(original, default, zeros(4), zeros(4)) == default
-@test switch(original, default, ones(4), zeros(4)) == original
-@test switch(original, default, ones(4), ones(4)) == [0. 1. 1. 1.]
+@test DSGE.switch(original, default, zeros(4), zeros(4)) == default
+@test DSGE.switch(original, default, ones(4), zeros(4)) == original
+@test DSGE.switch(original, default, ones(4), ones(4)) == [0. 1. 1. 1.]
 
 # Test choose_last_period
-@test choose_last_period(ones(3), 1, 3) == 0
-@test choose_last_period(ones(3), 2, 3) == 1
-@test choose_last_period(ones(3), 4, 3) == 3
+@test DSGE.choose_last_period(ones(3), 1, 3) == 0
+@test DSGE.choose_last_period(ones(3), 2, 3) == 1
+@test DSGE.choose_last_period(ones(3), 4, 3) == 3
