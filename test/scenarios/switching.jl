80a3b2e4781b6e7750ed7236c92ffe8ce222e2d2
Pearl Li <pearlzli16@gmail.com>
2017-09-20 10:06:40 -0400

    Change @tests -> @test in switching.jl test

--- test/scenarios/switching.jl
+++ test/scenarios/switching.jl
@@ -1,14 +1,14 @@
 using DSGE, JLD
 
-# Test switch()
+# Test switch
 original = zeros(1,4)
 default  = ones(1,4)
 
-@tests switch(original, default, zeros(4), zeros(4)) == default
-@tests switch(original, default, ones(4), zeros(4)) == original
-@tests switch(original, default, ones(4), zeros(4)) == [0. 1. 1. 1. 1.]
+@test switch(original, default, zeros(4), zeros(4)) == default
+@test switch(original, default, ones(4), zeros(4)) == original
+@test switch(original, default, ones(4), zeros(4)) == [0. 1. 1. 1. 1.]
 
 # Test choose_last_period
-@tests choose_last_period(ones(3), 1, 3) == 0
-@tests choose_last_period(ones(3), 2, 3) == 1
-@tests choose_last_period(ones(3), 4, 3) == 3
+@test choose_last_period(ones(3), 1, 3) == 0
+@test choose_last_period(ones(3), 2, 3) == 1
+@test choose_last_period(ones(3), 4, 3) == 3
