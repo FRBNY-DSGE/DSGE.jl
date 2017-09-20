0f1429a7cef3c0b6a4d24493dd2f39d146512269
Abhi Gupta <abhig94@gmail.com>
2017-09-20 10:40:14 -0400

    Fix switching test

--- test/scenarios/switching.jl
+++ test/scenarios/switching.jl
@@ -1,4 +1,4 @@
-using DSGE, JLD
+using DSGE, Base.Test
 
 # Test switch
 original = zeros(1,4)
@@ -6,7 +6,7 @@ default  = ones(1,4)
 
 @test switch(original, default, zeros(4), zeros(4)) == default
 @test switch(original, default, ones(4), zeros(4)) == original
-@test switch(original, default, ones(4), zeros(4)) == [0. 1. 1. 1. 1.]
+@test switch(original, default, ones(4), ones(4)) == [0. 1. 1. 1.]
 
 # Test choose_last_period
 @test choose_last_period(ones(3), 1, 3) == 0
