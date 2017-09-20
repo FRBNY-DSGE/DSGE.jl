f5dddca386e3fb2ff3a31eb3f039dac02a72ccb2
Pearl Li <pearlzli16@gmail.com>
2017-09-20 17:00:00 -0400

    Test plot_history_and_forecast with fourquarter=true, plot_scenario

--- test/plot/plot.jl
+++ test/plot/plot.jl
@@ -8,12 +8,13 @@ m <= Setting(:saveroot, tempdir())
 m <= Setting(:date_forecast_start, quartertodate("2015-Q4"))
 m <= Setting(:date_conditional_end, quartertodate("2015-Q4"))
 m <= Setting(:use_population_forecast, true)
-m <= Setting(:forecast_horizons, 8)
+m <= Setting(:forecast_horizons, 12)
 m <= Setting(:impulse_response_horizons, 8)
 
 # Run full-distribution forecast
 estroot = normpath(joinpath(dirname(@__FILE__), "..", "reference"))
 overrides = forecast_input_file_overrides(m)
+overrides[:mode] = joinpath(estroot, "optimize.h5")
 overrides[:full] = joinpath(estroot, "metropolis_hastings.h5")
 output_vars = add_requisite_output_vars([:histobs, :forecastobs, :shockdecobs, :irfobs])
 
@@ -26,6 +27,11 @@ m <= Setting(:forecast_block_size, 5)
 plot_history_and_forecast(m, :obs_nominalrate, :obs, :full, :none,
                           bdd_and_unbdd = true,
                           start_date = DSGE.quartertodate("2007-Q1"),
+                          verbose = :none)
+plot_history_and_forecast(m, :obs_nominalrate, :obs, :full, :none,
+                          bdd_and_unbdd = true,
+                          start_date = DSGE.quartertodate("2007-Q1"),
+                          fourquarter = true,
                           verbose = :none)
 
 # Plot forecast comparison
@@ -42,6 +48,19 @@ plot_impulse_response(m, :rm_sh, collect(keys(m.observables)), :obs, :full, :non
                       verbose = :none)
 plot_impulse_response(m, :rm_sh, collect(keys(m.observables)), :obs, :full, :none,
                       flip = true, verbose = :none)
+
+# Plot scenario, zeroing out measurement error
+for para in [:e_y, :e_π, :e_R]
+    m[para].value = 0
+end
+alt = Scenario(:altscen, "Test Alternative Scenario", [:obs_gdp, :obs_cpi], [:g_sh, :rm_sh], "REF")
+forecast_scenario(m, alt, verbose = :none)
+scenario_means_bands(m, alt, verbose = :none)
+plot_scenario(m, :obs_nominalrate, :obs, alt, untrans = true)
+plot_scenario(m, :obs_nominalrate, :obs, alt)
+plot_scenario(m, :obs_nominalrate, :obs, alt, fourquarter = true)
+@test_throws ErrorException plot_scenario(m, :obs_nominalrate, :obs, alt,
+                                          untrans = true, fourquarter = true)
 
 # Hair plot
 df = load_data(m)
