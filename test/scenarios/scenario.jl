28cb637ff8d0b98ce69a153703ecf1f6ce733ea8
Pearl Li <pearlzli16@gmail.com>
2017-09-21 10:19:11 -0400

    Add scenario shock scaling

--- test/scenarios/scenario.jl
+++ test/scenarios/scenario.jl
@@ -6,6 +6,7 @@ m <= Setting(:forecast_horizons, 2)
 
 # Scenario
 scen = Scenario(:testscen, "Test Scenario", [:obs_gdp, :obs_cpi], [:g_sh, :rm_sh], "REF")
+@test scen.shock_scaling == 1.0
 @test n_targets(scen) == 2
 @test n_instruments(scen) == 2
 
@@ -21,6 +22,11 @@ exp[:obs_nominalrate] = fill(NaN, 2)
 for col in names(exp)
     @test act[col] == exp[col] || (all(isnan(act[col])) && all(isnan(exp[col])))
 end
+
+# Scenario with shock scaling
+scale = Scenario(:scaledscen, "Test Shock Scaling Scenario", [:obs_gdp, :obs_cpi], [:g_sh, :rm_sh], "REF",
+                 shock_scaling = 2.0)
+@test scale.shock_scaling == 2.0
 
 # SwitchingScenario
 @test_throws ErrorException SwitchingScenario(:switch, scen, scen, [-1.0, 1.0], [1.0, 1.0])
