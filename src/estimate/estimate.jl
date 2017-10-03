309adc69c345c0a00b0b81f99b6b43cb2a30f33e
Michael Cai <michaeldcai@gmail.com>
2017-10-03 14:40:52 -0400

    Fix deprecation warning in v0.6

--- src/estimate/estimate.jl
+++ src/estimate/estimate.jl
@@ -187,7 +187,7 @@ function estimate(m::AbstractModel, data::Matrix{Float64};
             for i = (n-rank+1):n
                 S_inv[i, i] = 1/S_diag[i]
             end
-        hessian_inv = U*sqrt(S_inv) #this is the inverse of the hessian
+        hessian_inv = U*sqrt.(S_inv) #this is the inverse of the hessian
         DSGE.DegenerateMvNormal(params, hessian_inv)
     else
         DSGE.DegenerateMvNormal(params, proposal_covariance)
