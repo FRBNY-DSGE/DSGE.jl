f95c3e962fc1dc04b1c4660a16cdff2f41b9d38b
Michael Cai <michaeldcai@gmail.com>
2017-10-13 11:47:24 -0400

    Change error to warning if a column of df is all NaN

--- src/data/load_data.jl
+++ src/data/load_data.jl
@@ -358,7 +358,7 @@ function isvalid_data(m::AbstractModel, df::DataFrame; cond_type::Symbol = :none
     # Ensure that no series is all NaN
     for col in setdiff(names(df), [:date])
         if all(isnan.(df[col]))
-            error("df[$col] is all NaN.")
+            warn("df[$col] is all NaN.")
         end
     end
 
