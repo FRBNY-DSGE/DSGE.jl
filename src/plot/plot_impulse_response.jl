1f7bce1911576bc5a134a20b65f75e07f6b8de88
Pearl Li <pearlzli16@gmail.com>
2017-10-14 18:35:13 -0400

    Reimplement plot_impulse_response using plot recipe

--- src/plot/plot_impulse_response.jl
+++ src/plot/plot_impulse_response.jl
@@ -1,52 +1,33 @@
 """
 ```
 plot_impulse_response(m, shock, var, class, input_type, cond_type;
-    forecast_string = "", plotroot = figurespath(m, \"forecast\"),
     title = "", kwargs...)
 
 plot_impulse_response(m, shock, vars, class, input_type, cond_type;
     forecast_string = "", plotroot = figurespath(m, \"forecast\"),
     titles = [], kwargs...)
-
-plot_impulse_response(shock, var, mb; bands_pcts = [\"90.0%\"],
-    output_file = "", title = "", flip = false, verbose = :low)
 ```
 
-Plot the responses of `var` to `shock` from `mb`, possibly read in using
-`read_mb` (depending on the method). By default, only 90% bands are plotted.
+Plot the responses of `var` to `shock`. By default, only 90% bands are plotted.
 
 ### Inputs
 
+- `m::AbstractModel`
 - `shock::Symbol`: e.g. `:g_sh`
 - `var::Symbol` or `vars::Vector{Symbol}`: response variable(s), e.g. `:obs_gdp`
-
-**Methods 1 and 2 only:**
-
-- `m::AbstractModel`
 - `class::Symbol`
 - `input_type::Symbol`
 - `cond_type::Symbol`
 
-**Method 3 only:**
-
-- `mb::MeansBands`
-
 ### Keyword Arguments
-
-- `bands_pct::Vector{String}`
-- `output_file::String`: if specified, plot will be saved there as a PDF
-- `title::String` or `titles::Vector{String}`
-- `flip::Bool`: whether to flip the sign of the impulse response while plotting
-- `verbose::Symbol`
-
-**Methods 1 and 2 only:**
 
 - `forecast_string::String`
 - `plotroot::String`: if nonempty, plots will be saved in that directory
+- `title::String` or `titles::Vector{String}`
+- `verbose::Symbol`
 
-**Method 3 only:**
-
-- `output_file::String`: if nonempty, plot will be saved in that path
+See `?irf` for additional keyword arguments, all of which can be passed
+into `plot_history_and_forecast`.
 
 ### Output
 
@@ -54,14 +35,10 @@ Plot the responses of `var` to `shock` from `mb`, possibly read in using
 """
 function plot_impulse_response(m::AbstractModel, shock::Symbol, var::Symbol, class::Symbol,
                                input_type::Symbol, cond_type::Symbol;
-                               forecast_string::String = "",
-                               plotroot::String = figurespath(m, "forecast"),
                                title::String = "",
                                kwargs...)
 
     plots = plot_impulse_response(m, shock, [var], class, input_type, cond_type;
-                                  forecast_string = forecast_string,
-                                  plotroot = plotroot,
                                   titles = isempty(title) ? String[] : [title],
                                   kwargs...)
     return plots[var]
@@ -85,50 +62,17 @@ function plot_impulse_response(m::AbstractModel, shock::Symbol, vars::Vector{Sym
     # Loop through variables
     plots = OrderedDict{Symbol, Plots.Plot}()
     for (var, title) in zip(vars, titles)
-        output_file = if isempty(plotroot)
-            ""
-        else
-            get_forecast_filename(plotroot, filestring_base(m), input_type, cond_type,
-                                  Symbol("irf_", shock, "_", detexify(var)),
-                                  forecast_string = forecast_string,
-                                  fileformat = plot_extension())
-        end
+        # Call recipe
+        plots[var] = irf(shock, var, mb; title = title, kwargs...)
 
-        plots[var] = plot_impulse_response(shock, var, mb;
-                                           output_file = output_file, title = title,
-                                           kwargs...)
+        # Save plot
+        if !isempty(plotroot)
+            output_file = get_forecast_filename(plotroot, filestring_base(m), input_type, cond_type,
+                                                Symbol("irf_", shock, "_", detexify(var)),
+                                                forecast_string = forecast_string,
+                                                fileformat = plot_extension())
+            save_plot(plots[var], output_file, verbose = verbose)
+        end
     end
     return plots
-end
-
-function plot_impulse_response(shock::Symbol, var::Symbol, mb::MeansBands;
-                               bands_pcts::Vector{String} = ["90.0%"],
-                               output_file::String = "",
-                               title::String = "",
-                               flip::Bool = false,
-                               verbose::Symbol = :low)
-
-    # Plot title
-    if isempty(title)
-        title = string(var)
-    end
-    p = plot(title = title)
-
-    varshock = Symbol("$(var)__$(shock)")
-    sign = flip ? -1 : 1
-
-    # Plot bands
-    for pct in bands_pcts
-        plot!(p, sign * mb.bands[varshock][Symbol("$pct UB")],
-              fillto = sign * mb.bands[varshock][Symbol("$pct LB")],
-              label = "", color = :blue, α = 0.10)
-    end
-
-    # Plot mean
-    plot!(p, sign * mb.means[varshock], label = "", linewidth = 2, linecolor = :black)
-
-    # Save if `output_file` provided
-    save_plot(p, output_file, verbose = verbose)
-
-    return p
 end
