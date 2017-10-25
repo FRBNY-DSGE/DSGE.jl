8a192c921cdf312110b7cbf3492ae7bb05de0a8d
Pearl Li <pearlzli16@gmail.com>
2017-10-25 10:58:07 -0400

    Allow input_type, cond_type, forecast_string to vary in plot_forecast_comparison

--- src/plot/plot_forecast_comparison.jl
+++ src/plot/plot_forecast_comparison.jl
@@ -4,7 +4,9 @@ plot_forecast_comparison(m_old, m_new, var, class, input_type, cond_type;
     title = "", kwargs...)
 
 plot_forecast_comparison(m_old, m_new, vars, class, input_type, cond_type;
-    forecast_string = "", bdd_and_unbdd = false, bands_pcts = [\"90.0%\"],
+    input_type_old = input_type, cond_type_old = cond_type, 
+    forecast_string = "", forecast_string_old = forecast_string, 
+    bdd_and_unbdd = false, bands_pcts = [\"90.0%\"],
     old_hist_label = "", new_hist_label = "",
     old_forecast_label = \"Old Forecast\", new_forecast_label = \"New Forecast\",
     old_hist_color = :grey, new_hist_color = :black,
@@ -26,7 +28,10 @@ Plot forecasts from `m_old` and `m_new` of `var` or `vars`.
 
 ### Keyword Arguments
 
+- `input_type_old::Symbol`
+- `cond_type_old::Symbol`
 - `forecast_string::String`
+- `forecast_string_old::String`
 - `bdd_and_unbdd::Bool`: if true, then unbounded means and bounded bands are plotted
 - `bands_pcts::Vector{String}`: which bands to plot
 - `old_hist_label::String`
@@ -60,7 +65,10 @@ end
 function plot_forecast_comparison(m_old::AbstractModel, m_new::AbstractModel,
                                   vars::Vector{Symbol}, class::Symbol,
                                   input_type::Symbol, cond_type::Symbol;
+                                  input_type_old::Symbol = input_type,
+                                  cond_type_old::Symbol = cond_type,
                                   forecast_string::String = "",
+                                  forecast_string_old::String = forecast_string,
                                   bdd_and_unbdd::Bool = false,
                                   bands_pcts::Vector{String} = ["90.0%"],
                                   old_hist_label::String = "",
@@ -76,12 +84,12 @@ function plot_forecast_comparison(m_old::AbstractModel, m_new::AbstractModel,
                                   verbose::Symbol = :low,
                                   kwargs...)
     # Read in MeansBands
-    histold  = read_mb(m_old, input_type, cond_type, Symbol(:hist, class),
-                       forecast_string = forecast_string)
-    forecastold = read_mb(m_old, input_type, cond_type, Symbol(:forecast, class),
-                       forecast_string = forecast_string, bdd_and_unbdd = bdd_and_unbdd)
-    histnew  = read_mb(m_new, input_type, cond_type, Symbol(:hist, class),
-                       forecast_string = forecast_string)
+    histold = read_mb(m_old, input_type_old, cond_type_old, Symbol(:hist, class),
+                      forecast_string = forecast_string_old)
+    forecastold = read_mb(m_old, input_type_old, cond_type_old, Symbol(:forecast, class),
+                       forecast_string = forecast_string_old, bdd_and_unbdd = bdd_and_unbdd)
+    histnew = read_mb(m_new, input_type, cond_type, Symbol(:hist, class),
+                      forecast_string = forecast_string)
     forecastnew = read_mb(m_new, input_type, cond_type, Symbol(:forecast, class),
                        forecast_string = forecast_string, bdd_and_unbdd = bdd_and_unbdd)
 
