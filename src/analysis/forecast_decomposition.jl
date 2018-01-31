88f9125c41428fd87f0bc5ee48a8910fe91f1239
Abhi Gupta <abhig94@gmail.com>
2018-01-31 14:43:37 -0500

    Correct forecast decomposition in conditional cases, general clean up

--- src/analysis/forecast_decomposition.jl
+++ src/analysis/forecast_decomposition.jl
@@ -1,6 +1,6 @@
 """
 ```
-decompose_forecast_one_draw(model, df_new, df_old, params)
+decompose_forecast_one_draw(model, df_new, df_old, params_new, params_old, k, cond_type)
 ```
 
 Returns reestimation, state_updates, realized_shocks, data_revisions, overall_change
@@ -17,45 +17,52 @@ function decompose_forecast_one_draw(model::AbstractModel, df_new::DataFrame, df
 
     # Filter and smooth
     kal_new = DSGE.filter(model, df_new, sys_new; cond_type = cond_type) # |T, new para
-    kal_mix = DSGE.filter(model, df_new, sys_old; cond_type = cond_type) # |T, old para
-    kal_two = DSGE.filter(model, df_old, sys_new; cond_type = cond_type) # |T-K, new para
+    kal_mix = DSGE.filter(model, df_old, sys_new; cond_type = cond_type) # |T-K, new para
     kal_old = DSGE.filter(model, df_old, sys_old; cond_type = cond_type) # |T-k, old para
 
-    histstates_mix, histshocks_mix, _, _ = DSGE.smooth(model, df_new, sys_old, kal_mix; draw_states = false)
-    histstates_two, histshocks_two, _, _ = DSGE.smooth(model, df_old, sys_new, kal_two; draw_states = false)
-    histstates_new, histshocks_new, _, _ = DSGE.smooth(model, df_new, sys_new, kal_two; draw_states = false)
+    histstates, histshocks, _, _ = DSGE.smooth(model, df_new, sys_new, kal_new; draw_states = false, cond_type = cond_type)
 
     # Compute decomposition
-    reestimation    = zeros(n_observables(model), forecast_horizons(model))
-    data_revisions  = zeros(n_observables(model), forecast_horizons(model))
-    state_updates   = zeros(n_observables(model), forecast_horizons(model))
-    realized_shocks = zeros(n_observables(model), forecast_horizons(model))
-    overall_change  = zeros(n_observables(model), forecast_horizons(model))
+    n_obs    = n_observables(model)
+    n_shocks = n_shocks_exogenous(model)
+    n_states = n_states_augmented(model)
+
+    reestimation    = zeros(n_obs, forecast_horizons(model))
+    data_revisions  = zeros(n_obs, forecast_horizons(model))
+    state_updates   = zeros(n_obs, forecast_horizons(model))
+    realized_shocks = zeros(n_obs, forecast_horizons(model), n_shocks)
+    overall_change  = zeros(n_obs, forecast_horizons(model))
 
     Z, T, R, D, C = sys_old[:ZZ], sys_old[:TTT], sys_old[:RRR], sys_old[:DD], sys_old[:CCC]
     Z_new, T_new, R_new, D_new, C_new = sys_new[:ZZ], sys_new[:TTT], sys_new[:RRR], sys_new[:DD], sys_new[:CCC]
 
+    c = cond_type in [:semi, :full] ? 1 : 0
     partial_sum(M::Matrix, k::Int) = k <= 0 ? eye(size(M)...) : M^k + partial_sum(M, k-1)
 
-    # new decomposition
-    state_component(j)    = Z_new * T_new^(j+k) * (histstates_new[:,end-k] - kal_new[:filt][:,end-k])
-    revision_component(j) = Z_new * T_new^(j+k) * (kal_new[:filt][:,end-k] - kal_two[:zend])
-    para_component(j)     = Z_new * (T_new^(j+k) * kal_two[:zend] + partial_sum(T_new,j+k-1) * C_new) - Z * (T^(j+k) * kal_old[:zend] + partial_sum(T,j+k-1) * C) + (D_new - D)
-    all_components(j)     = Z_new * (T_new^j * kal_new[:zend] + C_new) - Z * (T^(j+k) * kal_old[:zend] + C) + (D_new - D)
-    function shock_component(j::Int)
-        total = zeros(n_states_augmented(model), 1)
-        for i in 0:k-1
-            total += T_new^(j+i) * R_new * histshocks_new[:,end-i]
+    state_component(h)    = Z_new * T_new^(h+k) * (histstates[:,end-k] - kal_new[:filt][:,end-k])
+    revision_component(h) = Z_new * T_new^(h+k) * (kal_new[:filt][:,end-k] - kal_mix[:filt][:,end])
+    para_component(h)     = Z_new * (T_new^(h+k) * kal_mix[:filt][:,end] + partial_sum(T_new,h+k) * C_new) - Z * (T^(h+k) * kal_old[:filt][:,end] + partial_sum(T,h+k) * C) + (D_new - D)
+    all_components(h)     = Z_new * (T_new^h * kal_new[:zend] + C_new) - Z * (T^(h+k) * kal_old[:zend] + C) + (D_new - D)
+    function shock_component(h::Int)
+        total = zeros(n_obs, 1, n_shocks)
+        for s in 1:n_shocks
+            shock_total = zeros(n_states, 1)
+            shock = zeros(n_shocks, 1)
+            for i in 0:k-1
+                shock[s,1] = histshocks[s,end-i]
+                shock_total += T_new^(h+i) * R_new * shock
+            end
+            total[:,1,s] = Z_new * shock_total
         end
-        return Z_new * total
+        return total
     end
 
     for t in 1:forecast_horizons(model)
-        reestimation[:,t]    = para_component(t)
-        state_updates[:,t]   = state_component(t)
-        realized_shocks[:,t] = shock_component(t)
-        data_revisions[:,t]  = revision_component(t)
-        overall_change[:,t]  = all_components(t)
+        reestimation[:,t]      = para_component(t-c)
+        state_updates[:,t]     = state_component(t-c)
+        realized_shocks[:,t,:] = shock_component(t-c)
+        data_revisions[:,t]    = revision_component(t-c)
+        overall_change[:,t]    = all_components(t-c)
     end
 
     return reestimation, state_updates, realized_shocks, data_revisions, overall_change
@@ -65,13 +72,14 @@ end
 
 """
 ```
+decompose_forecast_changes(m_new, m_old, input_type, cond_type;
+                           verbose = :low, transform = false,
+                           format_as_DataFrame= true)
+
 decompose_forecast_changes(m_new, m_old, df_new, df_old, input_type, cond_type;
                            verbose = :low, transform = false,
                            format_as_DataFrame= true)
 
-decompose_forecast_changes(m_new, m_old, input_type, cond_type;
-                           verbose = :low, transform = false,
-                           format_as_DataFrame= true)
 ```
 
 Decompose the difference between forecasts in `yr`:`quarter` and the subsequent quarter,
@@ -102,14 +110,14 @@ function decompose_forecast_changes(m_new::AbstractModel, m_old::AbstractModel,
     # calculate offset
     new_start = date_forecast_start(m_new)
     old_start = date_forecast_start(m_old)
-    quarter_diff = quarter_date_to_number(new_start) - quarter_date_to_number(old_start)
+    quarter_diff = DSGE.quarter_date_to_number(new_start) - DSGE.quarter_date_to_number(old_start)
     offset = Int(4 * quarter_diff)
     @assert offset >= 0 "The vintage of m_new must not be older than the vintage of m_old"
 
     # Initialize results matrices
     reestimation    = zeros(n_observables(m_new), forecast_horizons(m_new))
     state_updates   = zeros(n_observables(m_new), forecast_horizons(m_new))
-    realized_shocks = zeros(n_observables(m_new), forecast_horizons(m_new))
+    realized_shocks = zeros(n_observables(m_new), forecast_horizons(m_new), n_shocks_exogenous(m_new))
     data_revisions  = zeros(n_observables(m_new), forecast_horizons(m_new))
     overall_change  = zeros(n_observables(m_new), forecast_horizons(m_new))
 
@@ -157,7 +165,7 @@ function decompose_forecast_changes(m_new::AbstractModel, m_old::AbstractModel,
             end
 
             # Add results from this block to results matrices
-            reestimation    = reduce(+, state_updates, map(x->x[1], forecast_outputs))
+            reestimation    = reduce(+, reestimation, map(x->x[1], forecast_outputs))
             state_updates   = reduce(+, state_updates, map(x->x[2], forecast_outputs))
             realized_shocks = reduce(+, realized_shocks, map(x->x[3], forecast_outputs))
             data_revisions  = reduce(+, data_revisions, map(x->x[4], forecast_outputs))
@@ -195,37 +203,71 @@ function decompose_forecast_changes(m_new::AbstractModel, m_old::AbstractModel,
     if transform
         # quartertoannual works as a first approximation (ignoring per-capita
         # concerns) to most reverse transformations
-        reestimation    = quartertoannual(reestimation)
-        state_updates   = quartertoannual(state_updates)
-        realized_shocks = quartertoannual(realized_shocks)
-        data_revisions  = quartertoannual(data_revisions)
-        overall_change  = quartertoannual(overall_change)
+        reestimation    = DSGE.quartertoannual(reestimation)
+        state_updates   = DSGE.quartertoannual(state_updates)
+        realized_shocks = DSGE.quartertoannual(realized_shocks)
+        data_revisions  = DSGE.quartertoannual(data_revisions)
+        overall_change  = DSGE.quartertoannual(overall_change)
     end
 
     if format_as_DataFrame
-        function forecast_mat_to_df(mat, init_df, var_names)
-            df = deepcopy(init_df)
-            for (i,var_name) in enumerate(var_names)
-                df[var_name] = vec(mat[i,:])
+        n_periods  = forecast_horizons(m_new)
+        start_date = date_forecast_start(m_new)
+        end_date   = iterate_quarters(start_date, n_periods - 1)
+        var_names = sort(m_new.observables, by=x->m_new.observables[x]).keys
+
+        function forecast_mat_to_df(mat, names)
+            df = DataFrame()
+            df[:date] = DSGE.quarter_range(start_date, end_date)
+
+            for (i,name) in enumerate(names)
+                df[name] = vec(mat[i,:])
             end
             return df
         end
 
-        df = DataFrame()
-        n_periods = size(state_updates, 2)
-        start_date = date_forecast_start(m_new)
-        end_date = iterate_quarters(start_date, n_periods - 1)
-        df[:date] = DSGE.quarter_range(start_date, end_date)
-        var_names = sort(m_new.observables, by=x->m_new.observables[x]).keys
+        reestimation    = forecast_mat_to_df(reestimation, var_names)
+        state_updates   = forecast_mat_to_df(state_updates, var_names)
+        data_revisions  = forecast_mat_to_df(data_revisions, var_names)
+        overall_change  = forecast_mat_to_df(overall_change, var_names)
 
-        reestimation    = forecast_mat_to_df(reestimation, df, var_names)
-        state_updates   = forecast_mat_to_df(state_updates, df, var_names)
-        realized_shocks = forecast_mat_to_df(realized_shocks, df, var_names)
-        data_revisions  = forecast_mat_to_df(data_revisions, df, var_names)
-        overall_change  = forecast_mat_to_df(overall_change, df, var_names)
+        if false
+            realized_shocks = forecast_mat_to_df(sum(realized_shocks, 3), df, var_names)
+        else
+            realized_shocks_new = Vector{DataFrames.DataFrame}(n_shocks_exogenous(m_new))
+            for s in 1:n_shocks_exogenous(m_new)
+                new_var_names = map(x -> Symbol(x, :_, m_new.exogenous_shocks.keys[s]), var_names)
+                realized_shocks_new[s] = forecast_mat_to_df(realized_shocks[:,:,s], new_var_names)
+            end
 
+            realized_shocks = realized_shocks_new
+        end
     end
 
-
 return reestimation, state_updates, realized_shocks, data_revisions, overall_change
+end
+
+
+function collapse_shocks(shocks::Vector{DataFrames.DataFrame}, m::AbstractModel; cond_type::Symbol = :none)
+    n_shocks = n_shocks_exogenous(m)
+    var_names = sort(m.observables, by=x->m.observables[x]).keys
+
+    df = DataFrame()
+    n_periods = size(shocks[1],1)
+    start_date = date_forecast_start(m_new)
+    end_date = iterate_quarters(start_date, n_periods - 1)
+    df[:date] = DSGE.quarter_range(start_date, end_date)
+
+    for (i,var) in enumerate(var_names)
+        total = Vector{Float64}(size(shocks[1],1))
+        for s in 1:n_shocks
+            total += Vector(shocks[s][:,i+1])
+        end
+        df[var] = total
+    end
+    return df
+end
+
+function collapse_shocks(shocks::Array{Float64,3})
+    return squeeze(sum(shocks,3),3)
 end
