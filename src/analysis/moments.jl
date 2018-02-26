313390737fa8f393145e8029d1f07b7a4e0bbd8e
Michael Cai <michaeldcai@gmail.com>
2018-02-26 14:38:08 -0500

    Remove unnecessary get_forecast_input_file call

--- src/analysis/moments.jl
+++ src/analysis/moments.jl
@@ -30,7 +30,6 @@ function moment_tables(m::AbstractModel; percent::AbstractFloat = 0.90,
 
     ### 1. Load parameter draws from Metropolis-Hastings
 
-    filename = get_forecast_input_file(m, :full)
     params = if !isempty(subset_inds)
         # Use subset of draws
         if isempty(subset_string)
@@ -41,7 +40,6 @@ function moment_tables(m::AbstractModel; percent::AbstractFloat = 0.90,
         # Use all draws
         load_draws(m, :full; verbose = verbose)
     end
-
 
     ### 2. Compute posterior moments
 
