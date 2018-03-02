a5293257c62048e4090d0395f87a58478b880d5c
Michael Cai <michaeldcai@gmail.com>
2018-03-02 17:16:40 -0500

    Add shock groupings for m990 (basically the same as m1002)

--- src/models/m990/m990.jl
+++ src/models/m990/m990.jl
@@ -676,3 +676,29 @@ function settings_m990!(m::Model990)
     # Forecast
     m <= Setting(:shockdec_startdate, Nullable(quartertodate("2007-Q1")))
 end
+
+"""
+```
+shock_groupings(m::Model990)
+```
+
+Returns a `Vector{ShockGroup}`, which must be passed in to
+`plot_shock_decomposition`. See `?ShockGroup` for details.
+"""
+function shock_groupings(m::Model990)
+    gov = ShockGroup("g", [:g_sh], RGB(0.70, 0.13, 0.13)) # firebrick
+    bet = ShockGroup("b", [:b_sh], RGB(0.3, 0.3, 1.0))
+    fin = ShockGroup("FF", [:γ_sh, :μ_e_sh, :σ_ω_sh], RGB(0.29, 0.0, 0.51)) # indigo
+    tfp = ShockGroup("z", [:z_sh], RGB(1.0, 0.55, 0.0)) # darkorange
+    pmu = ShockGroup("p-mkp", [:λ_f_sh], RGB(0.60, 0.80, 0.20)) # yellowgreen
+    wmu = ShockGroup("w-mkp", [:λ_w_sh], RGB(0.0, 0.5, 0.5)) # teal
+    pol = ShockGroup("pol", vcat([:rm_sh], [Symbol("rm_shl$i") for i = 1:n_anticipated_shocks(m)]),
+                     RGB(1.0, 0.84, 0.0)) # gold
+    pis = ShockGroup("pi-LR", [:π_star_sh], RGB(1.0, 0.75, 0.793)) # pink
+    mei = ShockGroup("mu", [:μ_sh], :cyan)
+    mea = ShockGroup("me", [:lr_sh, :tfp_sh, :gdpdef_sh, :corepce_sh], RGB(0.0, 0.8, 0.0))
+    zpe = ShockGroup("zp", [:zp_sh], RGB(0.0, 0.3, 0.0))
+    det = ShockGroup("dt", [:dettrend], :gray40)
+
+    return [gov, bet, fin, tfp, pmu, wmu, pol, pis, mei, mea, zpe, det]
+end
