8d4a0b21656727f97351201f5f1c79dc6910352a
Pearl Li <pearlzli16@gmail.com>
2018-05-04 17:49:47 -0400

    Use which_density_bands as default bands_pcts in irf

--- src/plot/plot_impulse_response.jl
+++ src/plot/plot_impulse_response.jl
@@ -113,7 +113,7 @@ irf
                    label_mean_bands = false,
                    mean_color = :black,
                    bands_color = :blue,
-                   bands_pcts = ["90.0%"])
+                   bands_pcts = which_density_bands(irf.args[3]))
     # Error checking
     if length(irf.args) != 3 || typeof(irf.args[1]) != Symbol || typeof(irf.args[2]) != Symbol ||
         typeof(irf.args[3]) != MeansBands
