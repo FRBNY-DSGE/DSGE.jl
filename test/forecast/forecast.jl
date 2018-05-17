5b18c008649934f9aa90c4bddad53277ddf57cb3
Pearl Li <pearlzli16@gmail.com>
2018-05-17 12:06:48 -0400

    Start renaming z0, P0 -> s_0, P_0

--- test/forecast/forecast.jl
+++ test/forecast/forecast.jl
@@ -10,7 +10,7 @@ m <= Setting(:forecast_horizons, 1)
 system, kal = jldopen("$path/../reference/forecast_args.jld","r") do file
     read(file, "system"), read(file, "kalman")
 end
-z0 = zeros(n_states_augmented(m))
+s_0 = zeros(n_states_augmented(m))
 
 # Read expected output
 exp_states, exp_obs, exp_pseudo, exp_shocks =
@@ -22,7 +22,7 @@ exp_states, exp_obs, exp_pseudo, exp_shocks =
     end
 
 # Without shocks
-states, obs, pseudo, shocks = forecast(m, system, z0; draw_shocks = false)
+states, obs, pseudo, shocks = forecast(m, system, s_0; draw_shocks = false)
 
 @test_matrix_approx_eq exp_states states
 @test_matrix_approx_eq exp_obs    obs
@@ -30,7 +30,7 @@ states, obs, pseudo, shocks = forecast(m, system, z0; draw_shocks = false)
 @test_matrix_approx_eq exp_shocks shocks
 
 # Supplying shocks
-states, obs, pseudo, shocks = forecast(m, system, z0; shocks = shocks)
+states, obs, pseudo, shocks = forecast(m, system, s_0; shocks = shocks)
 
 @test_matrix_approx_eq exp_states states
 @test_matrix_approx_eq exp_obs    obs
@@ -38,11 +38,11 @@ states, obs, pseudo, shocks = forecast(m, system, z0; shocks = shocks)
 @test_matrix_approx_eq exp_shocks shocks
 
 # Draw normally distributed shocks
-states, obs, pseudo, shocks = forecast(m, system, z0; draw_shocks = true)
+states, obs, pseudo, shocks = forecast(m, system, s_0; draw_shocks = true)
 
 # Draw t-distributed shocks
 m <= Setting(:forecast_tdist_shocks, true)
-states, obs, pseudo, shocks = forecast(m, system, z0; draw_shocks = true)
+states, obs, pseudo, shocks = forecast(m, system, s_0; draw_shocks = true)
 m <= Setting(:forecast_tdist_shocks, false)
 
 # Enforce ZLB
@@ -52,10 +52,10 @@ zlb_value = forecast_zlb_value(m)
 shocks = zeros(n_shocks_exogenous(m), forecast_horizons(m))
 shocks[ind_r_sh, :] = -10.
 
-states, obs, pseudo, shocks = forecast(m, system, z0; shocks = shocks)
+states, obs, pseudo, shocks = forecast(m, system, s_0; shocks = shocks)
 @assert all(x -> x < zlb_value, obs[ind_r, :])
 
-states, obs, pseudo, shocks = forecast(m, system, z0; shocks = shocks, enforce_zlb = true)
+states, obs, pseudo, shocks = forecast(m, system, s_0; shocks = shocks, enforce_zlb = true)
 @assert all(x -> abs(x - zlb_value) < 0.01, obs[ind_r, :])
 @assert all(x -> x != -10.,                 shocks[ind_r_sh, :])
 
