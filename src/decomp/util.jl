bb1161b397dfa88cc4fabef1a56bbbeff30ea896
Pearl Li <pearlzli16@gmail.com>
2018-05-22 17:01:13 -0400

    Break out decomp checking into separate functions

--- src/decomp/util.jl
+++ src/decomp/util.jl
@@ -26,3 +26,60 @@ function compute_history_and_forecast(m::AbstractModel, df::DataFrame, class::Sy
 
     return hcat(hist, fcast[class])
 end
+
+function check_total_decomp(m_new::AbstractModel, m_old::AbstractModel,
+                            df_new::DataFrame, df_old::DataFrame,
+                            cond_new::Symbol, cond_old::Symbol, classes::Vector{Symbol},
+                            decomp::Dict{Symbol, Array{Float64}};
+                            hs = 1:forecast_horizons(m_old),
+                            atol::Float64 = 1e-8)
+    correct = true
+    for class in classes
+        # y^{new,new}_{t|T}, t = 1:T+H
+        y_new = compute_history_and_forecast(m_new, df_new, class, cond_type = cond_new)
+
+        # y^{old,old}_{t|T-k}, t = 1:T-k+H
+        y_old = compute_history_and_forecast(m_old, df_old, class, cond_type = cond_old)
+
+        # Total difference = y^{new,new}_{T-k+h|T} - y^{old,old}_{T-k+h|T-k}
+        y_new_Tmkph_T   = y_new[:, T-k+hs]
+        y_old_Tmkph_Tmk = y_old[:, T-k+hs]
+        exp_total = y_new_Tmkph_T - y_old_Tmkph_Tmk
+
+        # Compare sum of decomposed differences to expected total difference
+        if !isapprox(exp_total, decomp[Symbol(:decomptotal, class)], atol = atol)
+            @show decomp[Symbol(:decomptotal, class)]
+            @show exp_total
+            correct = false
+        end
+    end
+    return correct
+end
+
+function check_states_shocks_decomp(sys_new::System, s_tgt::Matrix{Float64}, s_tgT::Matrix{Float64},
+                                    class::Symbol, k::Int, h::Int,
+                                    state_comp::Vector{Float64}, shock_comp::Vector{Float64};
+                                    atol::Float64 = 1e-8)
+    ZZ, DD, TTT, RRR, CCC = class_system_matrices(sys_new, class)
+
+    # y_{T-k+h|T}
+    y_Tmkph_T = if h <= k
+        ZZ * (s_tgT[:, end-k+h] + CCC) + DD # history
+    else
+        ZZ * (TTT^(h-k) * s_tgt[:, end] + CCC) + DD # forecast
+    end
+
+    # y_{T-k+h|T-k}
+    y_Tmkph_Tmk = ZZ * (TTT^h * s_tgt[:, end-k] + CCC) + DD
+
+    # State and shock components = y_{T-k+h|T} - y_{T-k+h|T-k}
+    state_shock_comp = y_Tmkph_T - y_Tmkph_Tmk
+
+    if isapprox(state_shock_comp, state_comp + shock_comp, atol = atol)
+        return true
+    else
+        @show state_comp + shock_comp
+        @show state_shock_comp
+        return false
+    end
+end
