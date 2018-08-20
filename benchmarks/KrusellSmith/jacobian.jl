3214293f4a2e8b1db0e1ebd68b96de623b11525d
Michael Cai <michaeldcai@gmail.com>
2018-08-20 14:01:26 -0400

    Fix filepaths for KrusellSmith benchmarking files

--- benchmarks/KrusellSmith/jacobian.jl
+++ benchmarks/KrusellSmith/jacobian.jl
@@ -2,13 +2,15 @@ using DSGE
 using BenchmarkTools, JLD
 import DSGE: jacobian
 
+path = dirname(@__FILE__)
+
 # Benchmark current function
 m = KrusellSmith()
 jacobian(m)
 trial = @benchmark jacobian($m) gcsample = true
 
 # Compute time differential
-print_all_benchmarks(trial, "../reference/jacobian.jld", "jacobian")
+print_all_benchmarks(trial, "$path/../reference/jacobian.jld", "jacobian")
 
 # Optionally over-write the existing reference trial
 # write_ref_trial(trial, "jacobian")
--- benchmarks/KrusellSmith/steady_state.jl
+++ benchmarks/KrusellSmith/steady_state.jl
@@ -1,13 +1,15 @@
 using DSGE
 using BenchmarkTools, JLD
 
+path = dirname(@__FILE__)
+
 # Benchmark current function
 m = KrusellSmith()
 steadystate!(m)
 trial = @benchmark steadystate!($m) gcsample = true
 
 # Compute time differential
-print_all_benchmarks(trial, "ref/steady_state.jld", "steady_state")
+print_all_benchmarks(trial, "$path/../reference/steady_state.jld", "steady_state")
 
 # Optionally over-write the existing reference trial
 # write_ref_trial(trial, "steady_state")
