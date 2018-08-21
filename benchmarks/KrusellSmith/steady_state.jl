4a61f0df24826b57d217a26076fb164072e823ec
Michael Cai <michaeldcai@gmail.com>
2018-08-21 09:22:15 -0400

    Write KrusellSmith steady state benchmark

--- benchmarks/KrusellSmith/steady_state.jl
+++ benchmarks/KrusellSmith/steady_state.jl
@@ -8,8 +8,8 @@ m = KrusellSmith()
 steadystate!(m)
 trial = @benchmark steadystate!($m) gcsample = true
 
+# # Optionally over-write the existing reference trial
+# write_ref_trial(trial, "steady_state")
+
 # Compute time differential
 print_all_benchmarks(trial, "$path/../reference/steady_state.jld", "steady_state")
-
-# Optionally over-write the existing reference trial
-# write_ref_trial(trial, "steady_state")
