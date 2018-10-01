b1e37d5876cd3d481423f8d1a479f2f0c6404074
Michael Cai <michaeldcai@gmail.com>
2018-10-01 14:31:22 -0400

    Implement measurement equation for GDP. Needs to be cleaned up

--- src/models/heterogeneous_agent/bond_labor/measurement.jl
+++ src/models/heterogeneous_agent/bond_labor/measurement.jl
@@ -22,11 +22,13 @@ Cov(ϵ_t, u_t) = 0
 """
 function measurement{T<:AbstractFloat}(m::BondLabor{T}, TTT::Matrix{T},
                                        RRR::Matrix{T}, CCC::Vector{T})
-    endo      = m.endogenous_states
+    endo      = m.endogenous_states_unnormalized
     exo       = m.exogenous_shocks
     obs       = m.observables
 
     _n_model_states = n_model_states(m)
+    _n_states       = n_backward_looking_states(m)
+    _n_jumps        = n_jumps(m)
 
     _n_observables = n_observables(m)
     _n_shocks_exogenous = n_shocks_exogenous(m)
@@ -36,9 +38,66 @@ function measurement{T<:AbstractFloat}(m::BondLabor{T}, TTT::Matrix{T},
     EE = zeros(_n_observables, _n_observables)
     QQ = zeros(_n_shocks_exogenous, _n_shocks_exogenous)
 
+    nx = get_setting(m, :nx)
+    ns = get_setting(m, :ns)
+
+    ν   = m[:ν]
+    γ   = m[:γ]
+    abar = m[:abar]
+    R    = m[:R]
+
+    l   = m[:lstar].value
+    μ   = m[:μstar].value
+    c   = m[:cstar].value
+    η   = m[:ηstar].value
+    χss = m[:χstar].value
+
+    xgrid = m.grids[:xgrid].points
+    xgrid_total = m.grids[:xgrid_total]
+    xwts  = m.grids[:xgrid].weights
+    sgrid = m.grids[:sgrid].points
+    sgrid_total = m.grids[:sgrid_total]
+    swts  = m.grids[:sgrid].weights
+    weights_total = m.grids[:weights_total]
+
+    aborrow = abar/R
+    chipW = (1+ν)./(ν./(aborrow + χss - xgrid_total) + γ./χss)
+    chipR = (1/(R*R))*(ν*abar./(aborrow + χss - xgrid_total))./(ν./(aborrow + χss - xgrid_total) + γ./χss)
+
+    GDP  = 0.0
+    GDPZ = 0.0
+    GDPR = 0.0
+
+    GDPfn = zeros(1,2*ns*nx +2) # GDP as function of un-normalized MU Z l R
+    for ix =1:nx
+        for is=1:ns
+            i = ix + nx*(is-1)
+            GDP += μ[i]*swts[is]*xwts[ix]*η[i]
+            GDPZ += sgrid[is]*μ[i]*swts[is]*xwts[ix]*(η[i]/ν - ((γ*η[i])/(ν*c[i]))*chipW[i]*((l[i]^(-1/γ))>χss[i]) )
+            GDPR += -(sgrid[is]*μ[i]*γ*η[i]/(ν*c[i]))*swts[is]*xwts[ix]*chipR[i]*((l[i]^(-1/γ))>χss[i])
+        end
+    end
+
     # Output in log levels
-    ZZ[obs[:obs_gdp], endo[:z′_t]]   = (m[:Kstar]^m[:α])*(m[:Lstar]^(1.0-m[:α]))
-    ZZ[obs[:obs_gdp], endo[:K′_t]]   = (m[:α]/m[:Kstar])*(m[:Kstar]^m[:α])*(m[:Lstar]^(1.0-m[:α]))
+    GDPfn[1, endo[:μ′_t]] = η'.*sgrid_total'.*weights_total'
+    GDPfn[1, endo[:z′_t]] = GDPZ + GDP
+    GDPfn[1, endo[:l′_t]] = (1/γ)*μ'.*(sgrid_total'*(γ/ν).*(η./c)').*weights_total'.*( l.^(-1-1/γ) )'.*((l.^(-1/γ)).<=χss)'
+    GDPfn[1, endo[:R′_t]] = GDPR
+
+    Qx, Qy, _, _ = compose_normalization_matrices(m)
+    # Re-compute the TTT_jump matrix
+    # Kind of slow.. but a little more convenient than carrying around TTT_state from solve
+    # Refactor this to be better
+    TTT_jump_mul_state = TTT[n_backward_looking_states(m)+1:end, 1:n_backward_looking_states(m)]
+    TTT_state = TTT[1:n_backward_looking_states(m), 1:n_backward_looking_states(m)]
+    gx2  = Qy'*(TTT_jump_mul_state*inv(TTT_state))*Qx
+
+    # now we need to create GDP as a function of the normalized states
+    ZZ_states = (1/GDP)*GDPfn*[eye(nx*ns+1); gx2]*Qx' # this is for log GDP
+                                                      # to use the level of gdp, remove (1/GDP)
+    ZZ = Matrix{Float64}(_n_observables, _n_model_states)
+    ZZ[1:_n_states] = ZZ_states
+    ZZ[_n_states+1:end] = zeros(_n_jumps)
 
     # Measurement error
     EE[obs[:obs_gdp], obs[:obs_gdp]] = m[:e_y]
