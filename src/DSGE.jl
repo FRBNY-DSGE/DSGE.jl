fcfe8f054feede47e1738bdb45ea1463e184470b
Michael Cai <michaeldcai@gmail.com>
2018-10-24 16:29:06 -0400

    Add OrderedCollections

--- src/DSGE.jl
+++ src/DSGE.jl
@@ -2,8 +2,9 @@ isdefined(Base, :__precompile__) && __precompile__()
 
 module DSGE
     using Dates, Test, BenchmarkTools
-    using CSV, DataFrames, Distributed, Distributions, FileIO, FredData, HDF5, JLD2, LinearAlgebra, Missings, Nullables, Optim, Printf, Random, RecipesBase, SparseArrays, SpecialFunctions, StateSpaceRoutines, StatPlots, Dates
-    using DataStructures: SortedDict, insert!, ForwardOrdering, OrderedDict
+    using Distributed, Distributions, FileIO, FredData, HDF5, JLD2, LinearAlgebra, Missings, Nullables, Optim, Printf, Random, RecipesBase, SparseArrays, SpecialFunctions, StateSpaceRoutines, StatPlots
+    using CSV, DataFrames, OrderedCollections
+    using DataStructures: SortedDict, insert!, ForwardOrdering
     using QuantEcon: solve_discrete_lyapunov
     using Roots: fzero, ConvergenceFailed
     using StatsBase: sample
