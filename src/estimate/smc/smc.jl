506237c571c9b0cf01c517c866b976565ed81a33
Michael Cai <michaeldcai@gmail.com>
2018-12-11 14:06:17 -0500

    Uncomment temporary testing override of previous_data_vintage check

--- src/estimate/smc/smc.jl
+++ src/estimate/smc/smc.jl
@@ -60,12 +60,11 @@ function smc(m::AbstractModel, data::Matrix;
     # Time Tempering
     tempered_update = !isempty(old_data)
 
-    # TEMPORARILY DISABLE FOR TESTING.
-    # # Quick check that if there is a tempered update that the old vintage and current vintage are different
-    # if tempered_update
-        # old_vintage = get_setting(m, :previous_data_vintage)
-        # @assert old_vintage != data_vintage(m)
-    # end
+    # Quick check that if there is a tempered update that the old vintage and current vintage are different
+    if tempered_update
+        old_vintage = get_setting(m, :previous_data_vintage)
+        @assert old_vintage != data_vintage(m)
+    end
 
     # Step 0 (ϕ schedule) settings
     i = 1                           # The index tracking the stage of the algorithm
