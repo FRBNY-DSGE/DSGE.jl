5a023647735ee2611201da8c3beadb876e144e19
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-02-05 10:59:58 -0500

    Add unit tests for helper methods, provide docstrings for functions, add test files.

--- src/estimate/smc/initialization.jl
+++ src/estimate/smc/initialization.jl
@@ -107,12 +107,13 @@ end
 initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud)
 ```
 
-Draw from a general starting distribution (set by default to be from the prior) to initialize the SMC algorithm.
-Returns a tuple (logpost, loglh) and modifies the particle objects in the particle cloud in place.
-
+Draw from a general starting distribution (set by default to be from the prior) to
+initialize the SMC algorithm. Returns a tuple (logpost, loglh) and modifies the
+particle objects in the particle cloud in place.
 """
 function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud;
-                       parallel::Bool = false, use_chand_recursion::Bool = true, verbose::Symbol = :low)
+                       parallel::Bool = false, use_chand_recursion::Bool = true,
+                       verbose::Symbol = :low)
     n_parts = length(c)
     loglh   = zeros(n_parts)
     logpost = zeros(n_parts)
@@ -126,7 +127,8 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                 try
                     update!(m, draw)
                     draw_loglh = likelihood(m, data, catch_errors = true,
-                                            use_chand_recursion=use_chand_recursion, verbose = verbose)
+                                            use_chand_recursion=use_chand_recursion,
+                                            verbose = verbose)
                     draw_logpost = prior(m)
                     if (draw_loglh == -Inf) | (draw_loglh===NaN)
                         draw_logpost = -Inf
@@ -135,10 +137,7 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                 catch err
                     if isa(err, ParamBoundsError)
                         draw_loglh = draw_logpost = -Inf
-                    #elseif isa(err, SPDError)
-                    #    draw_loglh = draw_logpost = -Inf
                     else
-                     #   draw_loglh = draw_logpost = -Inf
                         throw(err)
                     end
                 end
@@ -169,8 +168,7 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
                     end
                 catch err
                     if isa(err, ParamBoundsError)
-                        loglh[i] = logpost[i] = -Inf #draws[:, i] = rand(m.parameters, 1)
-                        #continue
+                        loglh[i] = logpost[i] = -Inf
                     else
                         throw(err)
                     end
@@ -231,9 +229,16 @@ function initialize_likelihoods!(m::AbstractModel, data::Matrix{Float64}, c::Par
     update_logpost!(c, logpost)
 end
 
-function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud; tempered_update::Bool = false)
+"""
+```
+function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud;
+                                    tempered_update::Bool = false)
+```
+Initializes stage index, number of Φ stages, c, resamples, acceptance, and sampling time.
+"""
+function initialize_cloud_settings!(m::AbstractModel, cloud::ParticleCloud;
+                                    tempered_update::Bool = false)
     n_parts = length(cloud)
-
     cloud.tempering_schedule = zeros(1)
 
     if tempered_update
--- test/estimate/smc/initialization.jl
+++ test/estimate/smc/initialization.jl
@@ -1,7 +1,7 @@
 # To be removed after running this test individually in the REPL successfully
-using DSGE
-using HDF5, JLD, JLD2
-import Base.Test: @test, @testset
+@everywhere using DSGE, DSGEModels
+@everywhere using HDF5, JLD, JLD2
+@everywhere import Base.Test: @test, @testset
 
 path = dirname(@__FILE__)
 
@@ -29,7 +29,7 @@ m <= Setting(:use_fixed_schedule, true)
 
 ####################################################################
 test_init_cloud = ParticleCloud(m, get_setting(m, :n_particles))
-srand(42)
+@everywhere srand(42)
 DSGE.initial_draw!(m, data, test_init_cloud)
 
 #= JLD.jldopen("reference/initial_draw.jld", "w") do file
@@ -37,12 +37,57 @@ DSGE.initial_draw!(m, data, test_init_cloud)
 end
 JLD2.jldopen("reference/initial_draw.jld2", "w") do file
     write(file, "cloud", test_init_cloud)
-end =#
-
-saved_init_cloud = load("reference/initial_draw.jld", "cloud")
+end
+=#
+saved_init_cloud = load("reference/initial_draw.jld2", "cloud")
 
 @testset "Initial draw" begin
     @test @test_matrix_approx_eq DSGE.get_vals(test_init_cloud) DSGE.get_vals(saved_init_cloud)
     @test @test_matrix_approx_eq DSGE.get_loglh(test_init_cloud) DSGE.get_loglh(saved_init_cloud)
 end
 
+###################################################################
+# Smets Wouters
+###################################################################
+m = SmetsWoutersOrig()
+
+saveroot = normpath(joinpath(dirname(@__FILE__),"save"))
+m <= Setting(:saveroot, saveroot)
+
+data = readdlm("reference/YY.txt")
+data = data'
+
+m <= Setting(:n_particles, 12000)
+m <= Setting(:n_Φ, 500)
+m <= Setting(:λ, 2.1)
+m <= Setting(:n_smc_blocks, 1)
+m <= Setting(:use_parallel_workers, false)
+m <= Setting(:step_size_smc, 0.4)
+m <= Setting(:n_mh_steps_smc, 1)
+m <= Setting(:resampler_smc, :polyalgo)
+m <= Setting(:target_accept, 0.25)
+
+m <= Setting(:mixture_proportion, 0.9)
+m <= Setting(:tempering_target, 0.95)
+m <= Setting(:resampling_threshold, 0.5)
+m <= Setting(:use_fixed_schedule, true)
+m <= Setting(:use_chand_recursion, true)
+
+####################################################################
+test_init_cloud = ParticleCloud(m, get_setting(m, :n_particles))
+@everywhere srand(42)
+DSGE.initial_draw!(m, data, test_init_cloud)
+
+#= JLD.jldopen("reference/initial_draw.jld", "w") do file
+    write(file, "cloud", test_init_cloud)
+end
+JLD2.jldopen("reference/initial_draw_sw.jld2", "w") do file
+    write(file, "cloud", test_init_cloud)
+end
+=#
+saved_init_cloud = load("reference/initial_draw_sw.jld2", "cloud")
+
+@testset "Initial draw" begin
+    @test @test_matrix_approx_eq DSGE.get_vals(test_init_cloud) DSGE.get_vals(saved_init_cloud)
+    @test @test_matrix_approx_eq DSGE.get_loglh(test_init_cloud) DSGE.get_loglh(saved_init_cloud)
+end
