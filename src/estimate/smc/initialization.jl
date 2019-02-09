0083fd919858bed4ea13876575f7ceddd5d3a240
Ethan Matlin <ethanmatlin@gmail.com>
2019-02-09 15:50:36 -0500

    Take smc/initializiation from the smc branch because I was worried the merge introdcued a bug (in hindsight, I don't actually think this was the case)

--- src/estimate/smc/initialization.jl
+++ src/estimate/smc/initialization.jl
@@ -1,109 +1,5 @@
 """
 ```
-initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud, readin::Matrix{Float64})
-```
-
-Draw from a general starting distribution (set by default to be from the prior) to initialize the SMC algorithm.
-Returns a tuple (logpost, loglh) and modifies the particle objects in the particle cloud in place.
-
-"""
-function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud,
-                       path::String; # RECA
-                       parallel::Bool = false, use_chand_recursion::Bool = true,
-                       verbose::Symbol = :low)
-    # legacy - not currently necessary
-    n_parts = length(c)
-    loglh = zeros(n_parts)
-    logpost = zeros(n_parts)
-#=
-    draws   = transpose(readdlm(path * "001parasim.txt"))
-    if parallel
-        draws, loglh, logpost = @sync @parallel (vector_reduce) for i in 1:n_parts
-            draw = vec(draws[i]) #vec(rand(m.parameters, 1))
-            draw_loglh = 0.
-            draw_logpost = 0.
-            success = false
-            while !success
-                try
-                    update!(m, draw)
-                    draw_loglh   = likelihood(m, data, catch_errors = true, use_chand_recursion=use_chand_recursion, verbose = verbose)
-                    draw_logpost = prior(m)
-                    if (draw_loglh == -Inf) | (draw_loglh===NaN)
-                        draw_logpost = -Inf
-                        draw_loglh = -Inf
-                    end
-                catch err
-                    if isa(err, ParamBoundsError)
-                        draw_loglh = draw_logpost = -Inf
-                        #elseif isa(err, SPDError)
-                        #    draw_loglh = draw_logpost = -Inf
-                    else
-                        #   draw_loglh = draw_logpost = -Inf
-                        throw(err)
-                    end
-                end
-
-                if isinf(draw_loglh)
-                    draw = vec(rand(m.parameters, 1))
-                else
-                    success = true
-                end
-            end
-            vector_reshape(draw, draw_loglh, draw_logpost)
-        end
-        loglh = squeeze(loglh, 1)
-        logpost = squeeze(logpost, 1)
-    else
-        #draws = rand(m.parameters, n_parts)
-        for i in 1:n_parts
-            success = false
-            while !success
-                try
-                    update!(m, draws[:, i])
-                    loglh[i] = likelihood(m, data, catch_errors = true, use_chand_recursion = use_chand_recursion, verbose = verbose)
-                    logpost[i] = prior(m)
-                    if (loglh[i] == -Inf) | (loglh[i]===NaN)
-                        logpost[i] = -Inf
-                        loglh[i] = -Inf
-                    end
-                catch err
-                    if isa(err, ParamBoundsError)
-                        loglh[i] = logpost[i] = -Inf #draws[:, i] = rand(m.parameters, 1)
-                        #continue
-                    else
-                        throw(err)
-                    end
-                end
-                if isinf(loglh[i])
-                    @show "REDRAW AT PARTICLE", i
-                    draws[:, i] = rand(m.parameters, 1)
-                else
-                    success = true
-                end
-            end
-        end
-    end
-
-    draws_orig   = transpose(readdlm(path * "001parasim.txt"))
-    loglh_orig   = vec(readdlm(path * "001liksim.txt"))
-    logpost_orig = vec(readdlm(path * "001postsim.txt"))
-
-    @test_matrix_approx_eq draws_orig draws
-    @test_matrix_approx_eq loglh_orig loglh
-    @test_matrix_approx_eq logpost_orig logpost
-=#
-    draws   = transpose(readdlm(path * "001parasim.txt"))
-    loglh   = vec(readdlm(path * "001liksim.txt"))
-    logpost = vec(readdlm(path * "001postsim.txt")) - loglh
-    # redraw at 11371
-
-    update_draws!(c, draws)
-    update_loglh!(c, loglh)
-    update_logpost!(c, logpost)
-end
-
-"""
-```
 initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud)
 ```
 
@@ -181,7 +77,6 @@ function initial_draw!(m::AbstractModel, data::Matrix{Float64}, c::ParticleCloud
             end
         end
     end
-
     update_draws!(c, draws)
     update_loglh!(c, loglh)
     update_logpost!(c, logpost)
