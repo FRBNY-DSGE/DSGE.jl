33a3a59dac0d06a2ffe211d1046f93a16ddd0cf2
Ethan Matlin <ethanmatlin@gmail.com>
2019-02-11 16:56:42 -0500

    Cherry-pick 8eecb6c squashing '1+' bug in AS

--- test/estimate/optimize.jl
+++ test/estimate/optimize.jl
@@ -1,4 +1,4 @@
-using DSGE, JLD2, FileIO
+using DSGE
 using HDF5
 path = dirname(@__FILE__)
 
@@ -8,20 +8,34 @@ custom_settings = Dict{Symbol, Setting}(
 m = AnSchorfheide(custom_settings = custom_settings, testing = true)
 
 
-file = "$path/../reference/optimize.jld2"
-x0 = load(file, "params")
-data = Matrix{Float64}(load(file, "data")')
-minimizer = load(file, "minimizer")
-minimum = load(file, "minimum")
-H_expected = load(file, "H")
+file = "$path/../reference/optimize.h5"
+# For regenerating test file
+#=params_test = h5read(file, "params")
+data_test = h5read(file, "data")=#
+
+x0 = h5read(file, "params")
+data = h5read(file, "data")'
+minimizer = h5read(file, "minimizer")
+minimum = h5read(file, "minimum")
+H_expected = h5read(file, "H")
 
 # See src/estimate/estimate.jl
-DSGE.update!(m, x0)
+update!(m, x0)
 n_iterations = 3
 
 x0 = Float64[p.value for p in m.parameters]
 
 out, H = optimize!(m, data; iterations=n_iterations)
+
+# Re-generate test file
+#= h5open("$path/../reference/optimize.h5", "w") do file
+    file["params"] = params_test
+    file["data"] = data_test
+    file["minimizer"] = out.minimizer
+    file["minimum"] = out.minimum
+    file["H"] = H
+end =#
+
 @testset "Check optimize minimizers are the same" begin
     @test @test_matrix_approx_eq minimizer out.minimizer
     @test minimum ≈ out.minimum atol=5e-7
