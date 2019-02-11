56f1c2cc29c4666da179dd39473f387cbdbc5a62
Ethan Matlin <ethanmatlin@gmail.com>
2019-02-11 16:56:42 -0500

    Cherry-pick 8eeccb6c from dev into master squashing '1+' bug in Psi_1 of AnShorfheide and update relevant tests

--- test/estimate/optimize.jl
+++ test/estimate/optimize.jl
@@ -9,6 +9,10 @@ m = AnSchorfheide(custom_settings = custom_settings, testing = true)
 
 
 file = "$path/../reference/optimize.h5"
+# For regenerating test file
+#=params_test = h5read(file, "params")
+data_test = h5read(file, "data") =#
+
 x0 = h5read(file, "params")
 data = h5read(file, "data")'
 minimizer = h5read(file, "minimizer")
@@ -22,6 +26,15 @@ n_iterations = 3
 x0 = Float64[p.value for p in m.parameters]
 
 out, H = optimize!(m, data; iterations=n_iterations)
+
+# Re-generate test file
+#=h5open("$path/../reference/optimize.h5", "w") do file
+    file["params"] = params_test
+    file["data"] = data_test
+    file["minimizer"] = out.minimizer
+    file["minimum"] = out.minimum
+    file["H"] = H
+end=#
 
 @testset "Check optimize minimizers are the same" begin
     @test @test_matrix_approx_eq minimizer out.minimizer
