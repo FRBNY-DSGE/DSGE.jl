e33d0f0352abbd6a887aa81eeb10b146cb78bc14
Ethan Matlin <ethanmatlin@gmail.com>
2019-04-04 17:04:17 -0400

    Add prior for \phipi (taken from Psi_1 in M1002 and clean up parameter specifications

--- src/models/heterogeneous_agent/real_bond_mkup/real_bond_mkup.jl
+++ src/models/heterogeneous_agent/real_bond_mkup/real_bond_mkup.jl
@@ -264,7 +264,7 @@ those).
 """
 function init_parameters!(m::RealBondMkup)
 
-    # Initialize parameters
+    # Steady State Parameters
     m <= parameter(:R, 1.04, fixed = true,
                    description = "R: Steady-state gross real interest rate.", tex_label = "R")
     m <= parameter(:γ, 1.0, fixed = true,
@@ -273,30 +273,48 @@ function init_parameters!(m::RealBondMkup)
                    description = "Inverse Frisch elasticity of labor supply.", tex_label = "\\nu")
     m <= parameter(:abar, -0.5, fixed = true,
                    description = "Borrowing floor.", tex_label = "\\bar{a}")
-    m <= parameter(:ρ_z, 0.03081565350294113, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2), fixed=true,
+
+    # AR for observables (Non-steadystate)
+    m <= parameter(:ρ_z, 0.03081565350294113, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2),
+                   fixed=true,
                    description="ρ_z: AR(1) coefficient in the technology process.",
                    tex_label="\\rho_z")
-    m <= parameter(:σ_z, sqrt(.007), (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10), fixed=false,
+    m <= parameter(:ρ_mon, 0.0, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2),
+                   fixed = true,
+                   description = "ρ_mon: Persistence of monetary policy shock",
+                   tex_label = "\\rho_{mon}")
+    m <= parameter(:ρ_mkp, 0.0, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2),
+                   fixed = true,
+                   tex_label = "\\rho_{mkp}", description = "ρ_mkp: Persistence of the markup shock")
+
+    m <= parameter(:σ_z, sqrt(.007), (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10),
+                   fixed=false,
                    description="σ_z: The standard deviation of the process describing the stationary component of productivity.",
                    tex_label="\\sigma_{z}")
-    m <= parameter(:ρ_mon, 0., fixed = true, description = "ρ_mon: Persistence of monetary policy shock", tex_label = "\\rho_{mon}")
-    m <= parameter(:σ_mon, 0.2380, (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10), fixed = false,
+    m <= parameter(:σ_mon, 0.2380, (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10),
+                   fixed = false,
                    description="σ_mon: The standard deviation of the monetary policy shock.",
                    tex_label="\\sigma_{mon}")
-    m <= parameter(:ρ_mkp, 0.0, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2),
-                   fixed = true, tex_label = "\\rho_{mkp}", description = "ρ_mkp: Persistence of the markup shock")
-
-    # Taken from m1002
-    m <= parameter(:σ_mkp, 0.1314, (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10), fixed=false,
+    m <= parameter(:σ_mkp, 0.1314, (1e-8, 5.), (1e-8, 5.), Exponential(), RootInverseGamma(2, 0.10),
+                   fixed=false,
                    description="σ_mkp: The mean of the process that generates the price elasticity of the composite good. Specifically, the elasticity is (1+λ_{f,t})/(λ_{f_t}).",
                    tex_label="\\sigma_{\\lambda_f}")
 
+    # More non-steadystate parameters
     m <= parameter(:ρ_tay, 0.5, (0., 0.999), (0., 0.999), SquareRoot(), BetaAlt(0.5, 0.2),
-                   fixed = true, tex_label = "rho_{tay}", description = "ρ_tay: Persistence in the taylor rule")
+                   fixed = true,
+                   description = "ρ_tay: Persistence in the taylor rule",
+                   tex_label = "rho_{tay}")
     m <= parameter(:κ, 1.0, (0., 0.999), (0., 0.999), SquareRoot(), Distributions.Uniform(0.0, 1.0),
-                   fixed = true, tex_label = "\\kappa",
-                   description = "κ: The slope of the Phillips curve")
-    m <= parameter(:phipi, 1.5, fixed = true, description = "phipi: The slope of the taylor rule")
+                   fixed = true,
+                   description = "κ: The slope of the Phillips curve",
+                   tex_label = "\\kappa")
+    m <= parameter(:phipi, 1.5, (1e-5, 10.), (1e-5, 10.00), Exponential(), Normal(1.5, 0.25),
+                   fixed = true,
+                   description = "phipi: The slope of the taylor rule",
+                   tex_label = "\\phi_\\pi")
+
+    # More steadystate parameters
     m <= parameter(:μ_s, 0., fixed = true, description = "μ_s: Mu of log normal in income")
     m <= parameter(:σ_s, 0.1, fixed = true,
                    description = "σ_s: Sigma of log normal in income")
