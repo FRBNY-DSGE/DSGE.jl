79f2848e7233cd834b6b543a414d8c649157b40a
Ethan Matlin <ethanmatlin@gmail.com>
2019-04-19 09:56:45 -0400

    Implement HetDSGESimpleTaylor (however some tests still don't pass because I need to continue implementing the new grid thing Keshav does)

--- src/DSGE.jl
+++ src/DSGE.jl
@@ -151,7 +151,7 @@ module DSGE
         # models/
         init_parameters!, steadystate!, init_observable_mappings!, init_pseudo_observable_mappings!,
         Model990, Model1002, Model1010, SmetsWouters, AnSchorfheide,
-        KrusellSmith, BondLabor, RealBond, RealBondMkup, HetDSGE,
+        KrusellSmith, BondLabor, RealBond, RealBondMkup, HetDSGE, HetDSGESimpleTaylor,
         eqcond, measurement, pseudo_measurement, shock_groupings, Grid,
 
         #### Continuous time
@@ -363,6 +363,11 @@ module DSGE
     include("models/heterogeneous_agent/het_dsge/steady_state.jl")
     include("models/heterogeneous_agent/het_dsge/subspecs.jl")
     include("models/heterogeneous_agent/het_dsge/jacobian.jl")
+
+    include("models/heterogeneous_agent/het_dsge_simple_taylor/het_dsge_simple_taylor.jl")
+    include("models/heterogeneous_agent/het_dsge_simple_taylor/steady_state.jl")
+    include("models/heterogeneous_agent/het_dsge_simple_taylor/subspecs.jl")
+    include("models/heterogeneous_agent/het_dsge_simple_taylor/jacobian.jl")
 
     # Continuous Time Heterogenous Agent Models
     include("solve/solve_hank.jl")
