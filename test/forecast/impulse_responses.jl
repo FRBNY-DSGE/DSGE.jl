18eee262a14c294cf1f63f7814486a605127ea14
Michael Cai <michaeldcai@gmail.com>
2019-06-04 17:32:48 -0400

    Update impulse_responses unit tests to have all new methods' saved output in the jld2

--- test/forecast/impulse_responses.jl
+++ test/forecast/impulse_responses.jl
@@ -53,7 +53,7 @@ states, obs, pseudo = impulse_responses(m, system, horizon, shock_names, shock_v
 end
 
 exp_states_shockset, exp_obs_shockset, exp_pseudo_shockset =
-    jldopen("$path/../reference/impulse_responses_out.jld", "r") do file
+    jldopen("$path/../reference/impulse_responses_out.jld2", "r") do file
         read(file, "exp_states_shockset"), read(file, "exp_obs_shockset"), read(file, "exp_pseudo_shockset")
     end
 
@@ -75,7 +75,7 @@ states, obs, pseudo = impulse_responses(m, system, horizon, shock_name, state_na
 end
 
 exp_states_shockstates, exp_obs_shockstates, exp_pseudo_shockstates =
-    jldopen("$path/../reference/impulse_responses_out.jld", "r") do file
+    jldopen("$path/../reference/impulse_responses_out.jld2", "r") do file
         read(file, "exp_states_shockstate"), read(file, "exp_obs_shockstate"), read(file, "exp_pseudo_shockstate")
     end
 
@@ -97,7 +97,7 @@ states, obs, pseudo = impulse_responses(m, system, horizon, shock_name, obs_name
 end
 
 exp_states_shockobs, exp_obs_shockobs, exp_pseudo_shockobs =
-    jldopen("$path/../reference/impulse_responses_out.jld", "r") do file
+    jldopen("$path/../reference/impulse_responses_out.jld2", "r") do file
         read(file, "exp_states_shockobs"), read(file, "exp_obs_shockobs"), read(file, "exp_pseudo_shockobs")
     end
 
