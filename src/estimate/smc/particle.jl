15593bfcac81531e4d92480261fe04e5c001ec19
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-06-13 16:49:33 -0400

    Adding functions for new cloud object

--- src/estimate/smc/particle.jl
+++ src/estimate/smc/particle.jl
@@ -67,15 +67,24 @@ function ParticleCloud(m::AbstractModel, n_parts::Int)
 end
 
 function get_weights(c::ParticleCloud)
-    return map(p -> p.weight,c.particles)
+    return map(p -> p.weight, c.particles)
+end
+@inline function get_weights(c::Matrix{Float64})
+    return c[:,end]
 end
 
 function get_vals(c::ParticleCloud)
     return hcat(map(p -> p.value, c.particles)...)
 end
+@inline function get_vals(c::Matrix{Float64})
+    return c[:, 1:end-5]
+end
 
 function get_loglh(c::ParticleCloud)
     return map(p -> p.loglh, c.particles)
+end
+@inline function get_loglh(c::Matrix{Float64})
+    return c[:, 1:end-4]
 end
 
 function cloud_isempty(c::ParticleCloud)
@@ -85,59 +94,115 @@ end
 function get_old_loglh(c::ParticleCloud)
     return map(p -> p.old_loglh, c.particles)
 end
+@inline function get_old_loglh(c::Matrix{Float64})
+    return c[:,end-2]
+end
 
 function get_logpost(c::ParticleCloud)
     return map(p -> p.logpost + p.loglh, c.particles)
+end
+@inline function get_logpost(c::Matrix{Float64})
+    return c[:,end-3] .+ c[:,end-4]
+end
+
+#TODO: Fix logpost/logprior confusion
+function get_logpprior(c::ParticleCloud)
+    return map(p -> p.logpost, c.particles)
+end
+@inline function get_logprior(c::Matrix{Float64})
+    return c[:,end-3]
 end
 
 function get_likeliest_particle_value(c::ParticleCloud)
     return c.particles[indmax(get_loglh(c))].value
 end
+@inline function get_likeliest_particle_value(c::Matrix{Float64})
+    return c[indmax(get_loglh(c)), 1:end-4]
+end
 
-function update_draws!(c::ParticleCloud, draws::Array{Float64,2})
+function update_draws!(c::ParticleCloud, draws::Matrix{Float64})
     @assert size(draws) == (length(c.particles[1]),length(c))
     for (i,p) in enumerate(c.particles)
         update_val!(p, draws[:,i])
     end
 end
-
 function update_draws!(c::ParticleCloud, draws::Array{Particle,1})
     for (i,d) in enumerate(draws)
         c.particles[i] = d
     end
 end
-
-function update_weights!(c::ParticleCloud, incweight::Array{Float64,1})
-    new_weights = get_weights(c).*incweight
-    for (p,w) in zip(c.particles, new_weights)
-        update_weight!(p,w)
+@inline function update_draws!(c::Matrix{Float64}, draws::Matrix{Float64})
+    @assert size(draws) == (size(c,1), size(c,2)-5) "Draws are incorrectly sized"
+    I, J = size(draws)
+    for i = 1:I, j=1:J
+        c[i, j] = draws[i,j]
     end
 end
 
-function update_loglh!(c::ParticleCloud, loglh::Array{Float64,1})
+function update_weights!(c::ParticleCloud, incweight::Vector{Float64})
+    new_weights = get_weights(c).*incweight
+    for (p,w) in zip(c.particles, new_weights)
+        update_weight!(p, w)
+    end
+end
+@inline function update_weights!(c::Matrix{Float64}, incweight::Vector{Float64})
+    @assert size(c, 1) == length(incweight) "Dimensional mismatch in inc. weights"
+    for i=1:length(incweight)
+        c[i,end] *= incweight[i]
+    end
+end
+
+function update_loglh!(c::ParticleCloud, loglh::Vector{Float64})
     for (p,l) in zip(c.particles,loglh)
         p.loglh = l
     end
 end
+@inline function update_loglh!(c::Matrix{Float64}, loglh::Vector{Float64})
+    @assert size(c, 1) == length(loglh) "Dimensional mismatch"
+    for i=1:length(loglh)
+        c[i, end-4] = loglh[i]
+    end
+end
 
-function update_logpost!(c::ParticleCloud, logpost::Array{Float64,1})
+function update_logpost!(c::ParticleCloud, logpost::Vector{Float64})
     for (p,l) in zip(c.particles,logpost)
         p.logpost = l
     end
 end
+@inline function update_logpost!(c::Matrix{Float64}, logpost::Vector{Float64})
+    @assert size(c, 1) == length(logpost) "Dimensional mismatch"
+    for i=1:length(logpost)
+        c[i, end-3] = logpost[i]
+    end
+end
 
 function normalize_weights!(c::ParticleCloud)
-    new_weights = get_weights(c)/sum(get_weights(c))
+    new_weights = get_weights(c) / sum(get_weights(c))
     for (p,w) in zip(c.particles, new_weights)
         update_weight!(p,w)
     end
+end
+@inline function normalize_weights!(c::Matrix{Float64})
+    sum_weights = sum(get_weights(c))
+    c[:, end] /= sum_weights
 end
 
 function reset_weights!(c::ParticleCloud)
     map(p->update_weight!(p,1/length(c)),c.particles)
 end
+@inline function reset_weights!(c::Matrix{Float64})
+    n_parts = size(c, 1)
+    c[:, end] .= 1.0 / n_parts
+end
 
-function update_mutation!(p::Particle, para::Array{Float64,1},
+"""
+```
+function update_mutation!(p::Particle, para::Vector{Float64},
+                          like::Float64, post::Float64, old_like::Float64, accept::Bool)
+```
+Deprecated syntax. Accept should be a Float64, not a Bool.
+"""
+function update_mutation!(p::Particle, para::Vector{Float64},
                           like::Float64, post::Float64, old_like::Float64, accept::Bool)
     p.value = para
     p.loglh = like
@@ -145,8 +210,7 @@ function update_mutation!(p::Particle, para::Array{Float64,1},
     p.old_loglh = old_like
     p.accept = accept
 end
-
-function update_mutation!(p::Particle, para::Array{Float64,1},
+function update_mutation!(p::Particle, para::Vector{Float64},
                           like::Float64, post::Float64, old_like::Float64, accept::Float64)
     p.value = para
     p.loglh = like
@@ -154,28 +218,49 @@ function update_mutation!(p::Particle, para::Array{Float64,1},
     p.old_loglh = old_like
     p.accept = accept
 end
+@inline function update_mutation!(p::Vector{Float64}, para::Vector{Float64}, like::Float64,
+                                  post::Float64, old_like::Float64, accept::Float64)
+    p[1:end-5] = para
+    p[end-4]   = like
+    p[end-3]   = post
+    p[end-2]   = old_like
+    p[end-1]   = accept
+end
 
-function update_val!(p::Particle, val::Array{Float64,1})
+function update_val!(p::Particle, val::Vector{Float64})
     p.value = val
+end
+@inline function update_val!(p::Vector{Float64}, val::Vector{Float64})
+    @assert length(p)-5 == length(val) "Parameter vector length is wrong!"
+    p[1:length(val)] = val
 end
 
 function update_weight!(p::Particle, weight::Float64)
     p.weight = weight
 end
+@inline function update_weight!(p::Vector{Float64}, weight::Float64)
+    p[end] = weight
+end
 
 function update_acceptance_rate!(c::ParticleCloud)
     c.accept = mean(map(p -> p.accept, c.particles))
 end
+@inline function mean_acceptance_rate(c::Matrix{Float64})
+    return mean(c[:,end-1])
+end
 
+# TODO: Check this never gets called on new Particle type
 @inline function Base.length(c::ParticleCloud)
         return length(c.particles)
 end
-
 @inline function Base.length(p::Particle)
     return length(p.value)
 end
 
 function weighted_mean(c::ParticleCloud)
+    return dropdims(mean(get_vals(c), Weights(get_weights(c)), dims = 2), dims = 2)
+end
+@inline function weighted_mean(c::Matrix{Float64})
     return dropdims(mean(get_vals(c), Weights(get_weights(c)), dims = 2), dims = 2)
 end
 
@@ -184,13 +269,24 @@ function weighted_quantile(c::ParticleCloud, i::Int64)
     ub = quantile(get_vals(c)[i, :], Weights(get_weights(c)), .95)
     return lb, ub
 end
+@inline function weighted_quantile(c::Matrix{Float64}, i::Int64)
+    lb = quantile(c[:, i], Weights(c[:,end]), .05)
+    ub = quantile(c[:, i], Weights(c[:,end]), .95)
+    return lb, ub
+end
 
 function weighted_std(c::ParticleCloud)
+    return sqrt.(diag(weighted_cov(c)))
+end
+@inline function weighted_std(c::Matrix{Float64})
     return sqrt.(diag(weighted_cov(c)))
 end
 
 function weighted_cov(c::ParticleCloud)
     return cov(Matrix(get_vals(c)'), Weights(get_weights(c)), corrected = false)
+end
+@inline function weighted_cov(c::Matrix{Float64})
+    return cov(Matrix(get_vals(c)), Weights(get_weights(c)), corrected = false)
 end
 
 for op in (:(Base.:+),
@@ -204,6 +300,6 @@ for op in (:(Base.:+),
     @eval ($op)(p::Particle, x::Number)     = ($op)(p.value,x)
     @eval ($op)(x::Integer, p::Particle)    = ($op)(p.value,x)
     @eval ($op)(x::Number, p::Particle)     = ($op)(p.value,x)
-    @eval ($op)(p::Particle, q::Array{Float64,1})   = ($op)(p.value,q)
-    @eval ($op)(p::Array{Float64,1}, q::Particle)   = ($op)(p,q.value)
+    @eval ($op)(p::Particle, q::Vector{Float64})   = ($op)(p.value,q)
+    @eval ($op)(p::Vector{Float64}, q::Particle)   = ($op)(p,q.value)
 end
