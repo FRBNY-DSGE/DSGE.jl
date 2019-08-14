c7b49d2fcc8da2dde5eb7a0e7cb3d8812ac4ada5
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-14 14:00:07 -0400

    Fixing situation with Observables types

--- src/DSGE.jl
+++ src/DSGE.jl
@@ -17,11 +17,12 @@ module DSGE
     import LinearAlgebra: rank
     import Optim: optimize, SecondOrderOptimizer, MultivariateOptimizationResults
     import StateSpaceRoutines: KalmanFilter, augment_states_with_shocks
-    import ModelConstructors: <=
+    import ModelConstructors
 
     export
         # distributions_ext.jl
-        BetaAlt, GammaAlt, RootInverseGamma, DegenerateMvNormal, DegenerateDiagMvTDist, MatrixNormal,
+        BetaAlt, GammaAlt, RootInverseGamma, DegenerateMvNormal, DegenerateDiagMvTDist,
+        MatrixNormal,
 
         # settings.jl
         Setting, get_setting,
@@ -44,7 +45,8 @@ module DSGE
         n_parameters_free, n_pseudo_observables, get_dict, get_key,
         inds_states_no_ant, inds_shocks_no_ant, inds_obs_no_ant,
         spec, subspec, saveroot, dataroot,
-        data_vintage, data_id, cond_vintage, cond_id, cond_full_names, cond_semi_names, use_population_forecast,
+        data_vintage, data_id, cond_vintage, cond_id, cond_full_names, cond_semi_names,
+        use_population_forecast,
         use_parallel_workers,
         reoptimize, calculate_hessian, hessian_path, n_hessian_test_params,
         n_mh_blocks, n_mh_simulations, n_mh_burn, mh_thin,
@@ -61,13 +63,15 @@ module DSGE
         ShockGroup, alternative_policy,
 
         # parameters.jl
-       #= parameter, Transform, NullablePrior, AbstractParameter,
+     #= parameter, Transform, NullablePrior, AbstractParameter,
         Parameter, ParameterVector, ScaledParameter,
         UnscaledParameter, SteadyStateParameter, transform_to_real_line, transform_to_model_space,
-        update, update!, transform_to_model_space, transform_to_real_line, Interval, ParamBoundsError,=#
+        update, update!, transform_to_model_space, transform_to_real_line, Interval,
+        ParamBoundsError,=#
 
         # observables.jl
-        Observable, PseudoObservable, check_mnemonics,
+        #Observable,
+        #PseudoObservable, check_mnemonics,
 
         # statespace.jl
         Transition, Measurement, PseudoMeasurement, System, compute_system,
@@ -81,7 +85,8 @@ module DSGE
         df_to_matrix, hpfilter, difflog, quartertodate, percapita, nominal_to_real,
         oneqtrpctchange, annualtoquarter, quartertoannual, quartertoannualpercent,
         loggrowthtopct_percapita, loggrowthtopct, logleveltopct_annualized,
-        loggrowthtopct_annualized_percapita, loggrowthtopct_annualized, logleveltopct_annualized_percapita,
+        loggrowthtopct_annualized_percapita, loggrowthtopct_annualized,
+        logleveltopct_annualized_percapita,
         logleveltopct_annualized_approx, loggrowthtopct_4q_approx, logleveltopct_4q_approx,
         parse_data_series, collect_data_transforms, reverse_transform,
         subtract_quarters, iterate_quarters,
@@ -110,8 +115,10 @@ module DSGE
         meansbands_to_matrix, read_mb, read_bdd_and_unbdd_mb,
         get_meansbands_input_file, get_meansbands_output_file, get_product, get_class,
         which_density_bands,
-        prepare_meansbands_tables_timeseries, prepare_means_tables_shockdec, prepare_meansbands_table_irf,
-        write_meansbands_tables_timeseries, write_means_tables_shockdec, prepare_meansbands_table_irf,
+        prepare_meansbands_tables_timeseries, prepare_means_tables_shockdec,
+        prepare_meansbands_table_irf,
+        write_meansbands_tables_timeseries, write_means_tables_shockdec,
+        prepare_meansbands_table_irf,
         write_meansbands_tables_all, construct_fcast_and_hist_dfs,
         df_to_table, load_posterior_moments,
 
@@ -127,9 +134,11 @@ module DSGE
         # scenarios/
         AbstractScenario, SingleScenario, Scenario, SwitchingScenario, ScenarioAggregate,
         n_targets, n_instruments, n_target_horizons, targets_to_data,
-        compute_scenario_system, filter_shocks!, forecast_scenario, simulate_switching, scenario_means_bands,
+        compute_scenario_system, filter_shocks!, forecast_scenario, simulate_switching,
+        scenario_means_bands,
         get_scenario_input_file, n_scenario_draws, get_scenario_filename, get_scenario_output_files,
-        read_scenario_output, get_scenario_mb_input_file, get_scenario_mb_output_file, read_scenario_mb,
+        read_scenario_output, get_scenario_mb_input_file, get_scenario_mb_output_file,
+        read_scenario_mb,
         count_scenario_draws,
 
         # plot/
@@ -140,7 +149,8 @@ module DSGE
 
         # models/
         init_parameters!, steadystate!, init_observable_mappings!, init_pseudo_observable_mappings!,
-        Model990, Model1002, Model1010, SmetsWouters, AnSchorfheide, eqcond, measurement, pseudo_measurement,
+        Model990, Model1002, Model1010, SmetsWouters, AnSchorfheide, eqcond, measurement,
+        pseudo_measurement,
         shock_groupings,
 
         # util
