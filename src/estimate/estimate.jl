a8bb0e7af8976218ae77fa8be8ea8f9fe4943fac
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-23 23:31:12 -0400

    Extract Metropolis Hastings from estimate.jl, move it to its own file.

--- src/estimate/estimate.jl
+++ src/estimate/estimate.jl
@@ -240,181 +240,6 @@ function estimate(m::AbstractDSGEModel, data::AbstractArray;
     return nothing
 end
 
-
-"""
-```
-metropolis_hastings(propdist::Distribution, m::AbstractDSGEModel,
-    data::Matrix{T}, cc0::T, cc::T; verbose::Symbol = :low) where {T<:AbstractFloat}
-```
-
-Implements the Metropolis-Hastings MCMC algorithm for sampling from the posterior
-distribution of the parameters.
-
-### Arguments
-
-- `propdist`: The proposal distribution that Metropolis-Hastings begins sampling from.
-- `m`: The model object
-- `data`: Data matrix for observables
-- `cc0`: Jump size for initializing Metropolis-Hastings.
-- `cc`: Jump size for the rest of Metropolis-Hastings.
-
-### Optional Arguments
-
-- `verbose`: The desired frequency of function progress messages printed to
-  standard out. One of:
-
-```
-   - `:none`: No status updates will be reported.
-   - `:low`: Status updates provided at each block.
-   - `:high`: Status updates provided at each draw.
-```
-"""
-function metropolis_hastings(propdist::Distribution,
-                             m::AbstractDSGEModel,
-                             data::Matrix{T},
-                             cc0::T,
-                             cc::T;
-                             verbose::Symbol=:low,
-                             filestring_addl::Vector{String} = Vector{String}(undef, 0)) where {T<:AbstractFloat}
-
-
-    # If testing, set the random seeds at fixed numbers
-    if m.testing
-        Random.seed!(m.rng, 654)
-    end
-
-    # Set number of draws, how many we will save, and how many we will burn
-    # (initialized here for scoping; will re-initialize in the while loop)
-    n_blocks = 0
-    n_sim    = 0
-    mhthin   = 0
-    n_burn   = 0
-    n_params = n_parameters(m)
-
-    # Initialize algorithm by drawing para_old from a normal distribution centered on the
-    # posterior mode until the parameters are within bounds or the posterior value is
-    # sufficiently large.
-    para_old = rand(propdist, m; cc=cc0)
-    post_old = -Inf
-
-    n_blocks = n_mh_blocks(m)
-    n_sim    = n_mh_simulations(m)
-    n_burn   = n_mh_burn(m)
-    mhthin   = mh_thin(m)
-
-    initialized = false
-    while !initialized
-        post_old = posterior!(m, para_old, data; sampler = true)
-        if post_old > -Inf
-            propdist.μ = para_old
-            initialized = true
-        else
-            para_old = rand(propdist, m; cc=cc0)
-        end
-    end
-
-    # Report number of blocks that will be used
-    println(verbose, :low, "Blocks: $n_blocks")
-    println(verbose, :low, "Draws per block: $n_sim")
-
-    # For n_sim*mhthin iterations within each block, generate a new parameter draw.
-    # Decide to accept or reject, and save every (mhthin)th draw that is accepted.
-    all_rejections = 0
-
-    # Initialize matrices for parameter draws and transition matrices
-    mhparams = zeros(n_sim, n_parameters(m))
-
-    # Open HDF5 file for saving parameter draws
-    simfile = h5open(rawpath(m,"estimate","mhsave.h5",filestring_addl),"w")
-    n_saved_obs = n_sim * (n_blocks - n_burn)
-    parasim = d_create(simfile, "mhparams", datatype(Float64),
-                       dataspace(n_saved_obs,n_params),
-                       "chunk", (n_sim,n_params))
-
-    # Keep track of how long metropolis_hastings has been sampling
-    total_sampling_time = 0.
-
-    for block = 1:n_blocks
-
-        begin_time = time_ns()
-        block_rejections = 0
-
-        for j = 1:(n_sim*mhthin)
-
-            # Draw para_new from the proposal distribution
-            para_new = rand(propdist, m; cc = cc)
-
-            # Solve the model (checking that parameters are within bounds and
-            # gensys returns a meaningful system) and evaluate the posterior
-            post_new = posterior!(m, para_new, data; sampler = true)
-
-            println(verbose, :high, "Block $block, Iteration $j: posterior = $post_new")
-
-            # Choose to accept or reject the new parameter by calculating the
-            # ratio (r) of the new posterior value relative to the old one We
-            # compare min(1, r) to a number drawn randomly from a uniform (0, 1)
-            # distribution. This allows us to always accept the new draw if its
-            # posterior value is greater than the previous draw's, but it gives
-            # some probability to accepting a draw with a smaller posterior
-            # value, so that we may explore tails and other local modes.
-            r = exp(post_new - post_old)
-            x = rand(m.rng)
-
-            if x < min(1.0, r)
-                # Accept proposed jump
-                para_old = para_new
-                post_old = post_new
-                propdist.μ = para_new
-
-                println(verbose, :high, "Block $block, Iteration $j: accept proposed jump")
-            else
-                # Reject proposed jump
-                block_rejections += 1
-
-                println(verbose, :high, "Block $block, Iteration $j: reject proposed jump")
-            end
-
-            # Save every (mhthin)th draw
-            if j % mhthin == 0
-                draw_index = convert(Int, j/mhthin)
-                mhparams[draw_index, :]  = para_old'
-            end
-        end # of block
-
-        all_rejections += block_rejections
-        block_rejection_rate = block_rejections/(n_sim*mhthin)
-
-        ## Once every iblock times, write parameters to a file
-
-        # Calculate starting and ending indices for this block (corresponds to a new chunk in memory)
-        block_start = n_sim*(block-n_burn-1)+1
-        block_end   = block_start+n_sim-1
-
-        # Write parameters to file if we're past n_burn blocks
-        if block > n_burn
-            parasim[block_start:block_end, :] = map(Float64, mhparams)
-        end
-
-        # Calculate time to complete this block, average block time, and
-        # expected time to completion
-        block_time = (time_ns() - begin_time)/1e9
-        total_sampling_time += block_time
-        total_sampling_time_minutes = total_sampling_time/60
-        expected_time_remaining_sec     = (total_sampling_time/block)*(n_blocks - block)
-        expected_time_remaining_minutes = expected_time_remaining_sec/60
-
-        println(verbose, :low, "Completed $block of $n_blocks blocks.")
-        println(verbose, :low, "Total time to compute $block blocks: $total_sampling_time_minutes minutes")
-        println(verbose, :low, "Expected time remaining for Metropolis-Hastings: $expected_time_remaining_minutes minutes")
-        println(verbose, :low, "Block $block rejection rate: $block_rejection_rate \n")
-    end # of loop over blocks
-
-    close(simfile)
-
-    rejection_rate = all_rejections / (n_blocks*n_sim*mhthin)
-    println(verbose, :low, "Overall rejection rate: $rejection_rate")
-end
-
 """
 ```
 compute_parameter_covariance(m::AbstractDSGEModel)
@@ -429,46 +254,61 @@ parameter_covariance.h5 file in the `workpath(m, "estimate")` directory.
 function compute_parameter_covariance(m::AbstractDSGEModel;
                                       filestring_addl::Vector{String} = Vector{String}(undef, 0))
 
-    # Read in saved parameter draws
-    if get_setting(m, :sampling_method) == :MH
-        param_draws_path = rawpath(m, "estimate", "mhsave.h5", filestring_addl)
-        if !isfile(param_draws_path)
-            @printf stderr "Saved parameter draws not found.\n"
-            return
-        end
-        param_draws = h5open(param_draws_path, "r") do f
-            read(f, "mhparams")
-        end
-
-        # Calculate covariance matrix
-        param_covariance = cov(param_draws)
-
-        # Write to file
-        h5open(workpath(m, "estimate","parameter_covariance.h5", filestring_addl),"w") do f
-            f["mhcov"] = param_covariance
-        end
-    elseif get_setting(m, :sampling_method) == :SMC
-        param_draws_path = rawpath(m, "estimate", "smcsave.h5", filestring_addl)
-        if !isfile(param_draws_path)
-            @printf stderr "Saved parameter draws not found.\n"
-            return
-        end
-        param_draws = h5open(param_draws_path, "r") do f
-            read(f, "smcparams")
-        end
-
-        # Calculate covariance matrix
-        param_covariance = cov(param_draws)
-
-        # Write to file
-        h5open(workpath(m, "estimate","parameter_covariance.h5", filestring_addl),"w") do f
-            f["smccov"] = param_covariance
-        end
-    else
+    sampling_method = get_setting(m, :sampling_method)
+    if sampling_method ∉ [:MH, :SMC]
         throw("Invalid sampling method specified in setting :sampling_method")
     end
 
-    return param_covariance
+    prefix           = sampling_method == :MH ? "mh" : "smc"
+    param_draws_path = rawpath(m, "estimate", prefix * "save.h5", filestring_addl)
+    savepath         = workpath(m, "estimate", "parameter_covariance.h5", filestring_addl)
+
+    return compute_parameter_covariance(param_draws_path, sampling_method; savepath = savepath)
+end
+
+"""
+```
+compute_parameter_covariance(param_draws_path::String, sampling_method::Symbol;
+                             savepath = "parameter_covariance.h5")
+```
+
+Generic function calculates the parameter covariance matrix from saved parameter draws,
+writes it to the specified savepath.
+
+### Arguments
+* `param_draws_path::String`: Path to file where parameter draws are stored.
+* `sampling_method::Symbol`: Sampling method used for estimation.
+```
+   - `:MH`: Metropolis-Hastings
+   - `:SMC`: Sequential Monte Carlo
+```
+### Optional Arguments
+* `savepath::String`: Where parameter covariance matrix is to be saved. Will default
+    to "parameter_covariance.h5" if unspecified.
+"""
+function compute_parameter_covariance(param_draws_path::String, sampling_method::Symbol;
+                                      savepath::String = "parameter_covariance.h5")
+    if sampling_method ∉ [:MH, :SMC]
+        throw("Invalid sampling method specified in setting :sampling_method")
+    end
+    prefix = sampling_method == :MH ? "mh" : "smc"
+
+    if !isfile(param_draws_path)
+        @printf stderr "Saved parameter draws not found.\n"
+        return
+    end
+
+    param_draws = h5open(param_draws_path, "r") do f
+        read(f, prefix * "params")
+    end
+
+    # Calculate covariance matrix
+    param_covariance = cov(param_draws)
+
+    # Write to file
+    h5open(savepath, "w") do f
+        f[prefix * "cov"] = param_covariance
+    end
 end
 
 """
