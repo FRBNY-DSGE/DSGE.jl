7271cdc153496588691df123bc291cab58a78111
Ethan Matlin <ethanmatlin@gmail.com>
2019-08-26 17:14:27 -0400

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,7 +23,7 @@ my_tests = [
             "estimate/csminwel",
             "estimate/optimize",
 
-            "estimate/metropolis_hastings"
+            "estimate/metropolis_hastings",
 
             "estimate/smc/smc",
             "estimate/smc/helpers",
