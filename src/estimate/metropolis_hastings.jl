d07e728cdf1f2578014c4517eb5fd2201534606a
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2019-08-26 14:26:36 -0400

    Blocking passes tests

--- src/estimate/metropolis_hastings.jl
+++ src/estimate/metropolis_hastings.jl
@@ -126,22 +126,18 @@ function metropolis_hastings(propdist::Distribution,
                 # Draw para_new from the proposal distribution
 
                 # Previously:
-                para_new = rand(propdist, rng; cc = cc)
-
+                # para_new = rand(propdist, rng; cc = cc)
 
                 # Now:
                 p_block     = sort(p_block)
                 para_subset = para_old[p_block]
                 d_subset    = DegenerateMvNormal(propdist.μ[p_block], propdist.σ[p_block, p_block])
 
-                @show propdist.μ == propdist.μ[p_block]
-
-                #=
                 para_draw = rand(d_subset, rng; cc = cc)
 
                 para_new          = deepcopy(para_old)
                 para_new[p_block] = para_draw
-                =#
+
 
                 # Solve the model (checking that parameters are within bounds and
                 # gensys returns a meaningful system) and evaluate the posterior
