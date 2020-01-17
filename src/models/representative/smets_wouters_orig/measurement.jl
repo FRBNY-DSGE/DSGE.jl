acad0e2819f9aa4d00dbb2eab7d67c6a185f33e5
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2020-01-17 16:36:10 -0500

    Update SW Orig measurement for ss51

--- src/models/representative/smets_wouters_orig/measurement.jl
+++ src/models/representative/smets_wouters_orig/measurement.jl
@@ -77,6 +77,14 @@ function measurement(m::SmetsWoutersOrig{T},
         QQ[exo[:λ_f_sh], exo[:λ_f_sh]] = m[:σ_λ_f2]^2
         QQ[exo[:λ_w_sh], exo[:λ_w_sh]] = m[:σ_λ_w2]^2
         QQ[exo[:rm_sh], exo[:rm_sh]]   = m[:σ_rm2]^2
+    elseif subspec(m) in ["ss51"] && regime == 2
+        QQ[exo[:g_sh], exo[:g_sh]]     = m[:σ_g_r2]^2
+        QQ[exo[:b_sh], exo[:b_sh]]     = m[:σ_b_r2]^2
+        QQ[exo[:μ_sh], exo[:μ_sh]]     = m[:σ_μ_r2]^2
+        QQ[exo[:z_sh], exo[:z_sh]]     = m[:σ_z_r2]^2
+        QQ[exo[:λ_f_sh], exo[:λ_f_sh]] = m[:σ_λ_f_r2]^2
+        QQ[exo[:λ_w_sh], exo[:λ_w_sh]] = m[:σ_λ_w_r2]^2
+        QQ[exo[:rm_sh], exo[:rm_sh]]   = m[:σ_rm_r2]^2
     else
         QQ[exo[:g_sh], exo[:g_sh]]     = m[:σ_g]^2
         QQ[exo[:b_sh], exo[:b_sh]]     = m[:σ_b]^2
