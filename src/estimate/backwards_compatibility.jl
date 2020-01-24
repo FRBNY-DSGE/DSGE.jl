29a2f84c09359e9bb3f2b053c0b5363d1474fe62
Reca Sarfati <rebecca_sarfati@alumni.brown.edu>
2020-01-24 10:27:52 -0500

    Fix backwards compatibility between old and new clouds

--- src/estimate/backwards_compatibility.jl
+++ src/estimate/backwards_compatibility.jl
@@ -9,3 +9,20 @@ function old_to_new_cloud(cloud::DSGE.Cloud)::SMC.Cloud
                      cloud.ESS, cloud.stage_index, cloud.n_Φ, cloud.resamples,
                      cloud.c, cloud.accept, cloud.total_sampling_time)
 end
+
+"""
+```
+function Cloud(c::ParticleCloud)
+function Cloud(c::Cloud)
+```
+Returns a Cloud type.
+"""
+function SMC.Cloud(cloud::ParticleCloud)
+    return SMC.Cloud(hcat(Matrix{Float64}(get_vals(cloud)'), get_loglh(cloud), get_logprior(cloud),
+                      get_old_loglh(cloud), get_accept(cloud), get_weights(cloud)),
+                 cloud.tempering_schedule, cloud.ESS, cloud.stage_index, cloud.n_Φ,
+                 cloud.resamples, cloud.c, cloud.accept, cloud.total_sampling_time)
+end
+function SMC.Cloud(cloud::SMC.Cloud)
+    return cloud
+end
