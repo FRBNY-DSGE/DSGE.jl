bd76ad95bc39bd8629401fd5538f8de31a7677d7
William Chen <chenwilliamy77@gmail.com>
2020-02-24 09:19:31 -0500

    Implement compute population moments, VAR coefficients, and impulse responses when estimating a VAR using OLS with a DSGE as a prior (requires giving data in addition to a DSGEVAR object).

--- src/forecast/var/impulse_responses.jl
+++ src/forecast/var/impulse_responses.jl
@@ -1,7 +1,7 @@
 """
 ```
 impulse_responses(β, Σ, n_obs_shock, horizon, shock_size = 1;
-    method = :cholesky, flip_shocks = false, has_intercept = true,
+    method = :cholesky, flip_shocks = false, use_intercept = true,
     frequency_band = (2π/32, 2π/6)) where {S<:Real}
 ```
 computes the impulse responses of a VAR system represented in the form
@@ -28,8 +28,8 @@ where `Xₜ` stacks the lags of yₜ (with dimensions n_observables x n_regresso
     and `?cholesky_long_run_shock`.
 * `flip_shocks::Bool`: by default, we compute the impulse responses to a negative shock.
     Set `flip_shocks = true` to obtain a positive shock.
-* `has_intercept::Bool`: `impulse_responses` assumes `β` has constant term(s). If there
-    are no such terms, then `has_intercept` must be set to `false`.
+* `use_intercept::Bool`: `impulse_responses` assumes `β` has constant term(s). If there
+    are no such terms, then `use_intercept` must be set to `false`.
 * `frequency_band::Tuple{S,S}`: See `?maxBC_shock`.
 
 ### Outputs
@@ -39,13 +39,13 @@ function impulse_responses(β::AbstractMatrix{S}, Σ::AbstractMatrix{S}, n_obs_s
                            horizon::Int, shock_size::S = one(S);
                            method::Symbol = :cholesky,
                            flip_shocks::Bool = false,
-                           has_intercept::Bool = true,
+                           use_intercept::Bool = true,
                            frequency_band::Tuple{S,S} =
                            (2*π/32, 2*π/6)) where {S<:Real}
 
     # Compute dimensions
-    n = size(β,2)
-    lags = convert(Int, has_intercept ? (size(β,1) - 1) / n : size(β,1) / n)
+    n = size(β, 2)
+    lags = convert(Int, use_intercept ? (size(β,1) - 1) / n : size(β,1) / n)
 
     # Compute impact based on IRF type
     Y = zeros(lags + horizon, n)
@@ -63,7 +63,7 @@ function impulse_responses(β::AbstractMatrix{S}, Σ::AbstractMatrix{S}, n_obs_s
     end
 
     # For efficiency
-    if has_intercept
+    if use_intercept
         β = @views β[2:end, :]
     end
 
