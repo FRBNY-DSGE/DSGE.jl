1ebdf80d4f34ddd76de590aad48f2bb62560aba8
William Chen <chenwilliamy77@gmail.com>
2020-04-02 13:25:30 -0400

    Implement zero out most shocks subspecs in ss51 and ss52. Extend regime switching to cover case of one regime switch that happens after ZLB starts.

--- src/estimate/kalman.jl
+++ src/estimate/kalman.jl
@@ -110,6 +110,39 @@ function zlb_regime_indices(m::AbstractDSGEModel{S}, data::AbstractArray,
     return regime_inds
 end
 
+function regime_indices(m::AbstractDSGEModel{S}, data::AbstractArray,
+                        start_date::Dates.Date=date_presample_start(m)) where S<:AbstractFloat
+
+    T = size(data, 2)
+
+    n_regime1_periods = subtract_quarters(get_setting(m, :date_regime2_start), start_date)
+    # THIS HANDLES ONLY 2 CASES OF REGIME-SWITCHING
+    # It can happen only once (aside from ZLB)
+    # and it can either occur before or after the ZLB (but not both b/c it can happen once only)
+    if !(n_anticipated_shocks(m) > 0 && !isempty(data))
+        regime_inds = [1:n_regime1_periods, (n_regime1_periods+1):T]
+    elseif n_anticipated_shocks(m) > 0 && !isempty(data)
+        if start_date < date_presample_start(m)
+            error("Start date $start_date must be >= date_presample_start(m)")
+        elseif 0 < subtract_quarters(date_zlb_start(m), start_date) < T
+            n_nozlb_periods = subtract_quarters(date_zlb_start(m), start_date)
+            if n_regime1_periods > n_nozlb_periods
+                regime_inds = [1:n_nozlb_periods, n_nozlb_periods+1:n_regime1_periods,
+                               n_regime1_periods+1:T]
+            else
+                regime_inds = [1:n_regime1_periods,
+                               (n_regime1_periods+1):n_nozlb_periods,
+                               (n_nozlb_periods+1):T]
+            end
+        else
+            # if zlb_starts after end of sample, then the whole thing is n_nozlb_periods
+            regime_inds = [1:n_regime1_periods,
+                           (n_regime1_periods+1):T]
+        end
+    end
+    return regime_inds
+end
+
 """
 ```
 zlb_regime_matrices(m, system, start_date = date_presample_start(m))
@@ -154,3 +187,88 @@ function zlb_regime_matrices(m::AbstractDSGEModel{S}, system::System{S},
 
     return TTTs, RRRs, CCCs, QQs, ZZs, DDs, EEs
 end
+
+function zlb_plus_regime_matrices(m::AbstractDSGEModel{S}, system::RegimeSwitchingSystem{S},
+                                  start_date::Dates.Date=date_presample_start(m)) where S<:AbstractFloat
+    ### THIS IS WORK IN PROGRES, DOES NOT COVER ALL CASES FOR REGIME SWITCHING, ALSO ONLY FOR SWITCHING JUST ONCE.
+    if n_anticipated_shocks(m) > 0
+        if start_date < date_presample_start(m)
+            error("Start date $start_date must be >= date_presample_start(m)")
+
+            # TODO: This technically doesn't handle the case where the end_date of the sample
+            # is before the start of the ZLB
+        elseif date_presample_start(m) <= start_date <= date_zlb_start(m)
+            n_regimes = 3
+
+            shock_inds = inds_shocks_no_ant(m)
+            QQ_preZLB_R1 = zeros(size(system[1][:QQ]))
+            QQ_preZLB_R1[shock_inds, shock_inds] = system[1][:QQ][shock_inds, shock_inds]
+
+            # Figure out the appropriate regime switching mechanism
+            n_regime1_periods = subtract_quarters(get_setting(m, :date_regime2_start), start_date)
+            n_nozlb_periods = subtract_quarters(date_zlb_start(m), start_date)
+            if n_regime1_periods > n_nozlb_periods
+                QQ_ZLB = system[1][:QQ] # regime switch after ZLB
+                QQ_ZLB_R2 = system[2][:QQ]
+                QQs = Matrix{S}[QQ_preZLB_R1, QQ_ZLB, QQ_ZLB_R2]
+            elseif n_regime1_periods == n_nozlb_periods
+                QQ_ZLB = system[2][:QQ] # regime switch coincides ZLB
+                QQs = Matrix{S}[QQ_preZLB_R1, QQ_ZLB]
+            else
+                QQ_ZLB = system[2][:QQ] # regime switch before ZLB
+                QQ_preZLB_R2 = zeros(size(system[2][:QQ]))
+                QQ_preZLB_R2[shock_inds, shock_inds] = system[2][:QQ][shock_inds, shock_inds]
+                QQs = Matrix{S}[QQ_preZLB_R1, QQ_preZLB_R2, QQ_ZLB]
+            end
+        elseif date_zlb_start(m) < start_date
+            n_regimes = 1
+            QQs = Matrix{S}[system[:QQ]]
+        end
+    else
+#        @show "no ant"
+        n_regimes = 2
+        QQs = Matrix{S}[system[1][:QQ], system[2][:QQ]]
+    end
+
+    TTTs = vcat([system[1][:TTT]], fill(system[2][:TTT], n_regimes-1))
+    RRRs = vcat([system[1][:RRR]], fill(system[2][:RRR], n_regimes-1))
+    CCCs = vcat([system[1][:CCC]], fill(system[2][:CCC], n_regimes-1))
+    ZZs  = vcat([system[1][:ZZ]], fill(system[2][:ZZ], n_regimes-1))
+    DDs  = vcat([system[1][:DD]], fill(system[2][:DD], n_regimes-1))
+    EEs  = vcat([system[1][:EE]], fill(system[2][:EE], n_regimes-1))
+
+    return TTTs, RRRs, CCCs, QQs, ZZs, DDs, EEs
+end
+    #=  if n_anticipated_shocks(m) > 0
+        if start_date < date_presample_start(m)
+            error("Start date $start_date must be >= date_presample_start(m)")
+
+        # TODO: This technically doesn't handle the case where the end_date of the sample
+        # is before the start of the ZLB
+        elseif date_presample_start(m) <= start_date <= get_setting(m, :date_regime2_start) #date_zlb_start(m)
+            n_regimes = 3
+
+            shock_inds = inds_shocks_no_ant(m)
+            QQ_ZLB = system[2][:QQ]
+            QQ_preZLB = zeros(size(QQ_ZLB))
+            QQ_preZLB[shock_inds, shock_inds] = QQ_ZLB[shock_inds, shock_inds]
+            QQs = Matrix{S}[QQ_preZLB, QQ_preZLB, QQ_ZLB]
+
+        elseif date_zlb_start(m) < start_date
+            n_regimes = 2
+            QQs = Matrix{S}[system[1][:QQ], system[2][:QQ]]
+        end
+    else
+        n_regimes = 2
+        QQs = Matrix{S}[system[1][:QQ], system[2][:QQ]]
+    end
+
+    TTTs = vcat([system[1][:TTT]], fill(system[2][:TTT], n_regimes-1))
+    RRRs = vcat([system[1][:RRR]], fill(system[2][:RRR], n_regimes-1))
+    CCCs = vcat([system[1][:CCC]], fill(system[2][:CCC], n_regimes-1))
+    ZZs  = vcat([system[1][:ZZ]], fill(system[2][:ZZ], n_regimes-1))
+    DDs  = vcat([system[1][:DD]], fill(system[2][:DD], n_regimes-1))
+    EEs  = vcat([system[1][:EE]], fill(system[2][:EE], n_regimes-1))
+
+    return TTTs, RRRs, CCCs, QQs, ZZs, DDs, EEs
+end=#
