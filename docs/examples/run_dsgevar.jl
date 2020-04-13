a82c022f5f673c7d6db301fcaf5c445ba8801cc1
William Chen <chenwilliamy77@gmail.com>
2020-04-13 11:07:36 -0400

    Implement IRFs showing DSGEVAR(Inf) approximates the true DSGE irfs well, when you have an identified DSGEVAR

--- docs/examples/run_dsgevar.jl
+++ docs/examples/run_dsgevar.jl
@@ -3,11 +3,11 @@ using Plots.PlotMeasures
 
 # What do you want to do?
 estimate_dsgevar  = false   # Estimate a DSGEVAR using SMC
-use_estim_output  = true   # Use posterior from SMC estimation for following code. Otherwise, use default parameters.
+use_estim_output  = false   # Use posterior from SMC estimation for following code. Otherwise, use default parameters.
 get_VAR_system    = true   # Compute VAR coefficients and innovations-covariance matrix
 do_modal_irf      = true   # Compute IRFs using modal parameters
-compare_modal_irf = true  # Plot the modal DSGEVAR λ = ∞ rotation IRF and the actual DSGE IRF for observables
-do_full_band_irf  = true   # Compute IRFs using parameters drawn from a distribution
+compare_modal_irf = true   # Plot the modal DSGEVAR λ = ∞ rotation IRF and the actual DSGE IRF for observables
+do_full_band_irf  = false   # Compute IRFs using parameters drawn from a distribution
 create_meansbands = false  # Save full_band_irfs to MeansBands
 do_parallel       = false  # Use parallel workers
 n_workers         = 10
@@ -17,6 +17,9 @@ Random.seed!(1793)
 
 # Set up DSGE and load data
 m = AnSchorfheide("ss0")
+m[:e_R] = 0. # DSGEVAR does not use
+m[:e_π] = 0. # measurement error with
+m[:e_y] = 0. # subspec ss3
 forecast_string = "test_dsgevar" # Change this to an empty string if you don't want an identifier for saved output
 m <= Setting(:sampling_method, :SMC)
 m <= Setting(:impulse_response_horizons, 20)
@@ -29,19 +32,19 @@ m <= Setting(:date_mainsample_start, DSGE.quartertodate("1960-Q1")) # Set bounds
 m <= Setting(:date_presample_start, DSGE.quartertodate("1959-Q3"))  # data being loaded so we don't
 m <= Setting(:date_forecast_start, DSGE.quartertodate("2019-Q3"))   # get NaNs.
 data = df_to_matrix(m, load_data(m))    # the rows of the data MUST correspond
-data = data[1:2, :]                     # to the exact order of the observables specified by the DSGEVAR.
+                                        # to the exact order of the observables specified by the DSGEVAR.
 
 # Construct a DSGEVAR from AnSchorfheide
-dsgevar = DSGEVAR(m, collect(keys(m.exogenous_shocks)), "ss2")
+dsgevar = DSGEVAR(m, collect(keys(m.exogenous_shocks)), "ss3")
 DSGE.update!(dsgevar, λ = dsgevar_λ)
 
-#= ss2 prespecifies the observables, lags, and λ
+#= ss3 prespecifies the observables, lags, and λ
 Equivalently, we could have run
 
 ```
 dsgevar = DSGEVAR(m)
 DSGE.update!(dsgevar, observables =
-[:obs_gdp, :obs_cpi],
+collect(keys(m.observables)),
 shocks = collect(keys(m.exogenous_shocks)),
 lags = 4, λ = dsgevar_λ)
 ```
@@ -49,8 +52,8 @@ lags = 4, λ = dsgevar_λ)
 Or we could have also run
 
 ```
-dsgevar = DSGEVAR(m, "ss2")
-DSGE.update!(dsgevar, shocks = collect(keys(m.exogenous_shocks)), observables = [:obs_gdp, :obs_cpi])
+dsgevar = DSGEVAR(m, "ss3")
+DSGE.update!(dsgevar, shocks = collect(keys(m.exogenous_shocks)), observables = collect(keys(m.observables)))
 ```
 =#
 
@@ -97,6 +100,7 @@ if do_modal_irf
 
     if use_estim_output
         DSGE.update!(dsgevar, load_draws(dsgevar, :mode))
+        DSGE.update!(m,       load_draws(dsgevar, :mode))
     end
 
     ## DSGE impulse responses
@@ -140,6 +144,11 @@ if do_modal_irf
     DSGE.update!(dsgevar; λ = dsgevar_λ)
 
     if compare_modal_irf
+        DSGE.update!(dsgevar; λ = Inf)
+        modal_irfs[:var_approx][:rotation] =
+            impulse_responses(dsgevar, data; horizon = impulse_response_horizons(dsgevar),
+                              deviations = true, flip_shocks = false, normalize_rotation = true)
+        DSGE.update!(dsgevar; λ = dsgevar_λ)
         dsge_system = compute_system(m)
         dsge_states, dsge_obs, dsge_pseudo = impulse_responses(m, dsge_system)
         plots_dict = Dict{Symbol, Dict{Symbol,Plots.Plot}}()
@@ -147,7 +156,7 @@ if do_modal_irf
             plots_dict[shock] = Dict{Symbol, Plots.Plot}()
             for (obs, i) in DSGE.get_observables(dsgevar)
                 plots_dict[shock][obs] = plot(1:impulse_response_horizons(m), dsge_obs[i, :, j],
-                                              label = "DSGE", color = :red, linestyle = :dash)
+                                              label = "DSGE", color = :red, linestyle = :dash, linewidth = 3)
                 plot!(1:impulse_response_horizons(m), modal_irfs[:var_approx][:rotation][i, :, j],
                       label = "DSGE-VAR (\\lambda = Inf)", color = :black,
                       left_margin = 20px, linewidth = 3)
