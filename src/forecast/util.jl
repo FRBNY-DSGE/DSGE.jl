c21bba2c901054d38ff61038466752a4c42da506
William Chen <chenwilliamy77@gmail.com>
2020-04-28 11:19:51 -0400

    Extend forecast tests to include different ordering of regimes.

--- src/forecast/util.jl
+++ src/forecast/util.jl
@@ -66,7 +66,7 @@ function forecast_block_inds(m::AbstractDSGEModel, input_type::Symbol; subset_in
         if input_type == :full || input_type == :prior || input_type == :init_draw_shocks || input_type == :mode_draw_shocks
             regime_switching = (haskey(m.settings, :regime_switching) && haskey(m.settings, :regime_switching_ndraws)) ?
                 get_setting(m, :regime_switching) : false
-            ndraws    = regime_switching ? get_setting(m, :regime_switching_ndraws) : n_forecast_draws(m, input_type)
+            ndraws    = regime_switching ? get_setting(m, :regime_switching_ndraws) : n_forecast_draws(m, :full)
             jstep     = get_jstep(m, ndraws)
             start_ind = 1
             end_ind   = ndraws
