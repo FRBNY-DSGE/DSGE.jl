a1e2323e6ecfadacc2dc1bef8ce73817fcebebce
William Chen <chenwilliamy77@gmail.com>
2020-05-27 09:42:06 -0400

    Comment out pseudoobs we usually don't use after forecast.

--- src/models/representative/m1002/pseudo_observables.jl
+++ src/models/representative/m1002/pseudo_observables.jl
@@ -4,10 +4,10 @@ function init_pseudo_observable_mappings!(m::Model1002)
                     :MarginalCost, :Wages, :FlexibleWages, :Hours, :FlexibleHours, :z_t,
                     :Expected10YearRateGap, :NominalFFR, :Expected10YearRate,
                     :Expected10YearNaturalRate,
-                    :ExpectedNominalNaturalRate, :NominalRateGap, :LaborProductivityGrowth,
-                    :u_t, :NominalWageGrowth,
-                    :i_f_t, :R_t, :c_f_t, :qk_f_t, :k_f_t, :kbar_f_t, :u_f_t, :rk_f_t, :w_f_t, :L_f_t, :rktil_f_t, :n_f_t, :c_t,
-                    :b_t, :r_f_t]
+                    :ExpectedNominalNaturalRate, :NominalRateGap, :LaborProductivityGrowth]
+                    # :u_t, :NominalWageGrowth
+                    # :i_f_t, :R_t, :c_f_t, :qk_f_t, :k_f_t, :kbar_f_t, :u_f_t, :rk_f_t, :w_f_t, :L_f_t, :rktil_f_t, :n_f_t, :c_t,
+                    # :b_t, :r_f_t]
 
     if subspec(m) == "ss12"
         to_add = [:g_t, :b_t, :μ_t, :λ_f_t, :λ_w_t, :rm_t, :σ_ω_t, :μ_e_t,
@@ -176,54 +176,54 @@ function init_pseudo_observable_mappings!(m::Model1002)
     pseudo[:LaborProductivityGrowth].longname = "Labor Productivity Growth Rate"
     pseudo[:LaborProductivityGrowth].rev_transform = quartertoannual
 
-    pseudo[:u_t].name     = "u_t"
-    pseudo[:u_t].longname = "u_t"
+    # pseudo[:u_t].name     = "u_t"
+    # pseudo[:u_t].longname = "u_t"
 
-    pseudo[:NominalWageGrowth].name = "Nominal Wage Growth"
-    pseudo[:NominalWageGrowth].longname = "Nominal Wage Growth"
+    # pseudo[:NominalWageGrowth].name = "Nominal Wage Growth"
+    # pseudo[:NominalWageGrowth].longname = "Nominal Wage Growth"
 
-    pseudo[:i_f_t].name     = "i_f_t"
-    pseudo[:i_f_t].longname = "i_f_t"
+    # pseudo[:i_f_t].name     = "i_f_t"
+    # pseudo[:i_f_t].longname = "i_f_t"
 
-    pseudo[:R_t].name     = "R_t"
-    pseudo[:R_t].longname = "R_t"
+    # pseudo[:R_t].name     = "R_t"
+    # pseudo[:R_t].longname = "R_t"
 
-    pseudo[:c_f_t].name     = "c_f_t"
-    pseudo[:c_f_t].longname = "c_f_t"
+    # pseudo[:c_f_t].name     = "c_f_t"
+    # pseudo[:c_f_t].longname = "c_f_t"
 
-    pseudo[:r_f_t].name     = "r_f_t"
-    pseudo[:r_f_t].longname = "r_f_t"
+    # pseudo[:r_f_t].name     = "r_f_t"
+    # pseudo[:r_f_t].longname = "r_f_t"
 
-    pseudo[:qk_f_t].name     = "qk_f_t"
-    pseudo[:qk_f_t].longname = "qk_f_t"
+    # pseudo[:qk_f_t].name     = "qk_f_t"
+    # pseudo[:qk_f_t].longname = "qk_f_t"
 
-    pseudo[:k_f_t].name     = "k_f_t"
-    pseudo[:k_f_t].longname = "k_f_t"
+    # pseudo[:k_f_t].name     = "k_f_t"
+    # pseudo[:k_f_t].longname = "k_f_t"
 
-    pseudo[:kbar_f_t].name     = "kbar_f_t"
-    pseudo[:kbar_f_t].longname = "kbar_f_t"
+    # pseudo[:kbar_f_t].name     = "kbar_f_t"
+    # pseudo[:kbar_f_t].longname = "kbar_f_t"
 
-    pseudo[:u_f_t].name     = "u_f_t"
-    pseudo[:u_f_t].longname = "u_f_t"
+    # pseudo[:u_f_t].name     = "u_f_t"
+    # pseudo[:u_f_t].longname = "u_f_t"
 
-    pseudo[:rk_f_t].name     = "rk_f_t"
-    pseudo[:rk_f_t].longname = "rk_f_t"
+    # pseudo[:rk_f_t].name     = "rk_f_t"
+    # pseudo[:rk_f_t].longname = "rk_f_t"
 
-    pseudo[:w_f_t].name     = "w_f_t"
-    pseudo[:w_f_t].longname = "w_f_t"
+    # pseudo[:w_f_t].name     = "w_f_t"
+    # pseudo[:w_f_t].longname = "w_f_t"
 
-    pseudo[:L_f_t].name     = "L_f_t"
-    pseudo[:L_f_t].longname = "L_f_t"
+    # pseudo[:L_f_t].name     = "L_f_t"
+    # pseudo[:L_f_t].longname = "L_f_t"
 
 
-    pseudo[:rktil_f_t].name     = "rktil_f_t"
-    pseudo[:rktil_f_t].longname = "rktil_f_t"
+    # pseudo[:rktil_f_t].name     = "rktil_f_t"
+    # pseudo[:rktil_f_t].longname = "rktil_f_t"
 
-    pseudo[:b_t].name     = "b_t"
-    pseudo[:b_t].longname = "b_t"
+    # pseudo[:b_t].name     = "b_t"
+    # pseudo[:b_t].longname = "b_t"
 
-    pseudo[:n_f_t].name     = "n_f_t"
-    pseudo[:n_f_t].longname = "n_f_t"
+    # pseudo[:n_f_t].name     = "n_f_t"
+    # pseudo[:n_f_t].longname = "n_f_t"
 
     if subspec(m) in ["ss13", "ss14", "ss15", "ss16", "ss17", "ss18", "ss19"]
         pseudo[:Sinf_t].name     = "Sinf_t"
@@ -329,14 +329,14 @@ function init_pseudo_observable_mappings!(m::Model1002)
         end
     end
 
-    if subspec(m) == "ss60"
-        pseudo[:ziid].name     = "ziid"
-        pseudo[:ziid].longname = "ziid"
-        pseudo[:biidc].name     = "biidc"
-        pseudo[:biidc].longname = "biidc"
-        pseudo[:varphiiid].name     = "varphiiid"
-        pseudo[:varphiiid].longname = "varphiiid"
-    end
+    # if subspec(m) == "ss60"
+    #     pseudo[:ziid].name     = "ziid"
+    #     pseudo[:ziid].longname = "ziid"
+    #     pseudo[:biidc].name     = "biidc"
+    #     pseudo[:biidc].longname = "biidc"
+    #     pseudo[:varphiiid].name     = "varphiiid"
+    #     pseudo[:varphiiid].longname = "varphiiid"
+    # end
 
     if haskey(m.settings, :add_laborproductivity_measurement)
         if get_setting(m, :add_laborproductivity_measurement)
