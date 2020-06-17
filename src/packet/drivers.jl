f57009f383ea838c9ac9a30c376b2d3305e2763f
William Chen <chenwilliamy77@gmail.com>
2020-06-17 09:54:24 -0400

    Add params keyword to usual_model_forecast. Rename aug keyword to regime_switching.

--- src/packet/drivers.jl
+++ src/packet/drivers.jl
@@ -38,12 +38,14 @@ usual_model_forecast(m, input_type, cond_type,
     output_vars = [:histobs, :histpseudo, :forecastobs, :forecastpseudo];
     est_override = "", forecast_string = "",
     density_bands = [0.5, 0.6, 0.7, 0.8, 0.9],
-    mb_matrix = false, check_empty_columns = true)
+    mb_matrix = false, check_empty_columns = true, params = [])
 ```
 
 Forecast, compute means and bands, and optionally (if `mb_matrix`) convert
 `MeansBands` to matrices. If the path `est_override` is provided, it will be
 added to `forecast_input_file_overrides(m)`.
+
+See `?forecast_one` for descriptions of the keywords.
 """
 function usual_model_forecast(m::AbstractDSGEModel, input_type::Symbol, cond_type::Symbol,
                               output_vars::Vector{Symbol} = [:histobs, :histpseudo, :forecastobs, :forecastpseudo,
@@ -55,7 +57,8 @@ function usual_model_forecast(m::AbstractDSGEModel, input_type::Symbol, cond_typ
                               shock_name::Symbol = :none,
                               shock_var_name::Symbol = :none,
                               shock_var_value::Float64 = 0.0,
-                              check_empty_columns = true)
+                              check_empty_columns = true,
+                              params::AbstractArray{Float64} = AbstractVector{Float64}(undef, 0))
 
     # Override estimation file if necessary
     if !isempty(est_override)
@@ -69,7 +72,8 @@ function usual_model_forecast(m::AbstractDSGEModel, input_type::Symbol, cond_typ
                  shock_name = shock_name,
                  shock_var_name = shock_var_name,
                  shock_var_value = shock_var_value,
-                 check_empty_columns = check_empty_columns)
+                 check_empty_columns = check_empty_columns,
+                 params = params)
 
     # Compute means and bands
     compute_meansbands(m, input_type, cond_type, output_vars; forecast_string = forecast_string,
