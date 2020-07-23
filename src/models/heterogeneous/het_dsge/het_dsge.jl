b8c8a860b7392938ac348329afc4fde19b281470
William Chen <chenwilliamy77@gmail.com>
2020-07-23 13:18:10 -0400

    Fix data and save roots for heterogeneous models

--- src/models/heterogeneous/het_dsge/het_dsge.jl
+++ src/models/heterogeneous/het_dsge/het_dsge.jl
@@ -495,8 +495,8 @@ function model_settings!(m::HetDSGE)
     vint = Dates.format(now(), DSGE_DATE_FORMAT)
     m <= Setting(:data_vintage, vint, true, "vint", "Data vintage")
 
-    saveroot = normpath(joinpath(dirname(@__FILE__), "../../../","save"))
-    datapath = normpath(joinpath(dirname(@__FILE__), "../../../","save","input_data"))
+    saveroot = normpath(joinpath(dirname(@__FILE__), "../../../../","save"))
+    datapath = normpath(joinpath(dirname(@__FILE__), "../../../../","save","input_data"))
 
     m <= Setting(:saveroot, saveroot, "Root of data directory structure")
     m <= Setting(:dataroot, datapath, "Input data directory path")
