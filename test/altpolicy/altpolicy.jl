d7c58b100b5b75df6cd1a5464f8ed17e7b341018
William Chen <chenwilliamy77@gmail.com>
2020-07-23 13:17:40 -0400

    Complete new altpolicy tests.

--- test/altpolicy/altpolicy.jl
+++ test/altpolicy/altpolicy.jl
@@ -85,192 +85,102 @@ end
 # New Policy Rules #
 ####################
 
-m = Model1002(testing = true)
+m = Model1002(; testing = true, custom_settings = Dict{Symbol, Setting}(:add_pgap => Setting(:add_pgap, true)))
+my = Model1002(; testing = true, custom_settings = Dict{Symbol, Setting}(:add_pgap => Setting(:add_pgap, true),
+                                                                         :add_ygap => Setting(:add_ygap, true)))
 m <= Setting(:date_forecast_start, quartertodate("2015-Q4"))
 m <= Setting(:use_population_forecast, true)
+my <= Setting(:date_forecast_start, quartertodate("2015-Q4"))
+my <= Setting(:use_population_forecast, true)
 
 # Getting indices
 eq_mp    = m.equilibrium_conditions[:eq_mp]
 eq_other = setdiff(1:n_equilibrium_conditions(m), eq_mp)
+eq_mpy    = my.equilibrium_conditions[:eq_mp]
+eq_othery = setdiff(1:n_equilibrium_conditions(my), eq_mpy)
+eq_otherymp = 1:n_equilibrium_conditions(my)
 
 # Compare equilibrium conditions under historical and alternative rules
 Γ0_hist, Γ1_hist, ~ = eqcond(m)
+Γ0_histy, Γ1_histy, ~ = eqcond(my)
 
 # Matrices for alt_inflation
 m <= Setting(:alternative_policy, DSGE.alt_inflation())
 Γ0_alt_inf,  Γ1_alt_inf,  ~ = alternative_policy(m).eqcond(m)
 
-# Repeat for rw
-#m <= Setting(:alternative_policy, DSGE.rw())
-#Γ0_alt_rw,  Γ1_alt_rw,  ~ = alternative_policy(m).eqcond(m)
-
-# Repeat for rw_zero_rate
-#m <= Setting(:alternative_policy, DSGE.rw_zero_rate())
-#Γ0_alt_rw_zero,  Γ1_alt_rw_zero,  ~ = alternative_policy(m).eqcond(m)
-
 # Repeat for zero_rate
 m <= Setting(:alternative_policy, DSGE.zero_rate())
 Γ0_alt_zero,  Γ1_alt_zero,  ~ = alternative_policy(m).eqcond(m)
 
+# Repeat for rw
+my <= Setting(:alternative_policy, DSGE.rw())
+Γ0_alt_rw,  Γ1_alt_rw,  ~ = alternative_policy(my).eqcond(my)
+
+# Repeat for rw_zero_rate
+my <= Setting(:alternative_policy, DSGE.rw_zero_rate())
+Γ0_alt_rw_zero,  Γ1_alt_rw_zero,  ~ = alternative_policy(my).eqcond(my)
+
 # Repeat for smooth_ait_gdp
-#m <= Setting(:alternative_policy, DSGE.smooth_ait_gdp())
-#Γ0_alt_smooth,  Γ1_alt_smooth,  ~ = alternative_policy(m).eqcond(m)
+my <= Setting(:alternative_policy, DSGE.smooth_ait_gdp())
+Γ0_alt_smooth,  Γ1_alt_smooth,  ~ = alternative_policy(my).eqcond(my)
 
 # Repeat for smooth_ait_gdp_alt
-#m <= Setting(:alternative_policy, DSGE.smooth_ait_gdp_alt())
+my <= Setting(:alternative_policy, DSGE.smooth_ait_gdp_alt())
 
-#Γ0_alt_smooth_alt,  Γ1_alt_smooth_alt,  ~ = alternative_policy(m).eqcond(m)
+Γ0_alt_smooth_alt,  Γ1_alt_smooth_alt,  ~ = alternative_policy(my).eqcond(my)
 
 # Repeat for ait
-#m <= Setting(:alternative_policy, DSGE.ait())
-#m <= Setting(:pgap_type, :ait)
-#m <= Setting(:pgap_value, 0.)
-#Γ0_alt_ait,  Γ1_alt_ait,  ~ = alternative_policy(m).eqcond(m)
+my <= Setting(:alternative_policy, DSGE.ait())
+my <= Setting(:pgap_type, :ait)
+my <= Setting(:pgap_value, 0.)
+Γ0_alt_ait,  Γ1_alt_ait,  ~ = alternative_policy(my).eqcond(my)
 
 # Repeat for ngdp
-#m <= Setting(:alternative_policy, DSGE.ngdp())
-#m <= Setting(:pgap_type, :ngdp)
-#m <= Setting(:pgap_value, 12.)
-#Γ0_alt_ngdp,  Γ1_alt_ngdp,  ~ = alternative_policy(m).eqcond(m)
+my <= Setting(:alternative_policy, DSGE.ngdp())
+my <= Setting(:pgap_type, :ngdp)
+my <= Setting(:pgap_value, 12.)
+Γ0_alt_ngdp,  Γ1_alt_ngdp,  ~ = alternative_policy(my).eqcond(my)
 
 
 # Testing
 @testset "Compare AltPolicy eqconds under hist and alt rules" begin
-
     @test Γ0_hist[eq_other, :] ≈ Γ0_alt_inf[eq_other, :]
     @test Γ1_hist[eq_other, :] ≈ Γ1_alt_inf[eq_other, :]
     @test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_inf[eq_mp, :])
     @test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_inf[eq_mp, :])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_rw[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_rw[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_rw[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_rw[eq_mp, :])
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_rw[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_rw[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_rw[eq_mp, eq_othery])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_rw[eq_mp, eq_othery])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_rw_zero[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_rw_zero[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_rw_zero[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_rw_zero[eq_mp, :])
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_rw_zero[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_rw_zero[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_rw_zero[eq_mp, eq_othery])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_rw_zero[eq_mp, eq_othery])
 
-    @test Γ0_hist[eq_other, :] ≈ Γ0_alt_zero[eq_other, :]
-    @test Γ1_hist[eq_other, :] ≈ Γ1_alt_zero[eq_other, :]
+    @test Γ0_hist[eq_other, eq_other] ≈ Γ0_alt_zero[eq_other, eq_other]
+    @test Γ1_hist[eq_other, eq_other] ≈ Γ1_alt_zero[eq_other, eq_other]
     @test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_zero[eq_mp, :])
     @test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_zero[eq_mp, :])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_smooth[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_smooth[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_smooth[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_smooth[eq_mp, :])
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_smooth[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_smooth[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_smooth[eq_mp, eq_othery])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_smooth[eq_mp, eq_othery])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_smooth_alt[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_smooth_alt[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_smooth_alt[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_smooth_alt[eq_mp, :])
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_smooth_alt[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_smooth_alt[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_smooth_alt[eq_mp, eq_other])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_smooth_alt[eq_mp, eq_other])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_ait[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_ait[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_ait[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_ait[eq_mp, :])
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_ait[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_ait[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_ait[eq_mp, eq_othery])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_ait[eq_mp, eq_othery])
 
-    #@test Γ0_hist[eq_other, :] ≈ Γ0_alt_ngdp[eq_other, :]
-    #@test Γ1_hist[eq_other, :] ≈ Γ1_alt_ngdp[eq_other, :]
-    #@test !(Γ0_hist[eq_mp, :]  ≈ Γ0_alt_ngdp[eq_mp, :])
-    #@test !(Γ1_hist[eq_mp, :]  ≈ Γ1_alt_ngdp[eq_mp, :])
-end
-
-# Forecast under alt_inflation and rw
-#m <= Setting(:alternative_policy, DSGE.alt_inflation())
-#out_alt_inf = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-#m <= Setting(:alternative_policy, DSGE.rw())
-#m<= Setting(:pgap_type, :rw)
-#out_alt_rw = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-
-#m <= Setting(:alternative_policy, DSGE.rw_zero_rate())
-#m<= Setting(:pgap_type, :rw_zero_rate)
-#out_alt_rw_zero = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-#m <= Setting(:alternative_policy, DSGE.zero_rate())
-#m<= Setting(:pgap_type, :zero_rate)
-#out_alt_zero = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-
-#m <= Setting(:alternative_policy, DSGE.smooth_ait_gdp())
-#out_alt_smooth = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-#m <= Setting(:alternative_policy, DSGE.smooth_ait_gdp_alt())
-#out_alt_smooth_alt = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-
-#m <= Setting(:alternative_policy, DSGE.ait())
-#m <= Setting(:pgap_type, :ait)
-#m <= Setting(:pgap_value, 0.)
-#out_alt_ait = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-
-#m <= Setting(:alternative_policy, DSGE.ngdp())
-#m <= Setting(:pgap_type, :ngdp)
-#m <= Setting(:pgap_value, 12.)
-#out_alt_ngdp = DSGE.forecast_one_draw(m, :mode, :none, output_vars, paras, df, verbose = :none)
-
-
-# @testset "Check forecast under alt_inflation" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_inf[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_inf[:forecastpseudo])
-# end
-# @testset "Check forecast under rw" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_rw[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_rw[:forecastpseudo])
-# end
-
-# @testset "Check forecast under rw_zero_rate" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_rw_zero[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_rw_zero[:forecastpseudo])
-# end
-# @testset "Check forecast under zero_rate" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_zero[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_zero[:forecastpseudo])
-# end
-
-# @testset "Check forecast under smooth_ait_gdp" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_smooth[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_smooth[:forecastpseudo])
-# end
-# @testset "Check forecast under smooth_ait_gdp_alt" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_smooth_alt[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_smooth_alt[:forecastpseudo])
-# end
-
-# @testset "Check forecast under ait" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_ait[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_ait[:forecastpseudo])
-# end
-# @testset "Check forecast under ngdp" begin
-#     @test out_hist[:histpseudo] ≈ out_alt_ngdp[:histpseudo]
-#     @test !(out_hist[:forecastpseudo] ≈ out_alt_ngdp[:forecastpseudo])
-# end
-
-
-# Checking if setting variables works
-## These values are random and not actually good choices
-m <= Setting(:ait_Thalf, 14.2)
-m <= Setting(:gdp_Thalf, 5.6)
-m <= Setting(:smooth_ait_gdp_ρ_smooth, 0.23)
-m <= Setting(:smooth_ait_gdp_φ_π, 0.36)
-m <= Setting(:smooth_ait_gdp_φ_y, 0.56)
-m <= Setting(:smooth_ait_gdp_alt_ρ_smooth, 0.76)
-m <= Setting(:smooth_ait_gdp_alt_φ_π, 0.11)
-m <= Setting(:smooth_ait_gdp_alt_φ_y, 0.89)
-m <= Setting(:rw_ρ_smooth, 0.45)
-m <= Setting(:rw_φ_π, 0.83)
-m <= Setting(:rw_φ_y, 0.94)
-m <= Setting(:ρ_rw, 0.63)
-
-@testset "Checking Settings assignments" begin
-    @test get_setting(m, :ait_Thalf) == 14.2
-    @test get_setting(m, :gdp_Thalf) == 5.6
-    @test get_setting(m, :smooth_ait_gdp_ρ_smooth) == 0.23
-    @test get_setting(m, :smooth_ait_gdp_φ_π) == 0.36
-    @test get_setting(m, :smooth_ait_gdp_φ_y) == 0.56
-    @test get_setting(m, :smooth_ait_gdp_alt_ρ_smooth) == 0.76
-    @test get_setting(m, :smooth_ait_gdp_alt_φ_π) == 0.11
-    @test get_setting(m, :smooth_ait_gdp_alt_φ_y) == 0.89
-    @test get_setting(m, :rw_ρ_smooth) == 0.45
-    @test get_setting(m, :rw_φ_π) == 0.83
-    @test get_setting(m, :rw_φ_y) == 0.94
-    @test get_setting(m, :ρ_rw) == 0.63
+    @test Γ0_histy[eq_othery, eq_otherymp] ≈ Γ0_alt_ngdp[eq_othery, eq_otherymp]
+    @test Γ1_histy[eq_othery, eq_otherymp] ≈ Γ1_alt_ngdp[eq_othery, eq_otherymp]
+    @test !(Γ0_histy[eq_mp, eq_other]  ≈ Γ0_alt_ngdp[eq_mp, eq_othery])
+    @test !(Γ1_histy[eq_mp, eq_other]  ≈ Γ1_alt_ngdp[eq_mp, eq_othery])
 end
