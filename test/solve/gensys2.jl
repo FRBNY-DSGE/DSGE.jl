e2f6dda23f8567bbce8d950116187114f3a03d11
William Chen <chenwilliamy77@gmail.com>
2020-11-02 15:28:18 -0500

    Add test for redundant regimes in gensys2.

--- test/solve/gensys2.jl
+++ test/solve/gensys2.jl
@@ -29,10 +29,10 @@ using DSGE, ModelConstructors, Dates, Test
     ModelConstructors.set_regime_val!(m[:α], 4, m[:α].value)
     sys2 = compute_system(m)
 
-    @test sys1[1][:TTT] == sys2[1][:TTT]
-    @test sys1[2][:TTT] == sys2[2][:TTT]
-    @test sys1[3][:TTT] == sys2[3][:TTT]
-    @test sys1[4][:TTT] == sys2[4][:TTT]
+    @test sys1[1, :TTT] == sys2[1, :TTT]
+    @test sys1[2, :TTT] == sys2[2, :TTT]
+    @test sys1[3, :TTT] == sys2[3, :TTT]
+    @test sys1[4, :TTT] == sys2[4, :TTT]
 end
 
 
@@ -65,10 +65,10 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:TTT] == sys2[1][:TTT]
-    @test sys1[2][:TTT] == sys2[2][:TTT]
-    @test sys1[3][:TTT] != sys2[3][:TTT]
-    @test sys1[4][:TTT] == sys2[4][:TTT]
+    @test sys1[1, :TTT] == sys2[1, :TTT]
+    @test sys1[2, :TTT] == sys2[2, :TTT]
+    @test sys1[3, :TTT] != sys2[3, :TTT]
+    @test sys1[4, :TTT] == sys2[4, :TTT]
 end
 
 
@@ -102,10 +102,10 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:TTT] == sys2[1][:TTT]
-    @test sys1[2][:TTT] == sys2[2][:TTT]
-    @test sys1[3][:TTT] != sys2[3][:TTT]
-    @test sys1[4][:TTT] == sys2[4][:TTT]
+    @test sys1[1, :TTT] == sys2[1, :TTT]
+    @test sys1[2, :TTT] == sys2[2, :TTT]
+    @test sys1[3, :TTT] != sys2[3, :TTT]
+    @test sys1[4, :TTT] == sys2[4, :TTT]
 end
 
 @testset "sys1: no rule-switch, no parameter-switch. sys2: rule-switch in 1st forecast quarter, parameter-switch in 2nd forecastquarter" begin
@@ -138,12 +138,11 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:TTT] == sys2[1][:TTT]
-    @test sys1[2][:TTT] == sys2[2][:TTT]
-    @test sys1[3][:TTT] != sys2[3][:TTT]
-    @test sys1[4][:TTT] != sys2[4][:TTT]
+    @test sys1[1, :TTT] == sys2[1, :TTT]
+    @test sys1[2, :TTT] == sys2[2, :TTT]
+    @test sys1[3, :TTT] != sys2[3, :TTT]
+    @test sys1[4, :TTT] != sys2[4, :TTT]
 end
-
 
 @testset "sys1: no rule-switch, no parameter-swtich. sys2: no rule-switch, yes parameter-switch (in 1st forecast quarter)" begin
     m = Model1002("ss10")
@@ -170,11 +169,13 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:TTT] == sys2[1][:TTT]
-    @test sys1[2][:TTT] == sys2[2][:TTT]
-    @test sys1[3][:TTT] != sys2[3][:TTT]
-    @test sys1[4][:TTT] == sys2[4][:TTT]
+    @test sys1[1, :TTT] == sys2[1, :TTT]
+    @test sys1[2, :TTT] == sys2[2, :TTT]
+    @test sys1[3, :TTT] != sys2[3, :TTT]
+    @test sys1[4, :TTT] == sys2[4, :TTT]
+
 end
+
 
 ### Now with QQ
 @testset "sys1: rule-switch (1st forecast quarter), no parameter-switch. sys2: rule-switch (1st forecast quarter), no parameter-switch (parameter switch with same values)" begin
@@ -206,10 +207,10 @@ end
     ModelConstructors.set_regime_val!(m[:σ_g], 4, m[:σ_g].value)
     sys2 = compute_system(m)
 
-    @test sys1[1][:QQ] == sys2[1][:QQ]
-    @test sys1[2][:QQ] == sys2[2][:QQ]
-    @test sys1[3][:QQ] == sys2[3][:QQ]
-    @test sys1[4][:QQ] == sys2[4][:QQ]
+    @test sys1[1, :QQ] == sys2[1, :QQ]
+    @test sys1[2, :QQ] == sys2[2, :QQ]
+    @test sys1[3, :QQ] == sys2[3, :QQ]
+    @test sys1[4, :QQ] == sys2[4, :QQ]
 end
 
 @testset "sys1: no rule-swtich, sys2 rule-switch in 1st forecast quarter (so system 3 should be different)" begin
@@ -244,10 +245,10 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:QQ] == sys2[1][:QQ]
-    @test sys1[2][:QQ] == sys2[2][:QQ]
-    @test sys1[3][:QQ] == sys2[3][:QQ]
-    @test sys1[4][:QQ] == sys2[4][:QQ]
+    @test sys1[1, :QQ] == sys2[1, :QQ]
+    @test sys1[2, :QQ] == sys2[2, :QQ]
+    @test sys1[3, :QQ] == sys2[3, :QQ]
+    @test sys1[4, :QQ] == sys2[4, :QQ]
 end
 
 
@@ -282,10 +283,10 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:QQ] == sys2[1][:QQ]
-    @test sys1[2][:QQ] == sys2[2][:QQ]
-    @test sys1[3][:QQ] != sys2[3][:QQ]
-    @test sys1[4][:QQ] == sys2[4][:QQ]
+    @test sys1[1, :QQ] == sys2[1, :QQ]
+    @test sys1[2, :QQ] == sys2[2, :QQ]
+    @test sys1[3, :QQ] != sys2[3, :QQ]
+    @test sys1[4, :QQ] == sys2[4, :QQ]
 end
 
 @testset "sys1: no rule-switch, no parameter-switch. sys2: rule-switch in 1st forecast quarter, parameter-switch in 2nd forecastquarter" begin
@@ -321,10 +322,10 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:QQ] == sys2[1][:QQ]
-    @test sys1[2][:QQ] == sys2[2][:QQ]
-    @test sys1[3][:QQ] == sys2[3][:QQ]
-    @test sys1[4][:QQ] != sys2[4][:QQ]
+    @test sys1[1, :QQ] == sys2[1, :QQ]
+    @test sys1[2, :QQ] == sys2[2, :QQ]
+    @test sys1[3, :QQ] == sys2[3, :QQ]
+    @test sys1[4, :QQ] != sys2[4, :QQ]
 end
 
 
@@ -353,8 +354,47 @@ end
 
     sys2 = compute_system(m)
 
-    @test sys1[1][:QQ] == sys2[1][:QQ]
-    @test sys1[2][:QQ] == sys2[2][:QQ]
-    @test sys1[3][:QQ] != sys2[3][:QQ]
-    @test sys1[4][:QQ] == sys2[4][:QQ]
+    @test sys1[1, :QQ] == sys2[1, :QQ]
+    @test sys1[2, :QQ] == sys2[2, :QQ]
+    @test sys1[3, :QQ] != sys2[3, :QQ]
+    @test sys1[4, :QQ] == sys2[4, :QQ]
+end
+
+@testset "Adding redundant regimes does not affect gensys2" begin
+    # Add additional check for using replace_eqcond "falsely"
+    m = Model1002("ss10")
+    # Always need regime switching on, even if you're not switching parameters
+    m <= Setting(:regime_switching, true)
+
+    m <= Setting(:date_forecast_start, Date(2020, 9, 30))
+    m <= Setting(:regime_dates, Dict(1 => date_presample_start(m), #Start
+                                     2 => Date(1990, 3, 31), # Parameter switch in history
+                                     3 => Date(2020, 6, 30), # Parameter and/or rule switch in 1st forecast quarter
+                                     4 => Date(2020, 9, 30),
+                                     5 => Date(2020, 12, 31),
+                                     6 => Date(2021, 3, 31))) #Parameter and/or rule switch in 2nd forecast quarter
+    setup_regime_switching_inds!(m)
+
+    m <= Setting(:gensys2, true)
+    m <= Setting(:replace_eqcond, true)
+    m <= Setting(:replace_eqcond_func_dict, Dict(4 => DSGE.zero_rate_replace_eq_entries))
+    sys1 = compute_system(m)
+    m <= Setting(:regime_dates, Dict(1 => date_presample_start(m), #Start
+                                     2 => Date(1990, 3, 31), # Parameter switch in history
+                                     3 => Date(2020, 6, 30), # Parameter and/or rule switch in 1st forecast quarter
+                                     4 => Date(2020, 9, 30),
+                                     5 => Date(2020, 12, 31),
+                                     6 => Date(2021, 3, 31),
+                                     7 => Date(2021, 6, 30))) #Parameter and/or rule switch in 2nd forecast quarter
+    setup_regime_switching_inds!(m)
+    sys2 = compute_system(m)
+
+    for i in 1:n_regimes(sys1)
+        @test sys1[i, :TTT] ≈ sys2[i, :TTT]
+        @test sys1[i, :RRR] ≈ sys2[i, :RRR]
+        @test sys1[i, :CCC] ≈ sys2[i, :CCC]
+    end
+    @test sys2[6, :TTT] ≈ sys[7, :TTT]
+    @test sys2[6, :RRR] ≈ sys[7, :RRR]
+    @test sys2[6, :CCC] ≈ sys[7, :CCC]
 end
