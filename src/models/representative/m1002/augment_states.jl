ff664d8a7c45863158df2aedd09b2316a91ce07e
William Chen <chenwilliamy77@gmail.com>
2020-12-17 14:00:05 -0500

    Add @info statement noting when regime-switching and temporary policy settings info is printed.

--- src/models/representative/m1002/augment_states.jl
+++ src/models/representative/m1002/augment_states.jl
@@ -61,8 +61,8 @@ function augment_states(m::Model1002, TTT::Matrix{T}, RRR::Matrix{T}, CCC::Vecto
     RRR_aug = [RRR; zeros(n_states_add, n_exo)]
     CCC_aug = [CCC; zeros(n_states_add)]
 
-    for p in m.parameters
-        if !isempty(p.regimes)
+    for para in m.parameters
+        if !isempty(para.regimes)
             if (haskey(get_settings(m), :model2para_regimes) ? haskey(get_setting(m, :model2para_regime), para.key) : false)
                 ModelConstructors.toggle_regime!(para, reg, get_setting(m, :model2para_regime)[para.key])
             else
