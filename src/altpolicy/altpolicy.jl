0cc4eec1cd709a2c23bb084810782d934d3c7782
Shlok Goyal <shlok.goyal@gmail.com>
2021-01-07 16:32:49 -0500

    Revert "Move solve_altpolicy to altpolicy.jl"
    This reverts commit 09f95c48ad2ebf6d5b87126b92fe0e1f1dc1a190.

--- src/altpolicy/altpolicy.jl
+++ src/altpolicy/altpolicy.jl
@@ -37,7 +37,7 @@ Type defining an alternative policy rule.
 mutable struct AltPolicy
     key::Symbol
     eqcond::Function
-    solve::Function
+    solve::Fucntion
     replace_eqcond::Function
     forecast_init::Function
     color::Colorant
@@ -74,70 +74,5 @@ function altpolicy_replace_eq_entries(key::Symbol)
         rw_replace_eq_entries
     else
         error("AltPolicy $(key) has not been added to `altpolicy_replace_eq_entries` as having a method for replacing equilibrium conditions")
-    end
-end
-
-"""
-```
-altpolicy_solve(m::AbstractDSGEModel, altpolicy::AltPolicy;
-    regime_switching::Bool = false, regimes::Vector{Int} = [1])
-```
-
-Solves for the transition equation of `m` under the given altpolicy
-"""
-function altpolicy_solve(m::AbstractDSGEModel, altpolicy::AltPolicy;
-                         regime_switching::Bool = false, regimes::Vector{Int} = [1])
-    # Get equilibrium condition matrices
-
-    if length(regimes) == 1
-        Γ0, Γ1, C, Ψ, Π  = altpolicy.eqcond(m, regimes[1])
-        TTT_gensys, CCC_gensys, RRR_gensys, eu = gensys(Γ0, Γ1, C, Ψ, Π, 1+1e-6, verbose = :low)
-
-        # Check for LAPACK exception, existence and uniqueness
-        if eu[1] != 1 || eu[2] != 1
-            throw(GensysError())
-        end
-
-        TTT_gensys = real(TTT_gensys)
-        RRR_gensys = real(RRR_gensys)
-        CCC_gensys = real(CCC_gensys)
-
-        # Augment states
-        TTT, RRR, CCC = DSGE.augment_states(m, TTT_gensys, RRR_gensys, CCC_gensys; regime_switching = regime_switching,
-                                            reg = regimes[1])
-        return TTT, RRR, CCC
-    else
-        Γ0s = Vector{Matrix{Float64}}(undef, length(regimes))
-        Γ1s = Vector{Matrix{Float64}}(undef, length(regimes))
-        Cs = Vector{Vector{Float64}}(undef, length(regimes))
-        Ψs = Vector{Matrix{Float64}}(undef, length(regimes))
-        Πs = Vector{Matrix{Float64}}(undef, length(regimes))
-        for reg in regimes
-            Γ0s[reg], Γ1s[reg], Cs[reg], Ψs[reg], Πs[reg]  = altpolicy.eqcond(m, reg)
-        end
-
-        n_regimes = length(regimes)
-        TTTs= Vector{Matrix{Float64}}(undef, n_regimes)
-        RRRs = Vector{Matrix{Float64}}(undef, n_regimes)
-        CCCs = Vector{Vector{Float64}}(undef, n_regimes)
-
-        # Solve model
-        for reg in regimes
-            TTT_gensys, CCC_gensys, RRR_gensys, eu = gensys(Γ0s[reg], Γ1s[reg], Cs[reg], Ψs[reg], Πs[reg], 1+1e-6)
-
-            if !((eu[1] == 1) & (eu[2] == 1))
-                throw(GensysError("Gensys does not give existence"))
-            end
-            TTT_gensys = real(TTT_gensys)
-            RRR_gensys = real(RRR_gensys)
-            CCC_gensys = reshape(CCC_gensys, size(CCC_gensys, 1))
-
-            # Augment states
-            TTTs[reg], RRRs[reg], CCCs[reg] = DSGE.augment_states(m, TTT_gensys, RRR_gensys, CCC_gensys;
-                                                             regime_switching = regime_switching,
-                                                             reg = reg)
-        end
-
-        return TTTs, RRRs, CCCs
     end
 end
