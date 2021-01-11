7a4de9399129cca7ffe22357d14e5f97e5f44b7c
Alissa Johnson <alissa@oneotaslopes.org>
2021-01-11 15:15:07 -0500

    Switch over to using regime_eqcond_info throughout.

--- src/statespace_functions.jl
+++ src/statespace_functions.jl
@@ -144,7 +144,6 @@ function compute_system(m::AbstractDSGEModel{T}; apply_altpolicy::Bool = false,
     ## Correct the measurement equations for anticipated observables via convex combination
     for reg in sort!(collect(keys(get_setting(m, :regime_eqcond_info))))
         new_wt = regime_eqcond_info[reg].weights
-@show new_wt
         if has_fwd_looking_obs
             for k in get_setting(m, :forward_looking_observables)
                 # COMMENTED CODE IS THE DESIRED VERSION in some form or fashion. May want to write a helper function which
