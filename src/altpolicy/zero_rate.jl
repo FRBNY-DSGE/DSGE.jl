4ec513165440e76df4e21624fe95ef07d392a505
Alissa Johnson <alissa@oneotaslopes.org>
2021-01-22 16:07:32 -0500

    Remove toggle_regimes.

--- src/altpolicy/zero_rate.jl
+++ src/altpolicy/zero_rate.jl
@@ -18,7 +18,7 @@ function zero_rate_replace_eq_entries(m::AbstractDSGEModel,
     C[eq[:eq_mp]] = 0.0 / 4. - m[:Rstarn]
 
     return Γ0, Γ1, C, Ψ, Π
- end
+end
 
 """
 ```
@@ -28,7 +28,7 @@ zero_rate_eqcond(m::AbstractDSGEModel)
 Solves for the transition equation of `m` under a price level
 targeting rule (implemented by adding a price-gap state)
 """
-function zero_rate_eqcond(m::AbstractDSGEModel, reg::Int = 1; toggle_regimes = true)
+function zero_rate_eqcond(m::AbstractDSGEModel, reg::Int = 1)
 
     # get the old indices
     old_states = sort!(collect(values(m.endogenous_states)))
@@ -37,25 +37,21 @@ function zero_rate_eqcond(m::AbstractDSGEModel, reg::Int = 1; toggle_regimes = t
     # Get equilibrium condition matrices
     Γ0, Γ1, C, Ψ, Π  = eqcond(m, reg)
 
-    if toggle_regimes
-        for para in m.parameters
-            if !isempty(para.regimes)
-                if (haskey(get_settings(m), :model2para_regime) ? haskey(get_setting(m, :model2para_regime), para.key) : false)
-                    ModelConstructors.toggle_regime!(para, reg, get_setting(m, :model2para_regime)[para.key])
-                else
-                    ModelConstructors.toggle_regime!(para, reg)
-                end
+    for para in m.parameters
+        if !isempty(para.regimes)
+            if (haskey(get_settings(m), :model2para_regime) ? haskey(get_setting(m, :model2para_regime), para.key) : false)
+                ModelConstructors.toggle_regime!(para, reg, get_setting(m, :model2para_regime)[para.key])
+            else
+                ModelConstructors.toggle_regime!(para, reg)
             end
         end
     end
 
     Γ0, Γ1, C, Ψ, Π = zero_rate_replace_eq_entries(m, Γ0, Γ1, C, Ψ, Π)
 
-    if toggle_regimes
-        for para in m.parameters
-            if !isempty(para.regimes)
-                ModelConstructors.toggle_regime!(para, 1)
-            end
+    for para in m.parameters
+        if !isempty(para.regimes)
+            ModelConstructors.toggle_regime!(para, 1)
         end
     end
 
@@ -89,7 +85,7 @@ function zero_rate_solve(m::AbstractDSGEModel; regime_switching::Bool = false, r
 
         # Augment states
         TTT, RRR, CCC = DSGE.augment_states(m, TTT_gensys, RRR_gensys, CCC_gensys; regime_switching = regime_switching,
-                                       reg = regimes[1])
+                                            reg = regimes[1])
         return TTT, RRR, CCC
     else
         Γ0s = Vector{Matrix{Float64}}(undef, length(regimes))
@@ -119,8 +115,8 @@ function zero_rate_solve(m::AbstractDSGEModel; regime_switching::Bool = false, r
 
             # Augment states
             TTTs[reg], RRRs[reg], CCCs[reg] = DSGE.augment_states(m, TTT_gensys, RRR_gensys, CCC_gensys;
-                                                             regime_switching = regime_switching,
-                                                             reg = reg)
+                                                                  regime_switching = regime_switching,
+                                                                  reg = reg)
         end
     end
 
@@ -135,6 +131,6 @@ init_zero_rate_forecast(m::AbstractDSGEModel, shocks::Matrix{T}, final_state::Ve
 Adjust shocks matrix and final state vector for forecasting under the ZERO_RATE rule
 """
 function zero_rate_forecast_init(m::AbstractDSGEModel, shocks::Matrix{T}, final_state::Vector{T};
-                           cond_type::Symbol = :none) where {T<:AbstractFloat}
+                                 cond_type::Symbol = :none) where {T<:AbstractFloat}
     return shocks, final_state
 end
