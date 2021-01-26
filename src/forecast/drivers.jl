e12674c331e5ca47bdefd9783c37523ef3196f27
William Chen <chenwilliamy77@gmail.com>
2021-01-26 13:40:26 -0500

    Delete some additional comments

--- src/forecast/drivers.jl
+++ src/forecast/drivers.jl
@@ -753,13 +753,6 @@ function forecast_one_draw(m::AbstractDSGEModel{Float64}, input_type::Symbol, co
     # Compute state space
     update!(m, params) # Note that params is a Vector{Float64}, not a ParameterVector. This `update!` infers if the forecast is regime-switching if length(params) > length(m.parameters)
 
-#=    if regime_switching
-        # system = compute_system(m; apply_altpolicy = alternative_policy(m).solve != solve, tvis = tvis)
-        system = compute_system(m; tvis = tvis)
-    else
-        system = compute_system(m; tvis = tvis)
-    end=#
-
     system = compute_system(m; tvis = tvis)
 
     # Initialize output dictionary
@@ -871,9 +864,6 @@ function forecast_one_draw(m::AbstractDSGEModel{Float64}, input_type::Symbol, co
         # Re-solve model with alternative policy rule, if applicable
         apply_altpolicy = alternative_policy(m).solve != solve
         if apply_altpolicy
-#=            if !regime_switching
-                system = compute_system(m; apply_altpolicy = true, tvis = tvis)
-            end=#
 
             if haskey(get_settings(m), :skip_altpolicy_state_init) ? !get_setting(m, :skip_altpolicy_state_init) : true
                 # Adjust the initial state vector for pgap and ygap
