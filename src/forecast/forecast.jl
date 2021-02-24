39ffe3b92a78e422de6657aec6daafcf14adb2c9
Alissa Johnson <alissa@oneotaslopes.org>
2021-02-24 09:37:39 -0500

    Reapply ccde36cf.

--- src/forecast/forecast.jl
+++ src/forecast/forecast.jl
@@ -432,6 +432,7 @@ function forecast(m::AbstractDSGEModel, z0::Vector{S}, states::AbstractMatrix{S}
     end
     m <= Setting(:temporary_altpol_length, 0)
 
+    system = nothing
     if !isnothing(first_zlb_regime) # Then there are ZLB regimes to enforce
         first_zlb_regime += n_hist_regimes
         if cond_type != :none
@@ -530,8 +531,7 @@ function forecast(m::AbstractDSGEModel, z0::Vector{S}, states::AbstractMatrix{S}
             # tvis = haskey(get_settings(m), :tvis_information_set) && !isempty(get_setting(m, :tvis_information_set))
 
             # Recompute to account for new regimes
-            system = compute_system(m; tvis = true)
-
+            system = compute_system(m; tvis = tvis)
             if rerun_smoother # if state space system changes, then the smoothed states will also change generally
                 histstates, histshocks, histpseudo, initial_states =
                     smooth(m, df, system; cond_type = cond_type, draw_states = draw_states)
@@ -579,14 +579,27 @@ function forecast(m::AbstractDSGEModel, z0::Vector{S}, states::AbstractMatrix{S}
         end
     end
 
-    if !all(obs[get_observables(m)[:obs_nominalrate], :] .> tol)
+    if !all(obs[get_observables(m)[:obs_nominalrate], :] .> get_setting(m, :zero_rate_zlb_value)/4. + tol)
         if nan_failures
             @warn "Unable to enforce the ZLB. Throwing NaN for forecasts"
             states .= NaN
             obs    .= NaN
             pseudo .= NaN
         else
-            states, obs, pseudo = forecast(m, system, z0; cond_type = cond_type, shocks = shocks, enforce_zlb = true)
+            @warn "Unable to enforce the ZLB. Using unant shocks to enforce ZLB."
+            try
+                if isnothing(system)
+                    tvis = haskey(get_settings(m), :tvis_information_set) && !isempty(get_setting(m, :tvis_information_set))
+                    system = compute_system(m; tvis = tvis)
+                end
+                states, obs, pseudo = forecast(m, system, z0; cond_type = cond_type, shocks = shocks, enforce_zlb = true)
+            catch e
+                println(e)
+                @warn "Mystery error, NaNing forecasts."
+                states .= NaN
+                obs .= NaN
+                pseudo .=NaN
+            end
         end
     end
 
