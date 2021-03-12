61c3d531ae7a9ebbda057e1e92d48500daf9a035
William Chen <chenwilliamy77@gmail.com>
2021-03-12 13:57:23 -0500

    Extend gensys2 to allow sparse matrices

--- src/solve/gensys2.jl
+++ src/solve/gensys2.jl
@@ -20,7 +20,9 @@ function gensys2(m::AbstractDSGEModel, Γ0::Matrix{Float64}, Γ1::Matrix{Float64
     C_til = C
     Ψ_til = Ψ
 
-    Γ0_til, Γ1_til, Γ2_til, C_til, Ψ_til = gensys_to_predictable_form(Γ0, Γ1, C, Ψ, Π)
+    Γ0_til, Γ1_til, Γ2_til, C_til, Ψ_til =
+        gensys_to_predictable_form(Γ0, Γ1, C, Ψ, Π; use_sparse = (haskey(get_settings(m), :gensys2_sparse_matrices) &&
+                                                                  get_setting(m, :gensys2_sparse_matrices)))
 
     Tcal = Vector{Matrix{Float64}}(undef, T_switch)
     Rcal = Vector{Matrix{Float64}}(undef, T_switch)
@@ -55,7 +57,10 @@ function gensys2(m::AbstractDSGEModel, Γ0s::Vector{Matrix{Float64}}, Γ1s::Vect
     Ψ_tils = Vector{Matrix{Float64}}(undef, ntil)
 
     for i in 1:length(Γ0s)
-        Γ0_tils[i], Γ1_tils[i], Γ2_tils[i], C_tils[i], Ψ_tils[i] = gensys_to_predictable_form(Γ0s[i], Γ1s[i], Cs[i], Ψs[i], Πs[i])
+        Γ0_tils[i], Γ1_tils[i], Γ2_tils[i], C_tils[i], Ψ_tils[i] =
+            gensys_to_predictable_form(Γ0s[i], Γ1s[i], Cs[i], Ψs[i], Πs[i];
+                                       use_sparse = (haskey(get_settings(m), :gensys2_sparse_matrices) &&
+                                                     get_setting(m, :gensys2_sparse_matrices)))
     end
 
     Tcal = Vector{Matrix{Float64}}(undef, T_switch)
@@ -70,9 +75,9 @@ function gensys2(m::AbstractDSGEModel, Γ0s::Vector{Matrix{Float64}}, Γ1s::Vect
     Ccal[end] = CCC
 
     for t = 1:(T_switch-1)
-        Tcal[end-t] = (Γ2_tils[end-t]*TTT + Γ0_tils[end-t])\Γ1_tils[end-t]
-        Rcal[end-t] = (Γ2_tils[end-t]*TTT + Γ0_tils[end-t])\Ψ_tils[end-t]
-        Ccal[end-t] = (Γ2_tils[end-t]*TTT + Γ0_tils[end-t])\(C_tils[end-t] - Γ2_tils[end-t]*CCC)
+        Tcal[end-t] = (Γ2_tils[end-t] * TTT + Γ0_tils[end-t]) \ Γ1_tils[end-t]
+        Rcal[end-t] = (Γ2_tils[end-t] * TTT + Γ0_tils[end-t]) \ Ψ_tils[end-t]
+        Ccal[end-t] = (Γ2_tils[end-t] * TTT + Γ0_tils[end-t]) \ (C_tils[end-t] - Γ2_tils[end-t] * CCC)
 
         TTT = Tcal[end-t]
         CCC = Ccal[end-t]
@@ -83,24 +88,31 @@ function gensys2(m::AbstractDSGEModel, Γ0s::Vector{Matrix{Float64}}, Γ1s::Vect
 end
 
 function gensys_to_predictable_form(Γ0::AbstractMatrix{S}, Γ1::AbstractMatrix{S}, C::AbstractVector{S},
-                                    Ψ::AbstractMatrix{S}, Π::AbstractMatrix{S}) where {S <: Real}
+                                    Ψ::AbstractMatrix{S}, Π::AbstractMatrix{S};
+                                    use_sparse::Bool = false) where {S <: Real}
 
     nonexp_eq_ind = map(i -> all(Π[i, :] .== 0.), 1:size(Π, 1))
-    Γ0_til = zeros(S, size(Γ0))
-    Γ1_til = zeros(S, size(Γ1))
-    Γ2_til = zeros(S, size(Γ0))
+    if use_sparse
+        Γ0_til = spzeros(S, size(Γ0))
+        Γ1_til = zeros(S, size(Γ1))   # this one has to be a dense matrix for left-divide
+        Γ2_til = spzeros(S, size(Γ0))
+    else
+        Γ0_til = zeros(S, size(Γ0))
+        Γ1_til = zeros(S, size(Γ1))
+        Γ2_til = zeros(S, size(Γ0))
+    end
     C_til  = C
     Ψ_til  = Ψ
 
     for row in 1:length(nonexp_eq_ind)
         if nonexp_eq_ind[row]
             # Not expectational equation
-            Γ1_til[row, :]  = Γ1[row, :]
-            Γ0_til[row, :]  = Γ0[row, :]
+            Γ1_til[row, :]  = view(Γ1, row, :)
+            Γ0_til[row, :]  = view(Γ0, row, :)
         else
             # expectational equation, assumed to take the form zₜ = Eₜ₋₁[zₜ] + ηₜ
-            Γ0_til[row, findfirst(Γ1[row, :] .> 0)] = -1.
-            Γ2_til[row, findfirst(Γ0[row, :] .> 0)] = 1.
+            Γ0_til[row, findfirst(x -> x > 0., view(Γ1, row, :))] = -1.
+            Γ2_til[row, findfirst(x -> x > 0., view(Γ0, row, :))] = 1.
         end
     end
 
